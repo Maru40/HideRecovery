789d8cdf54de63430154ea6f18f9bf44a63841ac
Maru40 <umeda.4072@gmail.com>
2022-08-04 15:55:58 +0900

    PlayerObjectにカメラ系付与

--- HideRecovery/GameSources/PlayerObject.cpp
+++ HideRecovery/GameSources/PlayerObject.cpp
@@ -14,6 +14,14 @@
 #include "RotationController.h"
 #include "PlayerController.h"
 
+#include "CameraHelper.h"
+#include "CameraRotater.h"
+#include "PlayerCameraMover.h"
+#include "ChasingTarget.h"
+#include "SpringArmComponent.h"
+
+#include "LookAtCameraManager.h"
+
 namespace basecross {
 
 	PlayerObject::PlayerObject(const std::shared_ptr<Stage>& stage):
@@ -24,14 +32,36 @@ namespace basecross {
 		//デバッグ
 		auto draw = AddComponent<PNTStaticDraw>();
 		draw->SetMeshResource(L"DEFAULT_CUBE");
+		//draw->SetDiffuse(Col4(1.0f, 1.0f, 1.0f, 0.3f));
+		SetAlphaActive(true);
 
 		transform->SetScale(Vec3(1.0f, 1.5f, 1.0f));
 
 		AddComponent<CollisionObb>();
-		AddComponent<PlayerMover>();
+		auto playerMover = AddComponent<PlayerMover>();
+		playerMover->SetIsCameraAffected(true);
 		AddComponent<RotationController>();
 		AddComponent<PlayerController>();
-		AddComponent<Gravity>();
+		//AddComponent<Gravity>();
+
+		auto springArm = GetStage()->Instantiate<GameObject>(Vec3(), Quat());
+
+		auto chasingTarget = springArm->AddComponent<ChasingTarget>();
+		chasingTarget->SetTarget(GetThis<GameObject>());
+		auto cameraMover = springArm->AddComponent<PlayerCameraMover>();
+		cameraMover->SetMinRotX(-XM_PIDIV4);
+		cameraMover->SetMaxRotX(XM_PIDIV4);
+		auto springArmComponent = springArm->AddComponent<SpringArmComponent>();
+
+		Quat quat = Quat::Identity();
+		quat.rotationY(XM_PI);
+
+		auto tpsCamera = GetStage()->Instantiate<GameObject>(Vec3(0, 0, 3), quat);
+		auto virtualCamera = tpsCamera->AddComponent<VirtualCamera>(10);
+		tpsCamera->AddComponent<LookAtCameraManager>(GetThis<GameObject>());
+
+		springArmComponent->AddHitTag(L"Wall");
+		springArmComponent->SetChildObject(tpsCamera);
 	}
 
 }
