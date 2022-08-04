e4917f8cd4614bdea28e9cac30ddf4470a845af4
Maru40 <umeda.4072@gmail.com>
2022-08-04 09:32:09 +0900

    Stator_Heroの生成

---
+++ HideRecovery/GameSources/Stator_Hero.cpp
@@ -0,0 +1,111 @@
+/*!
+@file Stator_Hero.cpp
+@brief Stator_Heroなど実体
+*/
+
+#include "stdafx.h"
+#include "Project.h"
+
+#include "StatorBase.h"
+#include "EnemyStatorBase.h"
+#include "Stator_Hero.h"
+
+#include "EnemyBase.h"
+#include "EnemyMainStateMachine.h"
+
+#include "Utility_EnemyStator.h"
+
+#include "TimeHelper.h"
+#include "GameTimer.h"
+#include "EnemyStateNodeBase.h"
+#include "EnemyStateNode_Plowling.h"
+
+#include "GameManager.h"
+
+#include "TaskList.h"
+#include "Task_Wait.h"
+#include "Task_PlowlingMove.h"
+#include "Task_ToTargetMove.h"
+
+namespace basecross {
+	namespace Enemy {
+
+		//--------------------------------------------------------------------------------------
+		/// パラメータ
+		//--------------------------------------------------------------------------------------
+
+		struct Stator_Hero::Parametor {
+			std::shared_ptr<Enemy::StateNode::Plowling::Parametor> plowlingParamPtr;    //徘徊
+
+			Parametor():
+				plowlingParamPtr(new Enemy::StateNode::Plowling::Parametor())
+			{}
+		};
+
+		//--------------------------------------------------------------------------------------
+		/// 遷移条件メンバー
+		//--------------------------------------------------------------------------------------
+
+		Stator_Hero_TransitionMember::Stator_Hero_TransitionMember() :
+			attackRange(1.0f)
+		{}
+
+		//--------------------------------------------------------------------------------------
+		/// 遷移条件
+		//--------------------------------------------------------------------------------------
+
+
+
+		//--------------------------------------------------------------------------------------
+		/// ヒーローのステーター
+		//--------------------------------------------------------------------------------------
+
+		Stator_Hero::Stator_Hero(const std::shared_ptr<GameObject>& objPtr)
+			: Stator_Hero(objPtr, TransitionMember())
+		{}
+
+		Stator_Hero::Stator_Hero(const std::shared_ptr<GameObject>& objPtr, const TransitionMember& member)
+			: EnemyStatorBase(objPtr, member), m_paramPtr(new Parametor())
+		{
+			//GetRefBlackBoardParam().deathParamPtr = m_paramPtr->deathParamPtr;
+		}
+
+		void Stator_Hero::CreateNode() {
+			SettingParametor();
+			auto enemy = GetGameObject()->GetComponent<EnemyBase>();
+
+			//Start(生成中)
+			m_stateMachine->AddNode(State::Start, nullptr);
+
+			//徘徊
+			m_stateMachine->AddNode(
+				State::Plowling, std::make_shared<Enemy::StateNode::Plowling>(enemy, m_paramPtr->plowlingParamPtr));
+		}
+
+		void Stator_Hero::CreateEdge() {
+			using namespace UtilityStator;
+
+			//Start
+			m_stateMachine->AddEdge(State::Start, State::Plowling,
+				[&](const TransitionMember& member) { return GameManager::GetInstance()->GetCurrentState() == GameManager::State::Game; });
+
+		}
+
+		void Stator_Hero::SettingParametor() {
+			SettingPlowling();
+		}
+
+		void Stator_Hero::SettingPlowling() {
+			auto& plowlingParamPtr = m_paramPtr->plowlingParamPtr;
+
+			plowlingParamPtr->waitParamPtr->time = 1.0f;
+
+			//移動パラメータ
+			auto& moveParamPtr = plowlingParamPtr->moveParamPtr->moveParamPtr;
+			moveParamPtr->speed = 5.0f;
+			moveParamPtr->targetNearRange = 6.0f;
+			moveParamPtr->moveType = Task_ToTargetMove::MoveType::ArriveVelocity;
+		}
+
+	}
+}
