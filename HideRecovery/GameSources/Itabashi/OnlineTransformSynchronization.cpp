a387cc8a7f13fd0a252a5d5c8fce8dc7e9352438
Itabashi Oota <itaoo0430@gmail.com>
2022-08-09 15:36:20 +0900

    オンラインでTransform(座標と回転)を同期するコンポーネントを作成

---
+++ HideRecovery/GameSources/Itabashi/OnlineTransformSynchronization.cpp
@@ -0,0 +1,58 @@
+﻿#include "stdafx.h"
+#include "OnlineTransformSynchronization.h"
+
+namespace basecross
+{
+namespace Online
+{
+	struct OnlineTransformData
+	{
+		Vec3 position;
+		Quat rotation;
+	};
+
+	OnlineTransformSynchronization::OnlineTransformSynchronization(const std::shared_ptr<GameObject>& owner) :
+		OnlineComponent(owner)
+	{
+		
+	}
+
+	void OnlineTransformSynchronization::OnCreate()
+	{
+		m_transform = GetGameObject()->GetComponent<Transform>();
+	}
+
+	void OnlineTransformSynchronization::OnUpdate2()
+	{
+		auto transform = m_transform.lock();
+		int localNumber = OnlineManager::GetLocalPlayer().playerNumber;
+
+		if (!transform || m_playerNumber == 0 || m_playerNumber != localNumber)
+		{
+			return;
+		}
+
+		OnlineTransformData data;
+		data.position = transform->GetWorldPosition();
+		data.rotation = transform->GetWorldQuaternion();
+
+		OnlineManager::RaiseEvent(false, (std::uint8_t*)&data, sizeof(OnlineTransformData), EVENT_CODE);
+	}
+
+	void OnlineTransformSynchronization::OnCustomEventAction(int playerNumber, std::uint8_t eventCode, const std::uint8_t* bytes)
+	{
+		auto transform = m_transform.lock();
+
+		if (!transform || m_playerNumber != playerNumber)
+		{
+			return;
+		}
+
+		OnlineTransformData data = *(OnlineTransformData*)bytes;
+		
+		transform->SetWorldPosition(data.position);
+		transform->SetWorldQuaternion(data.rotation);
+	}
+
+}
+}
