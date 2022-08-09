b245db37d7ae8a8c1333a8a8181a1cfeefbe65c7
Maru40 <umeda.4072@gmail.com>
2022-08-09 15:28:56 +0900

    AnimationCtrlでI_StateNodeの実装

--- HideRecovery/GameSources/AnimationCtrl.h
+++ HideRecovery/GameSources/AnimationCtrl.h
@@ -1,19 +1,53 @@
 
 /*!
-@file PlayerAnimationCtrl.h
-@brief PlayerAnimationCtrl.hクラス
+@file AnimationCtrl.h
+@brief AnimationCtrl.hクラス
 担当：丸山裕喜
 */
 
 #pragma once
 #include "stdafx.h"
 
+#include "StateNodeBase.h"
+
 namespace basecross {
+
+	class I_AnimationStateNode {
+	public:
+		virtual void OnStart() = 0;
+		virtual bool OnUpdate(const float speed = 1.0f) = 0;
+		virtual void OnExit() = 0;
+	};
+
+	class AniamtionStateNodeBase : public I_AnimationStateNode
+	{
+		std::weak_ptr<GameObject> m_owner;
+		std::weak_ptr<SmBaseDraw> m_model;
+
+	public:
+		AniamtionStateNodeBase(const std::shared_ptr<GameObject>& owner) :
+			m_owner(owner)
+		{
+			m_model = owner->GetComponent<SmBaseDraw>(false);
+		}
+
+		virtual bool OnUpdate(const float speed = 1.0f) override {
+			if (!m_model.lock()) {
+				return true;
+			}
+
+			auto delta = App::GetApp()->GetElapsedTime();
+			auto model = m_model.lock();
+
+			model->UpdateAnimation(delta * speed);
+
+			return false;
+		}
+	};
 
 	class AnimationCtrl : public Component
 	{
 	public:
-
 		AnimationCtrl(const std::shared_ptr<GameObject>& objPtr) 
 			:Component(objPtr)
 		{}
