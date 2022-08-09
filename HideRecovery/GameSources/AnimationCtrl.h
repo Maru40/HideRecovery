943fde15168e73a0f050ddcc2a7d81c651ad0b73
Maru40 <umeda.4072@gmail.com>
2022-08-09 19:04:36 +0900

    AnimationCtrlでパラメータの設定

--- HideRecovery/GameSources/AnimationCtrl.h
+++ HideRecovery/GameSources/AnimationCtrl.h
@@ -14,22 +14,26 @@ namespace basecross {
 
 	class I_AnimationStateNode {
 	public:
+		virtual ~I_AnimationStateNode() = default;
+
 		virtual void OnStart() = 0;
 		virtual bool OnUpdate(const float speed = 1.0f) = 0;
 		virtual void OnExit() = 0;
 	};
 
-	class AniamtionStateNodeBase : public I_AnimationStateNode
+	class AnimationStateNodeBase : public I_AnimationStateNode
 	{
 		std::weak_ptr<GameObject> m_owner;
 		std::weak_ptr<SmBaseDraw> m_model;
 
 	public:
-		AniamtionStateNodeBase(const std::shared_ptr<GameObject>& owner) :
+		AnimationStateNodeBase(const std::shared_ptr<GameObject>& owner) :
 			m_owner(owner)
 		{
 			m_model = owner->GetComponent<SmBaseDraw>(false);
 		}
+
+		virtual ~AnimationStateNodeBase() = default;
 
 		virtual bool OnUpdate(const float speed = 1.0f) override {
 			if (!m_model.lock()) {
@@ -43,6 +47,18 @@ namespace basecross {
 
 			return false;
 		}
+
+		/// <summary>
+		/// 所有者の取得
+		/// </summary>
+		/// <returns>所有者</returns>
+		std::shared_ptr<GameObject> GetOwner() const { return m_owner.lock(); }
+
+		/// <summary>
+		/// モデルの取得
+		/// </summary>
+		/// <returns>モデル</returns>
+		std::shared_ptr<SmBaseDraw> GetModel() const { return m_model.lock(); }
 	};
 
 	class AnimationCtrl : public Component
@@ -51,6 +67,8 @@ namespace basecross {
 		AnimationCtrl(const std::shared_ptr<GameObject>& objPtr) 
 			:Component(objPtr)
 		{}
+
+		virtual ~AnimationCtrl() = default;
 
 		virtual void OnCreate() override {};
 		virtual void OnUpdate() override {};
