14f3bb05ddf22d7346122d85f4c80ae03fd83cb3
Takumi Watanabe <zeal20300166@fsgcl.jp>
2022-08-10 15:09:25 +0900

    AdvSmBaseDrawを追加

---
+++ HideRecovery/GameSources/Watanabe/Shader/AdvSmBaseDraw.cpp
@@ -0,0 +1,235 @@
+﻿#include "stdafx.h"
+#include "AdvSmBaseDraw.h"
+#include "DrawComponents.h"
+
+namespace basecross {
+	struct AdvSmBaseDraw::Impl :public DrawObjectBase {
+		//頂点変更する場合のメッシュ（オリジナル）
+		shared_ptr<MeshResource> m_OriginalMeshResource;
+		//オリジナルメッシュを使うかどうか
+		bool m_UseOriginalMeshResource;
+		///テクスチャリソース
+		weak_ptr<TextureResource> m_TextureResource;
+		///エミッシブ色
+		Col4 m_Emissive;
+		/// デフューズ色
+		Col4 m_Diffuse;
+		/// スペキュラー色
+		Col4 m_Specular;
+		///影を投影するかどうか
+		bool m_OwnShadowActive;
+		//モデルに入っているDiffuseを使うかどうか
+		bool m_ModelDiffusePriority;
+		//モデルに入っているEmissiveを使うかどうか
+		bool m_ModelEmissivePriority;
+		//モデルに入っているテクスチャを使うかどうか
+		bool m_ModelTextureEnabled;
+		///Instance描画用
+		///Instance最大値
+		size_t m_MaxInstance;
+		/// 行列用の頂点バッファ
+		ComPtr<ID3D11Buffer> m_MatrixBuffer;
+		///行列の配列
+		vector<Mat4x4> m_MatrixVec;
+		//行列を自動クリアするかどうか
+		bool m_AutoClearMatrixVec;
+		AdvSmBaseDraw::Impl() :
+			m_UseOriginalMeshResource(false),
+			m_Emissive(0, 0, 0, 0),
+			m_Diffuse(1.0f, 1.0f, 1.0f, 1.0f),
+			m_Specular(0, 0, 0, 0),
+			m_OwnShadowActive(false),
+			m_ModelDiffusePriority(false),
+			m_ModelEmissivePriority(false),
+			m_ModelTextureEnabled(true),
+			m_MaxInstance(2000),
+			m_AutoClearMatrixVec(false)
+		{}
+	};
+
+	AdvSmBaseDraw::AdvSmBaseDraw(const shared_ptr<GameObject>& GameObjectPtr) :
+		DrawComponent(GameObjectPtr),
+		pImpl(new Impl())
+	{
+		//パイプラインステートをデフォルトの３D
+		SetBlendState(BlendState::Opaque);
+		SetDepthStencilState(DepthStencilState::Default);
+		SetRasterizerState(RasterizerState::CullBack);
+		SetSamplerState(SamplerState::LinearClamp);
+	}
+
+	AdvSmBaseDraw::~AdvSmBaseDraw() {}
+
+	void AdvSmBaseDraw::SetConstants(AdvSimpleConstants& SmCb, const MeshPrimData& data) {
+		//行列の定義
+		auto PtrTrans = GetGameObject()->GetComponent<Transform>();
+		//行列の定義
+		Mat4x4 World, ViewMat, ProjMat;
+		//ワールド行列の決定
+		if (data.m_UseMeshToTransformMatrix) {
+			World = data.m_MeshToTransformMatrix * GetMeshToTransformMatrix();
+			World *= PtrTrans->GetWorldMatrix();
+		}
+		else {
+			World = GetMeshToTransformMatrix() * PtrTrans->GetWorldMatrix();
+		}
+		//転置する
+		World.transpose();
+		//カメラを得る
+		auto CameraPtr = GetGameObject()->OnGetDrawCamera();
+		//ビューと射影行列を得る
+		ViewMat = CameraPtr->GetViewMatrix();
+		//転置する
+		ViewMat.transpose();
+		//転置する
+		ProjMat = CameraPtr->GetProjMatrix();
+		ProjMat.transpose();
+		SmCb.World = World;
+		SmCb.View = ViewMat;
+		SmCb.Projection = ProjMat;
+		//エミッシブ
+		SmCb.Emissive = GetEmissive();
+		//デフィーズ
+		SmCb.Diffuse = GetDiffuse();
+		//スペキュラー
+		SmCb.Specular = GetSpecular();
+
+		SmCb.EyePos = CameraPtr->GetEye();
+		SmCb.EyePos.w = 1.0f;
+		//影用
+		if (GetOwnShadowActive()) {
+			//Vec3 CalcLightDir = -1.0 * StageLight.m_Directional;
+			//Vec3 LightAt = CameraPtr->GetAt();
+			//Vec3 LightEye = CalcLightDir;
+			//LightEye *= Shadowmap::GetLightHeight();
+			//LightEye = LightAt + LightEye;
+			//SmCb.LightPos = LightEye;
+			//SmCb.LightPos.w = 1.0f;
+			//Mat4x4 LightView, LightProj;
+			////ライトのビューと射影を計算
+			//LightView = XMMatrixLookAtLH(LightEye, LightAt, Vec3(0, 1.0f, 0));
+			//LightProj = XMMatrixOrthographicLH(Shadowmap::GetViewWidth(), Shadowmap::GetViewHeight(),
+			//	Shadowmap::GetLightNear(), Shadowmap::GetLightFar());
+			//SmCb.LightView = transpose(LightView);
+			//SmCb.LightProjection = transpose(LightProj);
+		}
+		//ボーンの設定
+		size_t BoneSz = pImpl->m_LocalBonesMatrix.size();
+		if (BoneSz > 0) {
+			UINT cb_count = 0;
+			for (size_t b = 0; b < BoneSz; b++) {
+				Mat4x4 mat = pImpl->m_LocalBonesMatrix[b];
+				mat.transpose();
+				SmCb.Bones[cb_count] = ((XMMATRIX)mat).r[0];
+				SmCb.Bones[cb_count + 1] = ((XMMATRIX)mat).r[1];
+				SmCb.Bones[cb_count + 2] = ((XMMATRIX)mat).r[2];
+				cb_count += 3;
+			}
+		}
+		else if (pImpl->m_MultiLocalBonesMatrix.size() > data.m_MultiMeshIndex) {
+			//マルチメッシュのボーンがあった
+			//ボーンの設定
+			BoneSz = pImpl->m_MultiLocalBonesMatrix[data.m_MultiMeshIndex].size();
+			if (BoneSz > 0) {
+				UINT cb_count = 0;
+				for (size_t b = 0; b < BoneSz; b++) {
+					Mat4x4 mat = pImpl->m_MultiLocalBonesMatrix[data.m_MultiMeshIndex][b];
+					mat.transpose();
+					SmCb.Bones[cb_count] = ((XMMATRIX)mat).r[0];
+					SmCb.Bones[cb_count + 1] = ((XMMATRIX)mat).r[1];
+					SmCb.Bones[cb_count + 2] = ((XMMATRIX)mat).r[2];
+					cb_count += 3;
+				}
+			}
+		}
+		SmCb.ActiveFlg.y = CameraPtr->GetFar();
+		SmCb.ActiveFlg.z = CameraPtr->GetNear();
+	}
+
+	bool AdvSmBaseDraw::IsOriginalMeshUse() const {
+		return pImpl->m_UseOriginalMeshResource;
+	}
+
+	void AdvSmBaseDraw::SetOriginalMeshUse(bool b) {
+		pImpl->m_UseOriginalMeshResource = b;
+	}
+
+	shared_ptr<MeshResource> AdvSmBaseDraw::GetOriginalMeshResource() const {
+		return pImpl->m_OriginalMeshResource;
+	}
+
+	void AdvSmBaseDraw::SetOriginalMeshResource(const shared_ptr<MeshResource>& MeshRes) {
+		pImpl->m_OriginalMeshResource = MeshRes;
+	}
+
+	shared_ptr<MeshResource> AdvSmBaseDraw::GetMeshResource() const {
+		if (IsOriginalMeshUse()) {
+			return pImpl->m_OriginalMeshResource;
+		}
+		else
+		{
+			auto shptr = pImpl->m_MeshResource.lock();
+			if (shptr) {
+				return shptr;
+			}
+		}
+		return nullptr;
+	}
+
+	void AdvSmBaseDraw::SetMeshResource(const shared_ptr<MeshResource>& MeshRes) {
+		pImpl->m_MeshResource = MeshRes;
+	}
+
+	void AdvSmBaseDraw::SetMeshResource(const wstring& MeshKey) {
+		this->SetMeshResource(App::GetApp()->GetResource<MeshResource>(MeshKey));
+	}
+
+	void AdvSmBaseDraw::SetTextureResource(const shared_ptr<TextureResource>& TextureRes) {
+		pImpl->m_TextureResource = TextureRes;
+	}
+
+	void AdvSmBaseDraw::SetTextureResource(const wstring& TextureKey) {
+		this->SetTextureResource(App::GetApp()->GetResource<TextureResource>(TextureKey));
+	}
+
+	shared_ptr<TextureResource> AdvSmBaseDraw::GetTextureResource() const {
+		//テクスチャがなければnullを返す
+		auto shptr = pImpl->m_TextureResource.lock();
+		if (shptr) {
+			return shptr;
+		}
+		return nullptr;
+	}
+
+	Col4 AdvSmBaseDraw::GetEmissive() const {
+		return pImpl->m_Emissive;
+	}
+
+	void AdvSmBaseDraw::SetEmissive(const Col4& col) {
+		pImpl->m_Emissive = col;
+	}
+
+	Col4 AdvSmBaseDraw::GetDiffuse() const {
+		return pImpl->m_Diffuse;
+	}
+
+	void AdvSmBaseDraw::SetDiffuse(const Col4& col) {
+		pImpl->m_Diffuse = col;
+	}
+
+	Col4 AdvSmBaseDraw::GetSpecular() const {
+		return pImpl->m_Specular;
+	}
+
+	void AdvSmBaseDraw::SetSpecular(const Col4& col) {
+		pImpl->m_Specular = col;
+	}
+
+	bool AdvSmBaseDraw::GetOwnShadowActive() const {
+		return pImpl->m_OwnShadowActive;
+	}
+
+	void AdvSmBaseDraw::SetOwnShadowActive(bool b) {
+		pImpl->m_OwnShadowActive = b;
+	}
+}
---
+++ HideRecovery/GameSources/Watanabe/Shader/AdvSmBaseDraw.h
@@ -0,0 +1,231 @@
+﻿#pragma once
+#include "stdafx.h"
+
+namespace basecross {
+	struct AdvSimpleConstants {
+		// ワールド行列
+		Mat4x4 World;
+		// ビュー行列
+		Mat4x4 View;
+		// 射影行列
+		Mat4x4 Projection;
+		// エミッシブ色
+		Col4 Emissive;
+		// デフューズ色
+		Col4 Diffuse;
+		// スペキュラー
+		Col4 Specular;
+		// テクスチャ=xがアクティブかどうか
+		Vec4 ActiveFlg;
+		// Eyeの位置
+		Vec4 EyePos;
+		// Bone配列
+		Vec4 Bones[3 * 72];
+		AdvSimpleConstants() {
+			memset(this, 0, sizeof(AdvSimpleConstants));
+			Diffuse = Col4(1.0f, 1.0f, 1.0f, 1.0f);
+		};
+	};
+
+	class AdvSmBaseDraw : public DrawComponent {
+	protected:
+		explicit AdvSmBaseDraw(const shared_ptr<GameObject>& GameObjectPtr);
+
+		virtual ~AdvSmBaseDraw();
+
+		void SetConstants(AdvSimpleConstants& SmCb, const MeshPrimData& data);
+
+		template <typename T_VShader, typename T_PShader>
+		void DrawStatic(const MeshPrimData& data) {
+			auto Dev = App::GetApp()->GetDeviceResources();
+			auto pD3D11DeviceContext = Dev->GetD3DDeviceContext();
+			auto RenderState = Dev->GetRenderState();
+			// NULLのシェーダリソースの準備
+			ID3D11ShaderResourceView* pNull[D3D11_COMMONSHADER_INPUT_RESOURCE_SLOT_COUNT] = { nullptr };
+			// サンプラーの準備
+			ID3D11SamplerState* pNullSR[D3D11_COMMONSHADER_SAMPLER_SLOT_COUNT] = { nullptr };
+			// 各オブジェクト共通処理
+			// シェーダの設定
+			// 頂点シェーダ
+			pD3D11DeviceContext->VSSetShader(T_VShader::GetPtr()->GetShader(), nullptr, 0);
+			// インプットレイアウトの設定
+			pD3D11DeviceContext->IASetInputLayout(T_VShader::GetPtr()->GetInputLayout());
+			// ピクセルシェーダ
+			pD3D11DeviceContext->PSSetShader(T_PShader::GetPtr()->GetShader(), nullptr, 0);
+			// 個別処理
+			AdvSimpleConstants SmCb;
+			// コンスタントバッファの作成
+			SetConstants(SmCb, data);
+			// テクスチャ
+			auto shTex = GetTextureResource();
+			if (shTex) {
+				// テクスチャがある
+				SmCb.ActiveFlg.x = 1;
+			}
+			else {
+				// 描画コンポーネントにはテクスチャがない
+				if (shTex = data.m_TextureResource.lock()) {
+					// テクスチャがある
+					SmCb.ActiveFlg.x = 1;
+				}
+				else {
+					SmCb.ActiveFlg.x = 0;
+				}
+			}
+			// コンスタントバッファの更新
+			pD3D11DeviceContext->UpdateSubresource(CBAdvSimple::GetPtr()->GetBuffer(), 0, nullptr, &SmCb, 0, 0);
+			// コンスタントバッファの設定
+			ID3D11Buffer* pConstantBuffer = CBAdvSimple::GetPtr()->GetBuffer();
+			// 頂点シェーダに渡す
+			pD3D11DeviceContext->VSSetConstantBuffers(0, 1, &pConstantBuffer);
+			// ピクセルシェーダに渡す
+			pD3D11DeviceContext->PSSetConstantBuffers(0, 1, &pConstantBuffer);
+
+			// ストライドとオフセット
+			UINT stride = data.m_NumStride;
+			UINT offset = 0;
+
+			// 描画方法のセット
+			pD3D11DeviceContext->IASetPrimitiveTopology(data.m_PrimitiveTopology);
+			// 頂点バッファのセット
+			pD3D11DeviceContext->IASetVertexBuffers(0, 1, data.m_VertexBuffer.GetAddressOf(), &stride, &offset);
+			// インデックスバッファのセット
+			pD3D11DeviceContext->IASetIndexBuffer(data.m_IndexBuffer.Get(), DXGI_FORMAT_R16_UINT, 0);
+			// 各レンダリングステートの設定
+			// ブレンドステート
+			RenderState->SetBlendState(pD3D11DeviceContext, GetBlendState());
+			// デプスステンシルステート
+			RenderState->SetDepthStencilState(pD3D11DeviceContext, GetDepthStencilState());
+			// テクスチャとサンプラー
+			if (shTex) {
+				pD3D11DeviceContext->PSSetShaderResources(0, 1, shTex->GetShaderResourceView().GetAddressOf());
+				// サンプラーを設定
+				RenderState->SetSamplerState(pD3D11DeviceContext, GetSamplerState(), 0);
+			}
+			else {
+				// シェーダーリソースもクリア
+				pD3D11DeviceContext->PSSetShaderResources(0, D3D11_COMMONSHADER_INPUT_RESOURCE_SLOT_COUNT, pNull);
+				// サンプラーもクリア
+				RenderState->SetSamplerAllClear(pD3D11DeviceContext);
+			}
+			// 影とサンプラー
+			if (GetOwnShadowActive()) {
+				// シャドウマップのレンダラーターゲット
+				auto ShadowmapPtr = Dev->GetShadowMapRenderTarget();
+				ID3D11ShaderResourceView* pShadowSRV = ShadowmapPtr->GetShaderResourceView();
+				pD3D11DeviceContext->PSSetShaderResources(1, 1, &pShadowSRV);
+				// シャドウマップサンプラー
+				ID3D11SamplerState* pShadowSampler = RenderState->GetComparisonLinear();
+				pD3D11DeviceContext->PSSetSamplers(1, 1, &pShadowSampler);
+			}
+			// ラスタライザステートと描画
+			if (GetRasterizerState() == RasterizerState::DoubleDraw) {
+				// 透明処理用
+				// ラスタライザステート(裏描画)
+				pD3D11DeviceContext->RSSetState(RenderState->GetCullFront());
+				// 描画
+				pD3D11DeviceContext->DrawIndexed(data.m_NumIndicis, 0, 0);
+				// ラスタライザステート（表描画）
+				pD3D11DeviceContext->RSSetState(RenderState->GetCullBack());
+				// 描画
+				pD3D11DeviceContext->DrawIndexed(data.m_NumIndicis, 0, 0);
+			}
+			else {
+				RenderState->SetRasterizerState(pD3D11DeviceContext, GetRasterizerState());
+				// 描画
+				pD3D11DeviceContext->DrawIndexed(data.m_NumIndicis, 0, 0);
+			}
+		}
+
+	public:
+		/**
+		 * @brief オリジナルメッシュを使うかどうか
+		 *
+		 * @return 使う場合はtrue
+		 */
+		bool IsOriginalMeshUse() const;
+		/**
+		 * @brief オリジナルメッシュを使うかどうかを設定
+		 *
+		 * @param b オリジナルメッシュを使うかどうか
+		 */
+		void SetOriginalMeshUse(bool b);
+		/**
+		 * @brief オリジナルなメッシュリソースの取得
+		 *
+		 * @return オリジナルなメッシュリソース
+		 */
+		shared_ptr<MeshResource> GetOriginalMeshResource() const;
+		/**
+		 * @brief オリジナルなメッシュリソースの設定
+		 *
+		 * @param MeshRes メッシュリソース
+		 */
+		void SetOriginalMeshResource(const shared_ptr<MeshResource>& MeshRes);
+		/**
+		 * @brief オリジナルメッシュを作成する
+		 *
+		 * @param Vertices 頂点の配列
+		 * @param indices インデックスの配列
+		 */
+		template <typename T>
+		void CreateOriginalMesh(vector<T>& Vertices, vector<uint16_t>& indices) {
+			try {
+				auto MeshRes = MeshResource::CreateMeshResource(Vertices, indices, true);
+				SetOriginalMeshResource(MeshRes);
+			}
+			catch (...) {
+				throw;
+			}
+		}
+		/**
+		 * @brief オリジナルメッシュの頂点バッファを変更する
+		 *
+		 * @param Vertices 頂点配列
+		 */
+		template <typename T>
+		void UpdateVertices(const vector<T>& Vertices) {
+			auto MeshRes = GetOriginalMeshResource();
+			if (!MeshRes) {
+				throw BaseException(
+					L"オリジナルメッシュが作成されていません",
+					L"if (!GetOriginalMeshResource())",
+					L"StaticBaseDraw::UpdateVertices()"
+				);
+			}
+			MeshRes->UpdateVirtexBuffer(Vertices);
+		}
+
+		shared_ptr<MeshResource> GetMeshResource() const;
+
+		virtual void SetMeshResource(const shared_ptr<MeshResource>& MeshRes);
+
+		void SetMeshResource(const wstring& MeshKey);
+
+		void SetTextureResource(const shared_ptr<TextureResource>& TextureRes);
+
+		void SetTextureResource(const wstring& TextureKey);
+
+		shared_ptr<TextureResource> GetTextureResource() const;
+
+		Col4 GetEmissive() const;
+
+		void SetEmissive(const Col4& col);
+
+		Col4 GetDiffuse() const;
+
+		void SetDiffuse(const Col4& col);
+
+		Col4 GetSpecular() const;
+
+		void SetSpecular(const Col4& col);
+
+		bool GetOwnShadowActive() const;
+
+		void SetOwnShadowActive(bool b);
+
+	private:
+		struct Impl;
+		unique_ptr<Impl> pImpl;
+	};
+}
