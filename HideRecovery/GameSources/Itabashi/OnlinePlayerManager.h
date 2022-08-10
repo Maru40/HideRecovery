c548b38c218ced622cfaf244bd5c57bb7a97115c
Itabashi Oota <itaoo0430@gmail.com>
2022-08-10 16:00:29 +0900

    OnlinePlayerManagerにコメントを追加

--- HideRecovery/GameSources/Itabashi/OnlinePlayerManager.h
+++ HideRecovery/GameSources/Itabashi/OnlinePlayerManager.h
@@ -12,16 +12,33 @@ namespace Online
 	class OnlinePlayerManager : public OnlineComponent
 	{
 	public:
+		/// <summary>
+		/// プレイヤーの最大数
+		/// </summary>
 		static constexpr int MAX_PLAYER_NUM = 6;
 
 	private:
+		/// <summary>
+		/// プレイヤー番号の使用状況
+		/// </summary>
 		bool m_numberUses[MAX_PLAYER_NUM];
 
+		/// <summary>
+		/// プレイヤー配列
+		/// </summary>
 		std::vector<std::weak_ptr<PlayerObject>> m_players;
-
+		/// <summary>
+		/// shard_ptrに変換した配列を返す
+		/// </summary>
+		/// <returns></returns>
 		std::vector<std::shared_ptr<PlayerObject>> GetPlayers();
-
-		std::shared_ptr<PlayerObject> CreatePlayerObject(int playerManager, const Vec3& position);
+		/// <summary>
+		/// プレイヤーオブジェクトの生成
+		/// </summary>
+		/// <param name="playerNumber">プレイヤー番号</param>
+		/// <param name="position">生成する座標</param>
+		/// <returns></returns>
+		std::shared_ptr<PlayerObject> CreatePlayerObject(int playerNumber, const Vec3& position);
 	public:
 
 		OnlinePlayerManager(const std::shared_ptr<GameObject>& owner);
