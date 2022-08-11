aaee89aed53a1bdcd206ba83bffe685a9a247b1b
Takumi Watanabe <zeal20300166@fsgcl.jp>
2022-08-11 12:08:06 +0900

    CSVのデータ管理用クラスを追加

---
+++ HideRecovery/GameSources/Watanabe/Utility/CSVLoad.cpp
@@ -0,0 +1,48 @@
+﻿/*!
+@file   CSVLoad.cpp
+@brief  CSVのデータを管理するクラス実体
+ */
+
+#include "stdafx.h"
+#include "CSVLoad.h"
+#include "DataExtracter.h"
+
+namespace basecross {
+	// インスタンスの初期化
+	unique_ptr<CSVLoad, CSVLoad::Deleter> CSVLoad::m_instance = nullptr;
+
+	void CSVLoad::RegisterFile(const wstring& key, const wstring& filePath) {
+		CsvFile csvFile;
+		csvFile.SetFileName(filePath);
+		// 失敗したら
+		if (!csvFile.ReadCsv()) {
+			throw BaseException(
+				L"ファイルの読み込みに失敗しました。",
+				L"if (!csvFile.ReadCsv())",
+				L"void CSVLoad::RegisterFile()"
+			);
+		}
+		m_data[key] = csvFile.GetCsvVec();
+	}
+
+	vector<wstring> CSVLoad::GetData(const wstring& key)const {
+		return m_data.at(key);
+	}
+
+	bool CSVLoad::CheckData(const wstring& key) {
+		return m_data.count(key) != 0;
+	}
+
+	int CSVHelper::SearchIndexDataTarget(const vector<wstring>& data, const wstring& searchData, UINT searchCol) {
+		int index = -1;
+		for (int i = 0; i < data.size(); i++) {
+			auto tokens = DataExtracter::DelimitData(data[i]);
+			if (tokens[searchCol] == searchData) {
+				index = i;
+				break;
+			}
+		}
+		return index;
+	}
+}
+//end basecross
---
+++ HideRecovery/GameSources/Watanabe/Utility/CSVLoad.h
@@ -0,0 +1,58 @@
+﻿/*!
+@file   CSVLoad.h
+@brief  CSVのデータを管理するクラス
+ */
+
+#pragma once
+#include "stdafx.h"
+#include "BaseSingleton.h"
+
+namespace basecross {
+	// シングルトン
+	class CSVLoad :public BaseSingleton<CSVLoad> {
+	private:
+		CSVLoad() {}
+		~CSVLoad() {}
+		friend class BaseSingleton<CSVLoad>;
+	private:
+		// データ（キー,データ）
+		map<wstring, vector<wstring>> m_data;
+	public:
+		/**
+		 * @brief ファイルを登録する
+		 *
+		 * @param key キー
+		 * @param filePath ファイルパス
+		 */
+		void RegisterFile(const wstring& key, const wstring& filePath);
+
+		/**
+		 * @brief データを取得する
+		 *
+		 * @param key キー
+		 * @return キーに紐づいているデータ
+		 */
+		vector<wstring> GetData(const wstring& key)const;
+
+		/**
+		 * @brief キーが存在するか
+		 *
+		 * @param key キー
+		 * @return すでに存在している場合true
+		 */
+		bool CheckData(const wstring& key);
+	};
+
+	struct CSVHelper {
+		/**
+		 * @brief データから目的のインデックスを探す
+		 *
+		 * @param data 探すデータ
+		 * @param searchData 見つけたいデータ
+		 * @param searchCol 探す列
+		 * @return データのあるインデックス（-1の場合見つからなかった）
+		 */
+		static int SearchIndexDataTarget(const vector<wstring>& data, const wstring& searchData, UINT searchCol = 0);
+	};
+}
+//end basecross
