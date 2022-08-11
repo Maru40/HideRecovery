be312d45434b4bf670338f66bceea03d98f5e3d1
Takumi Watanabe <zeal20300166@fsgcl.jp>
2022-08-11 14:25:43 +0900

    アニメーション用の基底クラスを実装した

---
+++ HideRecovery/GameSources/Watanabe/Component/Animator.cpp
@@ -0,0 +1,10 @@
+﻿#include "stdafx.h"
+#include "Animator.h"
+
+namespace basecross {
+	Animator::Animator(const shared_ptr<GameObject>& owner)
+	{}
+
+	bool Animator::LoadMotionData(wstring data) {
+	}
+}
