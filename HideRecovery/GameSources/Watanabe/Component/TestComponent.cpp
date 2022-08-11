2e48411a2f3fd0c073a2a50aafd032140663cd3c
Takumi Watanabe <zeal20300166@fsgcl.jp>
2022-08-11 11:57:54 +0900

    任意の処理を追加できるテスト用のコンポーネントを作成

---
+++ HideRecovery/GameSources/Watanabe/Component/TestComponent.cpp
@@ -0,0 +1,19 @@
+﻿#include "stdafx.h"
+#include "TestComponent.h"
+
+namespace basecross {
+	void TestComponent::OnCreate() {
+		if (m_fOnCreate)
+			m_fOnCreate();
+	}
+	void TestComponent::OnUpdate() {
+		if (m_fOnUpdate)
+			m_fOnUpdate();
+	}
+
+	void TestComponent::SetOnCreateFunction(Func func) { m_fOnCreate = func; }
+	void TestComponent::SetOnUpdateFunction(Func func) { m_fOnUpdate = func; }
+
+	void TestComponent::ResetOnCreateFunction() { m_fOnCreate = nullptr; }
+	void TestComponent::ResetOnUpdateFunction() { m_fOnUpdate = nullptr; }
+}
