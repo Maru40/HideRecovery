15fda1731201457bec17344b9ebb50f60a35193f
Itabashi Oota <itaoo0430@gmail.com>
2022-08-14 16:32:00 +0900

    HideItemコンポーネントを、ObjectHiderとItemコンポーネントの管理クラスに変更

--- HideRecovery/GameSources/HideItem.h
+++ HideRecovery/GameSources/HideItem.h
@@ -1,34 +1,47 @@
-
+ï»¿
 /*!
 @file HideItem.h
-@brief HideItem‚È‚Ç
-’S“–FŠÛR—TŠì
+@brief HideItemãªã©
+æ‹…å½“ï¼šä¸¸å±±è£•å–œ
 */
 
 #pragma once
 #include "stdafx.h"
-
 #include "ItemBase.h"
 
 namespace basecross {
 
+	namespace Operator
+	{
+		class ObjectHider;
+	}
+
+	class Item;
+
+
 	//--------------------------------------------------------------------------------------
-	/// ‰B‚·ƒAƒCƒeƒ€
+	/// éš ã™ã‚¢ã‚¤ãƒ†ãƒ 
 	//--------------------------------------------------------------------------------------
 	class HideItem : public ItemBase
 	{
+		std::weak_ptr<Operator::ObjectHider> m_hider;
+		std::weak_ptr<Item> m_item;
+
 	public:
 		/// <summary>
-		/// ƒRƒ“ƒXƒgƒ‰ƒNƒ^
+		/// ã‚³ãƒ³ã‚¹ãƒˆãƒ©ã‚¯ã‚¿
 		/// </summary>
-		/// <param name="objPtr">‚±‚ÌƒNƒ‰ƒX‚ğŠ—L‚·‚éƒQ[ƒ€ƒIƒuƒWƒFƒNƒg</param>
+		/// <param name="objPtr">ã“ã®ã‚¯ãƒ©ã‚¹ã‚’æ‰€æœ‰ã™ã‚‹ã‚²ãƒ¼ãƒ ã‚ªãƒ–ã‚¸ã‚§ã‚¯ãƒˆ</param>
 		HideItem(const std::shared_ptr<GameObject>& objPtr);
 
 		void OnCreate() override;
 
 	private:
 
+	public:
 
+		std::shared_ptr<Operator::ObjectHider> GetObjectHider() const { return m_hider.lock(); }
+		std::shared_ptr<Item> GetItem() const { return m_item.lock(); }
 	};
 
 }
