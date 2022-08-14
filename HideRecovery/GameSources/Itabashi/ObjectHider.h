2fd6431a2ab5da925c247d2462b5390c8286f9be
Itabashi Oota <itaoo0430@gmail.com>
2022-08-14 16:27:54 +0900

    オブジェクトに隠れる機能を与えるコンポーネントを作成

---
+++ HideRecovery/GameSources/Itabashi/ObjectHider.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+
+namespace basecross
+{
+namespace Operator
+{
+	class ObjectHider : public Component
+	{
+		std::weak_ptr<Transform> m_transform;
+
+	public:
+
+		ObjectHider(const std::shared_ptr<GameObject>& owner);
+
+		void OnCreate() override;
+
+		void Hide();
+		void Hide(const Vec3& position);
+
+		void Appear();
+		void Appear(const Vec3& position);
+	};
+}
+}
