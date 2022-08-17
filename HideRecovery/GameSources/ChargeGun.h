343c054ac4e125ee08a2a40d33a23a7a3203a777
Maru40 <umeda.4072@gmail.com>
2022-08-17 09:26:15 +0900

    チャージガンの生成

---
+++ HideRecovery/GameSources/ChargeGun.h
@@ -0,0 +1,32 @@
+
+/*!
+@file ChargeGun.h
+@brief ChargeGunなど
+担当：丸山裕喜
+*/
+
+#pragma once
+#include "stdafx.h"
+
+#include "WeponBase.h"
+
+namespace basecross {
+	//--------------------------------------------------------------------------------------
+	///	前方宣言
+	//--------------------------------------------------------------------------------------
+	class ChargeBulletObject;
+
+	//--------------------------------------------------------------------------------------
+	///	チャージ銃
+	//--------------------------------------------------------------------------------------
+	class ChargeGun : public WeponBase<ChargeBulletObject>
+	{
+	public:
+		ChargeGun(const std::shared_ptr<GameObject>& objPtr);
+
+		void OnUpdate() override;
+
+		void Shot(const Vec3& direct) override;
+	};
+
+}
