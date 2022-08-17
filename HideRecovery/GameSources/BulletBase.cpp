7ba8dc2346b4cd24cf232c623b7d0afa2a73ef97
Maru40 <umeda.4072@gmail.com>
2022-08-17 09:28:34 +0900

    Bulletの基底クラスの生成

--- HideRecovery/GameSources/BulletBase.cpp
+++ HideRecovery/GameSources/BulletBase.cpp
@@ -1,7 +1,7 @@
 
 /*!
-@file StatorBase.cpp
-@brief StatorBaseのクラス実体
+@file BulletBase.cpp
+@brief BulletBaseのクラス実体
 担当：丸山裕喜
 */
 
@@ -11,9 +11,33 @@
 #include "BulletBase.h"
 
 namespace basecross {
+	//--------------------------------------------------------------------------------------
+	///	弾丸の基底クラスのパラメータ
+	//--------------------------------------------------------------------------------------
+
+	BulletBase_Parametor::BulletBase_Parametor() :
+		moveDirect(Vec3::Forward()),
+		moveSpeed(30.0f),
+		maxRange(30.0f)
+	{}
+
+	//--------------------------------------------------------------------------------------
+	///	弾丸の基底クラス
+	//--------------------------------------------------------------------------------------
 
 	BulletBase::BulletBase(const std::shared_ptr<GameObject>& objPtr)
 		:Component(objPtr)
 	{}
 
+	void BulletBase::OnUpdate() {
+		MoveUpdate();
+	}
+
+	void BulletBase::MoveUpdate() {
+		auto delta = App::GetApp()->GetElapsedTime();
+
+		auto position = transform->GetPosition();
+		position += GetMoveDirect() * m_param.moveSpeed * delta;
+		transform->SetPosition(position);
+	}
 }
--- HideRecovery/GameSources/BulletBase.h
+++ HideRecovery/GameSources/BulletBase.h
@@ -10,16 +10,35 @@
 namespace basecross {
 
 	//--------------------------------------------------------------------------------------
+	///	弾丸の基底クラスのパラメータ
+	//--------------------------------------------------------------------------------------
+	struct BulletBase_Parametor {
+		Vec3 moveDirect;	//移動方向
+		float moveSpeed;	//移動スピード
+		float maxRange;		//移動最大距離
+
+		BulletBase_Parametor();
+	};
+
+	//--------------------------------------------------------------------------------------
 	///	弾丸の基底クラス
 	//--------------------------------------------------------------------------------------
 	class BulletBase : public Component
 	{
+	public:
+		using Parametor = BulletBase_Parametor;
+
+	private:
+		Parametor m_param;	//パラメータ
+
 	public:
 		/// <summary>
 		/// コンストラクタ
 		/// </summary>
 		/// <param name="objPtr">このクラスを所有するゲームオブジェクト</param>
 		BulletBase(const std::shared_ptr<GameObject>& objPtr);
+
+		void OnUpdate() override;
 
 		/// <summary>
 		/// 撃つ処理
@@ -31,6 +50,45 @@ namespace basecross {
 		/// 撃つ処理が終了したタイミングで呼ぶ処理
 		/// </summary>
 		virtual void ShotEnd() {}
+
+	private:
+		/// <summary>
+		/// 移動更新
+		/// </summary>
+		void MoveUpdate();
+
+	public:
+		//--------------------------------------------------------------------------------------
+		///	アクセッサ
+		//--------------------------------------------------------------------------------------
+
+		void SetParametor(const Parametor& parametor) noexcept { m_param = parametor; }
+
+		Parametor GetParametor() const noexcept { return m_param; }
+
+		/// <summary>
+		/// 移動方向の設定
+		/// </summary>
+		/// <param name="direct">移動方向</param>
+		void SetMoveDirect(const Vec3& direct) noexcept { m_param.moveDirect = direct; }
+
+		/// <summary>
+		/// 移動方向の取得
+		/// </summary>
+		/// <returns>移動方向</returns>
+		Vec3 GetMoveDirect() const noexcept { return m_param.moveDirect; }
+
+		/// <summary>
+		/// 移動最大距離の設定
+		/// </summary>
+		/// <param name="range">移動最大距離</param>
+		void SetMaxRange(const float range) noexcept { m_param.maxRange = range; }
+
+		/// <summary>
+		/// 移動最大距離の取得
+		/// </summary>
+		/// <returns>移動最大距離</returns>
+		float GetMaxRange() const noexcept { return m_param.maxRange; }
 	};
 
 }
