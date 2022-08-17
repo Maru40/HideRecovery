2d9c6c4239ad91116eabd44add9622a848c4de30
Takumi Watanabe <zeal20300166@fsgcl.jp>
2022-08-17 13:45:52 +0900

    各チーム用の自陣オブジェクトを作成した

---
+++ HideRecovery/GameSources/Watanabe/StageObject/OwnArea.cpp
@@ -0,0 +1,37 @@
+﻿#include "stdafx.h"
+#include "OwnArea.h"
+#include "../Utility/DataExtracter.h"
+
+namespace basecross {
+	OwnArea::OwnArea(const shared_ptr<Stage>& stage)
+		:StageObjectBase(stage, L"OwnArea")
+	{}
+	OwnArea::OwnArea(const shared_ptr<Stage>& stage, const wstring& line)
+		: StageObjectBase(stage, L"OwnArea")
+	{
+		vector<wstring> tokens = DataExtracter::DelimitData(line);
+		size_t nextIndex = DataExtracter::TransformDataExtraction(tokens, m_transformData);
+
+		m_areaRadius = (float)_wtof(tokens[nextIndex + 0].c_str());
+
+		wstring teamType = tokens[nextIndex + 1];
+		if (teamType == L"East") {
+			m_team = Team::East;
+		}
+		else if (teamType == L"West") {
+			m_team = Team::West;
+		}
+		else {
+			throw BaseException(
+				L"Teamが不正な値です。",
+				L"Team : " + teamType,
+				L"OwnArea::OwnArea()"
+			);
+		}
+	}
+
+	void OwnArea::OnCreate() {
+	}
+	void OwnArea::OnUpdate() {
+	}
+}
---
+++ HideRecovery/GameSources/Watanabe/StageObject/OwnArea.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+#include "stdafx.h"
+#include "StageObjectBase.h"
+#include "PlayerSpawnPoint.h"
+
+namespace basecross {
+	class OwnArea :public StageObjectBase {
+		float m_areaRadius;
+		Team m_team;
+	public:
+		OwnArea(const shared_ptr<Stage>& stage);
+		OwnArea(const shared_ptr<Stage>& stage, const wstring& line);
+
+		void OnCreate()override;
+		void OnUpdate()override;
+	};
+}
