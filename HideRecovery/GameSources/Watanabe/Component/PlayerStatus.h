88e81df8443f1e765a999e565d3888b17acc5faf
Takumi Watanabe <zeal20300166@fsgcl.jp>
2022-08-17 10:28:33 +0900

    プレイヤーのステータス用のコンポーネントを作成した

---
+++ HideRecovery/GameSources/Watanabe/Component/PlayerStatus.h
@@ -0,0 +1,34 @@
+﻿#pragma once
+#include "stdafx.h"
+
+namespace basecross {
+	struct Status {
+		int hp;
+		const int maxHp;
+		float skillGauge;
+
+		Status(int maxHp)
+			:hp(maxHp), maxHp(maxHp), skillGauge(0)
+		{}
+
+		void Reset() {
+			hp = maxHp;
+			skillGauge = 0;
+		}
+	};
+
+	class PlayerStatus :public Component {
+		Status m_status;
+	public:
+		PlayerStatus(const shared_ptr<GameObject>& owner);
+
+		void OnCreate()override;
+		void OnUpdate()override {}
+		void OnDraw()override {}
+
+		void AddDamage(int damage);
+		bool IsDead();
+
+		void Respawn();
+	};
+}
