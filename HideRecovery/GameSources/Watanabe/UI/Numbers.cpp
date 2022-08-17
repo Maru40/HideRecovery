e9e1f4256abd855ad94af3b8d28fd2c0ee414c91
Takumi Watanabe <zeal20300166@fsgcl.jp>
2022-08-17 15:50:04 +0900

    数字表示用のUIクラスを実装した

---
+++ HideRecovery/GameSources/Watanabe/UI/Numbers.cpp
@@ -0,0 +1,23 @@
+﻿#include "stdafx.h"
+#include "Numbers.h"
+
+namespace basecross {
+	NumberSprite::NumberSprite(const shared_ptr<Stage>& stage)
+		:GameObject(stage), m_number(0)
+	{}
+
+	void NumberSprite::OnCreate() {
+	}
+
+	void NumberSprite::SetValue(int number) {
+	}
+
+	Numbers::Numbers(const shared_ptr<Stage>& stage)
+		:GameObject(stage)
+	{}
+
+	void Numbers::OnCreate() {
+	}
+	void Numbers::SetNumber(int num) {
+	}
+}
---
+++ HideRecovery/GameSources/Watanabe/UI/Numbers.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+#include "stdafx.h"
+
+namespace basecross {
+	class NumberSprite :public GameObject {
+		int m_number;
+	public:
+		NumberSprite(const shared_ptr<Stage>& stage);
+
+		void OnCreate()override;
+		void SetValue(int number);
+	};
+
+	class Numbers :public GameObject {
+		vector<shared_ptr<NumberSprite>> m_numbers;
+		int m_numDigits;
+	public:
+		Numbers(const shared_ptr<Stage>& stage);
+
+		void OnCreate()override;
+		void SetNumber(int num);
+	};
+}
