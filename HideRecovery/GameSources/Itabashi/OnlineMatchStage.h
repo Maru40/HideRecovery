67e6ab8307588e0db9a782b456c879ac6764dead
Itabashi Oota <itaoo0430@gmail.com>
2022-08-17 13:35:00 +0900

    オンラインマッチング用のステージを作成

---
+++ HideRecovery/GameSources/Itabashi/OnlineMatchStage.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+#include "stdafx.h"
+
+namespace basecross
+{
+	class OnlineMatchStage : public Stage
+	{
+	public:
+		void OnCreate() override;
+
+		void OnUpdate() override;
+	};
+}
