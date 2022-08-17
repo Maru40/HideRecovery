ebd66a1d6511462855f0688e8ccb6dc1e7a5e8c7
Maru40 <umeda.4072@gmail.com>
2022-08-17 22:55:09 +0900

    PlayerSpawnPointObjectでPlayerSpawnPointをアタッチ

--- HideRecovery/GameSources/Watanabe/StageObject/PlayerSpawnPointObject.cpp
+++ HideRecovery/GameSources/Watanabe/StageObject/PlayerSpawnPointObject.cpp
@@ -8,6 +8,8 @@
 #include "../Utility/DataExtracter.h"
 #include "../DebugClass/Debug.h"
 
+#include "Maruyama/Player/Component/PlayerSpawnPoint.h"
+
 namespace basecross {
 	PlayerSpawnPointObject::PlayerSpawnPointObject(const shared_ptr<Stage>& stage)
 		:StageObjectBase(stage, L"PlayerSpawnPoint"), m_id(0), m_team(Team::East)
@@ -37,6 +39,8 @@ namespace basecross {
 
 	void PlayerSpawnPointObject::OnCreate() {
 		Debug::GetInstance()->Log(L"Create PlayerSpawnPoint");
+
+		AddComponent<PlayerSpawnPoint>(PlayerSpawnPoint::Parametor(m_team, m_id));
 	}
 	void PlayerSpawnPointObject::OnUpdate() {
 	}
