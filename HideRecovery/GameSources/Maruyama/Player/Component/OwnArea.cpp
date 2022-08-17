4f87e8269881771684ac6d4351a6148f154ed0f5
Maru40 <umeda.4072@gmail.com>
2022-08-17 22:22:30 +0900

    OwnAreaの生成

---
+++ HideRecovery/GameSources/Maruyama/Player/Component/OwnArea.cpp
@@ -0,0 +1,71 @@
+
+/*!
+@file OwnArea.cpp
+@brief OwnAreaクラス実体
+担当：丸山裕喜
+*/
+
+#include "stdafx.h"
+#include "Project.h"
+
+#include "OwnArea.h"
+
+#include "Maruyama/Interface/I_TeamMember.h"
+
+#include "MaruUtility.h"
+
+namespace basecross {
+
+	//--------------------------------------------------------------------------------------
+	/// 自陣エリアのパラメータ
+	//--------------------------------------------------------------------------------------
+
+	OwnArea_Parametor::OwnArea_Parametor(const Team& team, const float radius):
+		team(team),
+		radius(radius)
+	{}
+
+	//--------------------------------------------------------------------------------------
+	/// 自陣エリア本体
+	//--------------------------------------------------------------------------------------
+
+	OwnArea::OwnArea(const std::shared_ptr<GameObject>& objPtr, const Parametor& parametor) :
+		Component(objPtr),
+		m_param(parametor)
+	{}
+
+	void OwnArea::OnCreate() {
+		
+	}
+
+	//--------------------------------------------------------------------------------------
+	/// アクセッサ
+	//--------------------------------------------------------------------------------------
+
+	void OwnArea::AddMember(const std::shared_ptr<I_TeamMember>& member) {
+		member->SetOwnArea(GetThis<OwnArea>());
+		m_members.push_back(member);
+	}
+
+	bool OwnArea::RemoveMember(const std::shared_ptr<I_TeamMember>& member) {
+		auto iter = m_members.begin();
+		while (iter != m_members.end()) {
+			if ((*iter).lock() == member) {
+				iter = m_members.erase(iter);
+				return true;
+			}
+			iter++;
+		}
+
+		return false;
+	}
+
+	bool OwnArea::IsInArea(const std::shared_ptr<I_TeamMember>& member) {
+		return IsInArea(member->GetGameObject());
+	}
+
+	bool OwnArea::IsInArea(const std::shared_ptr<GameObject>& member) {
+		auto toObjectVec = maru::Utility::CalcuToTargetVec(GetGameObject(), member);
+		return toObjectVec.length() < GetRadius();	//半径内ならtrue
+	}
+}
