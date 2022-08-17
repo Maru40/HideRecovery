b0256adb08f859fcdd6433c60db2640ff2b38717
Itabashi Oota <itaoo0430@gmail.com>
2022-08-17 13:35:17 +0900

    オンラインマッチング用のコンポーネントを作成

---
+++ HideRecovery/GameSources/Itabashi/OnlineMatching.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+#include "stdafx.h"
+#include "OnlineManager.h"
+
+namespace basecross
+{
+namespace Online
+{
+	class OnlineMatching : public OnlineMatching
+	{
+	public:
+	};
+}
+}
