87077c6670dd552c656f5e7ab379cc3bcca312fc
Maru40 <umeda.4072@gmail.com>
2022-08-17 22:54:46 +0900

    PlayerSpawnPointの生成

---
+++ HideRecovery/GameSources/Maruyama/Player/Component/PlayerSpawnPoint.cpp
@@ -0,0 +1,25 @@
+
+/*!
+@file PlayerSpawnPoint.cpp
+@brief PlayerSpawnPointクラス実体
+担当：丸山裕喜
+*/
+
+#include "stdafx.h"
+#include "Project.h"
+
+#include "PlayerSpawnPoint.h"
+
+namespace basecross {
+
+	PlayerSpawnPoint_Parametor::PlayerSpawnPoint_Parametor(const Team& team, const int id) :
+		team(team),
+		id(id)
+	{}
+
+	PlayerSpawnPoint::PlayerSpawnPoint(const std::shared_ptr<GameObject>& objPtr, const Parametor& parametor) :
+		Component(objPtr),
+		m_param(parametor)
+	{}
+
+}
