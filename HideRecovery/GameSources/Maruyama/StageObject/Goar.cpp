b07dd13cccef16f96fe8268916c3b1ab4a4c665b
Maru40 <umeda.4072@gmail.com>
2022-08-18 09:43:26 +0900

    Goarの追加

---
+++ HideRecovery/GameSources/Maruyama/StageObject/Goar.cpp
@@ -0,0 +1,29 @@
+
+/*!
+@file Goar.cpp
+@brief Goarクラス実体
+担当：丸山裕喜
+*/
+
+
+#include "stdafx.h"
+#include "Project.h"
+
+#include "Goar.h"
+
+namespace basecross {
+
+	Goar::Goar(const std::shared_ptr<GameObject>& objPtr):
+		Component(objPtr)
+	{}
+
+	void Goar::OnCreate() {
+
+	}
+
+	void Goar::OnUpdate() {
+
+	}
+}
+
+//endbasecross
