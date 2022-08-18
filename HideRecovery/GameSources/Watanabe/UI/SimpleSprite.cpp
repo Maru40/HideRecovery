daedf881aa6bd5554d61402620d2d0bb39993e19
Takumi Watanabe <zeal20300166@fsgcl.jp>
2022-08-18 15:07:46 +0900

    シンプルなスプライト表示クラスを作成した

---
+++ HideRecovery/GameSources/Watanabe/UI/SimpleSprite.cpp
@@ -0,0 +1,31 @@
+﻿#include "stdafx.h"
+#include "SimpleSprite.h"
+
+namespace basecross {
+	SimpleSprite::SimpleSprite(const shared_ptr<Stage>& stage, Type type, const wstring& key)
+		:GameObject(stage), m_type(type), m_key(key)
+	{}
+
+	void SimpleSprite::SetupTexture() {
+	}
+	void SimpleSprite::SetupSpriteData() {
+	}
+
+	void SimpleSprite::OnCreate() {
+		switch (m_type)
+		{
+		case SimpleSprite::Type::Texture:
+			SetupTexture();
+			break;
+		case SimpleSprite::Type::SpriteData:
+			SetupSpriteData();
+			break;
+		default:
+			break;
+		}
+	}
+
+	shared_ptr<PCTSpriteDraw> SimpleSprite::GetDrawComponent() {
+		return m_spriteDraw.lock();
+	}
+}
