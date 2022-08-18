c56d8a8f6d28e72e56ce039da0e04557471c6190
Maru40 <umeda.4072@gmail.com>
2022-08-18 15:02:57 +0900

    Teleportの生成

---
+++ HideRecovery/GameSources/Maruyama/Player/Component/Teleport.h
@@ -0,0 +1,23 @@
+
+/*!
+@file Teleport.h
+@brief Teleportなど
+担当：丸山裕喜
+*/
+
+#pragma once
+#include "stdafx.h"
+
+namespace basecross {
+
+	//--------------------------------------------------------------------------------------
+	/// テレポート機能
+	//--------------------------------------------------------------------------------------
+	class Teleport : public Component
+	{
+	public:
+		Teleport(const std::shared_ptr<GameObject>& objPtr);
+
+	};
+
+}
