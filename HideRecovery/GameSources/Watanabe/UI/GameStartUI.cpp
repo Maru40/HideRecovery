01768f36b5483d6c7247cc1380a0d3c24eda5682
Takumi Watanabe <zeal20300166@fsgcl.jp>
2022-08-18 15:20:34 +0900

    ゲーム開始時の演出を行うクラスを作成した

---
+++ HideRecovery/GameSources/Watanabe/UI/GameStartUI.cpp
@@ -0,0 +1,13 @@
+﻿#include "stdafx.h"
+#include "GameStartUI.h"
+
+namespace basecross {
+	GameStartUI::GameStartUI(const shared_ptr<Stage>& stage)
+		:GameObject(stage)
+	{}
+
+	void GameStartUI::OnCreate() {
+	}
+	void GameStartUI::OnUpdate() {
+	}
+}
