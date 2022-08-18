5e290760a0b146e347d5805e15389bf28a7cba1f
Itabashi Oota <itaoo0430@gmail.com>
2022-08-18 18:16:02 +0900

    オンラインのマッチング用コンポーネントを作成

--- HideRecovery/GameSources/Itabashi/OnlineMatching.h
+++ HideRecovery/GameSources/Itabashi/OnlineMatching.h
@@ -1,14 +1,48 @@
 ﻿#pragma once
 #include "stdafx.h"
 #include "OnlineManager.h"
+#include <unordered_map>
 
 namespace basecross
 {
 namespace Online
 {
-	class OnlineMatching : public OnlineMatching
+	class OnlineMatching : public OnlineComponent
 	{
 	public:
+		static constexpr int MAX_PLAYER_NUM = 6;
+
+		static constexpr std::uint8_t EXECUTE_UPDATE_PLAYER_NUMBERS = 101;
+		
+	private:
+
+		static int m_playerNumbers[MAX_PLAYER_NUM];
+		//static std::unordered_map<int,int> m_player
+
+		void PlayerStateUpdateEvent(int playerNumber, bool isAdd);
+
+		void ExecuteGetPlayerStateEvent(int playerNumbers[]);
+
+	public:
+		OnlineMatching(const std::shared_ptr<GameObject>& owner);
+
+		void OnCreate() override;
+
+		void OnCreateRoom() override;
+
+		void OnJoinRoom() override;
+
+		void OnJoinRoomEventAction(int playerNumber, const std::vector<int>& playerNumbers, const ExitGames::LoadBalancing::Player& player) override;
+
+		void OnLeaveRoomEventAction(int playerNumber, bool isInactive) override;
+
+		void OnCustomEventAction(int playerNumber, std::uint8_t eventCode, const std::uint8_t* bytes) override;
+
+		bool IsFullyOccupied() const;
+
+		int GetPlayerCount() const;
+
+		void Reset();
 	};
 }
 }
