8f5581d9c4c6d8259d3f3468c87302b5a724e832
Takumi Watanabe <zeal20300166@fsgcl.jp>
2022-08-18 13:30:53 +0900

    カウントダウン用のクラスを作成した

---
+++ HideRecovery/GameSources/Watanabe/UI/CountDownUI.cpp
@@ -0,0 +1,26 @@
+﻿#include "stdafx.h"
+#include "CountDownUI.h"
+
+namespace basecross {
+	CountDownUI::CountDownUI(const shared_ptr<Stage>& stage)
+		:GameObject(stage), m_startTime(3), m_presentTime(0), m_isActive(true)
+	{}
+
+	void CountDownUI::OnCreate() {
+		m_number = GetStage()->AddGameObject<NumberSprite>();
+		m_presentTime = m_startTime;
+	}
+	void CountDownUI::OnUpdate() {
+		// 無効の場合 または カウントダウンが終了時 何もしない
+		if (!m_isActive || m_presentTime <= 0.0f) {
+			SetDrawActive(false);
+			return;
+		}
+
+		float delta = App::GetApp()->GetElapsedTime();
+
+		m_number->SetValue((int)floorf(m_presentTime));
+
+		m_presentTime -= delta;
+	}
+}
---
+++ HideRecovery/GameSources/Watanabe/UI/CountDownUI.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+#include "stdafx.h"
+#include "Numbers.h"
+
+namespace basecross {
+	class CountDownUI :public GameObject {
+		// 開始時間
+		float m_startTime;
+		// 現在の時間
+		float m_presentTime;
+		// カウントダウンが有効か
+		bool m_isActive;
+		shared_ptr<NumberSprite> m_number;
+	public:
+		CountDownUI(const shared_ptr<Stage>& stage);
+
+		void OnCreate()override;
+		void OnUpdate()override;
+	};
+}
