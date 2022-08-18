838f397814d8d5a024aaacacc56c89f1e6ab1d34
Takumi Watanabe <zeal20300166@fsgcl.jp>
2022-08-18 11:33:02 +0900

    SpriteData管理クラスを作成した

---
+++ HideRecovery/GameSources/Watanabe/UI/SpriteDataManager.cpp
@@ -0,0 +1,10 @@
+﻿#include "stdafx.h"
+#include "SpriteDataManager.h"
+
+namespace basecross {
+	// インスタンスの初期化
+	unique_ptr<SpriteDataManager, SpriteDataManager::Deleter> SpriteDataManager::m_instance = nullptr;
+
+	SpriteData SpriteDataManager::GetSpriteData(const wstring name) {
+	}
+}
---
+++ HideRecovery/GameSources/Watanabe/UI/SpriteDataManager.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+#include "stdafx.h"
+#include "../Utility/BaseSingleton.h"
+
+namespace basecross {
+	struct SpriteData {
+		// 使うテクスチャのキー
+		wstring useTextureKey;
+		// 原点
+		Vec2 origin;
+		// サイズ
+		Vec2 size;
+	};
+
+	class SpriteDataManager :public BaseSingleton<SpriteDataManager> {
+		map<wstring, SpriteData> m_spriteDataMap;
+	public:
+		/// <summary>
+		/// 任意のSpriteDataを取得する
+		/// </summary>
+		/// <param name="name">識別用の名前</param>
+		/// <returns>SpriteData</returns>
+		SpriteData GetSpriteData(const wstring name);
+	private:
+		SpriteDataManager() {}
+		~SpriteDataManager() {}
+		friend class BaseSingleton<SpriteDataManager>;
+	};
+}
