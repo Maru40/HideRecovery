4ca0b1bdfbc998435644b3da191baa6ae41a0085
Itabashi Oota <itaoo0430@gmail.com>
2022-08-18 18:16:45 +0900

    OnlineMatchStageのステージ遷移用コンポーネントを作成

---
+++ HideRecovery/GameSources/Itabashi/MatchStageTransitioner.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+#include "stdafx.h"
+
+namespace basecross
+{
+namespace Online
+{
+	class OnlineMatching;
+}
+	class MatchStageTransitioner : public Component
+	{
+		std::weak_ptr<Online::OnlineMatching> m_onlineMatching;
+
+	public:
+		MatchStageTransitioner(const std::shared_ptr<GameObject>& owner);
+
+		void OnCreate() override;
+
+		void OnUpdate() override;
+	};
+}
