78793c55c0ef661a0a35690084d9864e3551ee49
Takumi Watanabe <zeal20300166@fsgcl.jp>
2022-08-20 14:53:02 +0900

    演出操作用のインターフェイスを用意した

---
+++ HideRecovery/GameSources/Watanabe/Interface/I_Performable.h
@@ -0,0 +1,28 @@
+﻿/*!
+@file   I_Performable.h
+@brief  一連の流れの演出を操作するインターフェイス
+*/
+
+#pragma once
+#include "stdafx.h"
+
+namespace basecross {
+	class I_Performable {
+	public:
+		/// <summary>
+		/// 演出開始
+		/// </summary>
+		virtual void Start() = 0;
+
+		/// <summary>
+		/// 演出のリセット
+		/// </summary>
+		virtual void Reset() = 0;
+
+		/// <summary>
+		/// 有効の切り替え
+		/// </summary>
+		/// <param name="flg">trueで有効</param>
+		virtual void SetActive(bool flg) = 0;
+	};
+}
