aab32fe1334d894e07f642ceb24f7bc2be14d1b4
Maru40 <umeda.4072@gmail.com>
2022-08-21 17:25:51 +0900

    Task_GoalAniamtionの生成

---
+++ HideRecovery/GameSources/Maruyama/Player/Component/Task_GoalAnimation.cpp
@@ -0,0 +1,19 @@
+
+/*!
+@file Task_GoalAnimation.cpp
+@brief Task_GoalAnimationクラス実体
+担当：丸山裕喜
+*/
+
+#include "stdafx.h"
+#include "Project.h"
+
+#include "Task_GoalAnimation.h"
+
+namespace basecross {
+	namespace Task {
+
+
+
+	}
+}
---
+++ HideRecovery/GameSources/Maruyama/Player/Component/Task_GoalAnimation.h
@@ -0,0 +1,17 @@
+
+/*!
+@file Task_GoalAnimation.h
+@brief Task_GoalAnimation‚È‚Ç
+’S“–FŠÛR—TŠì
+*/
+
+#pragma once
+#include "stdafx.h"
+
+namespace basecross {
+	namespace Task {
+		
+		
+
+	}
+}
