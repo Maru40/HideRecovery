61f4eef7bfaf857dece39526950f3e8a1d48a733
Maru40 <umeda.4072@gmail.com>
2022-08-22 08:42:19 +0900

    Task＿GoalAnimationでダンク後の動きを追加

--- HideRecovery/GameSources/Maruyama/Player/Component/Task_GoalAnimation.cpp
+++ HideRecovery/GameSources/Maruyama/Player/Component/Task_GoalAnimation.cpp
@@ -10,10 +10,51 @@
 
 #include "Task_GoalAnimation.h"
 
+#include "VelocityManager.h"
+#include "MaruUtility.h"
+#include "Mathf.h"
+
 namespace basecross {
 	namespace Task {
 
+		ReturnJump_Parametor::ReturnJump_Parametor() :
+			returnDirect(Vec3(0.0f)),
+			jumpRad(XMConvertToRadians(65.0f)),
+			jumpUpPower(850.0f),
+			jumpHorizontalPower(450.0f)
+		{}
 
+		ReturnJump::ReturnJump(const std::shared_ptr<GameObject>& objPtr, const std::shared_ptr<Parametor>& paramPtr) :
+			TaskNodeBase(objPtr),
+			m_param(paramPtr)
+		{}
 
+		void ReturnJump::OnStart() {
+			if (auto velocityManager = GetOwner()->GetComponent<VelocityManager>(false)) {
+				velocityManager->AddForce(CalculateJumpVec().GetNormalized() * GetParametor()->jumpHorizontalPower);
+				velocityManager->AddForce(Vec3::Up() * GetParametor()->jumpUpPower);
+			}
+		}
+
+		bool ReturnJump::OnUpdate() {
+			return true;
+		}
+
+		void ReturnJump::OnExit() {
+			if (auto gravity = GetOwner()->GetComponent<Gravity>(false)) {
+				gravity->SetGravityVerocityZero();
+				gravity->SetUpdateActive(true);
+			}
+		}
+
+		Vec3 ReturnJump::CalculateJumpVec() {
+			Vec3 direction = GetParametor()->returnDirect;
+			direction.y = 0;
+
+			Vec3 right = GetOwner()->GetComponent<Transform>()->GetRight();
+			auto resultVec = maru::Mathf::RotationRadianVec(direction, GetParametor()->jumpRad, right);
+
+			return resultVec;
+		}
 	}
 }
--- HideRecovery/GameSources/Maruyama/Player/Component/Task_GoalAnimation.h
+++ HideRecovery/GameSources/Maruyama/Player/Component/Task_GoalAnimation.h
@@ -8,10 +8,41 @@
 #pragma once
 #include "stdafx.h"
 
+#include "TaskList.h"
+
 namespace basecross {
 	namespace Task {
+
+		struct ReturnJump_Parametor {
+			Vec3 returnDirect;
+			float jumpRad;
+			float jumpUpPower;
+			float jumpHorizontalPower;
+
+			ReturnJump_Parametor();
+		};
 		
-		
+		class ReturnJump : public TaskNodeBase<GameObject>
+		{
+		public:
+			using Parametor = ReturnJump_Parametor;
+
+		private:
+			std::weak_ptr<Parametor> m_param;
+
+		public:
+			ReturnJump(const std::shared_ptr<GameObject>& objPtr, const std::shared_ptr<Parametor>& paramPtr);
+
+			void OnStart() override;
+			bool OnUpdate() override;
+			void OnExit() override;
+
+		private:
+			Vec3 CalculateJumpVec();
+
+		public:
+			std::shared_ptr<Parametor> GetParametor() const { return m_param.lock(); };
+		};
 
 	}
 }
