4ac069ed3ec8c07cbcfbdf09d1ac315065878b10
Maru40 <umeda.4072@gmail.com>
2022-08-21 14:31:44 +0900

    I_TaskListの生成

---
+++ HideRecovery/GameSources/Maruyama/Interface/I_TaskLister.h
@@ -0,0 +1,18 @@
+/*!
+@file I_TaskLister.h
+@brief I_TaskLister‚È‚Ç
+’S“–FŠÛR—TŠì
+*/
+
+#pragma once
+#include "stdafx.h"
+
+namespace basecross {
+
+	class I_TaskLister
+	{
+	public:
+		virtual ~I_TaskLister() = default;
+	};
+
+}
