3bd4540e2c5981cb263de01dede17c3e06979ab6
Maru40 <umeda.4072@gmail.com>
2022-08-22 09:13:12 +0900

    Teleportでテレポートの流れを生成

--- HideRecovery/GameSources/Maruyama/Player/Component/Teleport.cpp
+++ HideRecovery/GameSources/Maruyama/Player/Component/Teleport.cpp
@@ -10,11 +10,66 @@
 
 #include "Teleport.h"
 
+#include "Watanabe/Component/PlayerAnimator.h"
+
+#include "SingletonComponent.h"
+#include "ScreenFadeManager.h"
+#include "FadeSprite.h"
+
 namespace basecross {
 
 	Teleport::Teleport(const std::shared_ptr<GameObject>& objPtr) :
-		Component(objPtr)
+		Component(objPtr),
+		m_param(Parametor())
 	{}
+
+	void Teleport::OnLateStart() {
+		auto animator = GetGameObject()->GetComponent<PlayerAnimator>(false);
+		if (!animator) {
+			return;
+		}
+
+		auto exit = [&, animator]() {
+			auto fadeManager = ScreenFadeManager::GetInstance();
+
+			auto endEvent = [fadeManager]() {
+				if (!fadeManager) {
+					return;
+				}
+
+				fadeManager->FadeStart(FadeType::In);
+			};
+
+			if (fadeManager) {
+				fadeManager->FadeStart(FadeType::Out, endEvent);
+			}
+
+			transform->SetPosition(GetTeleportPosition());
+
+			animator->ChangePlayerAnimation(PlayerAnimationState::State::Wait);	//アニメーションの再生
+				//エフェクトの再生
+		};
+
+		//アニメーションイベントの登録
+		animator->AddAnimationEvent(
+			PlayerAnimationState::State::Dash,
+			nullptr,
+			nullptr,
+			exit
+		);
+	}
+
+	void Teleport::OnUpdate() {
+
+	}
+
+	void Teleport::StartTeleport() {
+		if (auto animator = GetGameObject()->GetComponent<PlayerAnimator>(false)) {
+				//テレポートアニメーション
+		}
+
+			//エフェクトの再生
+	}
 
 	void Teleport::OpenMap() {
 
--- HideRecovery/GameSources/Maruyama/Player/Component/Teleport.h
+++ HideRecovery/GameSources/Maruyama/Player/Component/Teleport.h
@@ -13,19 +13,58 @@ namespace basecross {
 	//--------------------------------------------------------------------------------------
 	/// テレポート機能
 	//--------------------------------------------------------------------------------------
+
+	struct Teleport_Parametor 
+	{
+		Vec3 position;	//テレポートする位置
+	};
+
+	//--------------------------------------------------------------------------------------
+	/// テレポート機能
+	//--------------------------------------------------------------------------------------
 	class Teleport : public Component
 	{
 	public:
+		using Parametor = Teleport_Parametor;
+
+	private:
+		Parametor m_param;
+
+	public:
 		Teleport(const std::shared_ptr<GameObject>& objPtr);
+
+		void OnLateStart() override;
+		void OnUpdate() override;
 
 	private:
 
 
 	public:
 		/// <summary>
+		/// テレポート開始
+		/// </summary>
+		void StartTeleport();
+
+		/// <summary>
 		/// マップを開く。
 		/// </summary>
 		void OpenMap();
+
+		//--------------------------------------------------------------------------------------
+		/// アクセッサ
+		//--------------------------------------------------------------------------------------
+
+		/// <summary>
+		/// テレポート位置の設定
+		/// </summary>
+		/// <param name="position">テレポート位置の設定</param>
+		void SetTeleportPosition(const Vec3& position) noexcept { m_param.position = position; }
+
+		/// <summary>
+		/// テレポート位置の取得
+		/// </summary>
+		/// <returns>テレポート位置</returns>
+		Vec3 GetTeleportPosition() const noexcept { return m_param.position; }
 	};
 
 }
