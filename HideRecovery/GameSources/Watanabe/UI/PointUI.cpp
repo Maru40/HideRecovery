dabd6e52495d4173d8ce618279c3e58321cb3c1b
Takumi Watanabe <zeal20300166@fsgcl.jp>
2022-08-23 10:53:08 +0900

    ポイントUIを作成した

---
+++ HideRecovery/GameSources/Watanabe/UI/PointUI.cpp
@@ -0,0 +1,13 @@
+﻿#include "stdafx.h"
+#include "PointUI.h"
+
+namespace basecross {
+	PointUI::PointUI(const shared_ptr<Stage>& stage)
+		:GameObject(stage)
+	{}
+
+	void PointUI::OnCreate() {
+	}
+	void PointUI::OnUpdate() {
+	}
+}
---
+++ HideRecovery/GameSources/Watanabe/UI/PointUI.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+#include "stdafx.h"
+#include "Numbers.h"
+
+namespace basecross {
+	class PointUI :public GameObject {
+		shared_ptr<Numbers> m_numbers;
+	public:
+		PointUI(const shared_ptr<Stage>& stage);
+
+		void OnCreate()override;
+		void OnUpdate()override;
+	};
+}
