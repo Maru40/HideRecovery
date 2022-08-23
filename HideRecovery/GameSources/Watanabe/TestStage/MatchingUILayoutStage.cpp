ade4d9bc5918bc1a17e10ef12a6efd4ecf66f133
Takumi Watanabe <zeal20300166@fsgcl.jp>
2022-08-23 17:11:32 +0900

    マッチング画面のUIレイアウト調整用のテストステージを作成した

---
+++ HideRecovery/GameSources/Watanabe/TestStage/MatchingUILayoutStage.cpp
@@ -0,0 +1,28 @@
+﻿#include "stdafx.h"
+#include "MatchingUILayoutStage.h"
+#include "../UI/UIObjects.h"
+#include "../DebugClass/Debug.h"
+
+namespace basecross {
+	void MatchingUILayoutStage::CreateViewLight() {
+		const Vec3 eye(5.0f, 5.0f, -5.0f);
+		const Vec3 at(0.0f);
+		auto PtrView = CreateView<SingleView>();
+		//ビューのカメラの設定
+		auto PtrCamera = ObjectFactory::Create<Camera>();
+		PtrView->SetCamera(PtrCamera);
+		PtrCamera->SetEye(eye);
+		PtrCamera->SetAt(at);
+		//マルチライトの作成
+		auto PtrMultiLight = CreateLight<MultiLight>();
+		//デフォルトのライティングを指定
+		PtrMultiLight->SetDefaultLighting();
+	}
+	void MatchingUILayoutStage::OnCreate() {
+		CreateViewLight();
+		AddGameObject<Debug>();
+		Debug::GetInstance()->Log(L"MatchingUILayoutStage");
+	}
+	void MatchingUILayoutStage::OnUpdate() {
+	}
+}
---
+++ HideRecovery/GameSources/Watanabe/TestStage/MatchingUILayoutStage.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+#include "stdafx.h"
+
+namespace basecross {
+	class MatchingUILayoutStage :public Stage {
+		void CreateViewLight();
+	public:
+		MatchingUILayoutStage() :Stage() {}
+
+		void OnCreate()override;
+		void OnUpdate()override;
+	};
+}
