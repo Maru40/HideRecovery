384213e6b302be59328374bc03478257131f57f3
Takumi Watanabe <zeal20300166@fsgcl.jp>
2022-08-23 09:45:49 +0900

    HPゲージ表示クラスを作成した

---
+++ HideRecovery/GameSources/Watanabe/UI/HPGaugeUI.cpp
@@ -0,0 +1,13 @@
+﻿#include "stdafx.h"
+#include "HPGaugeUI.h"
+
+namespace basecross {
+	HPGaugeUI::HPGaugeUI(const shared_ptr<Stage>& stage)
+		:GameObject(stage)
+	{}
+
+	void HPGaugeUI::OnCreate() {
+	}
+	void HPGaugeUI::OnUpdate() {
+	}
+}
---
+++ HideRecovery/GameSources/Watanabe/UI/HPGaugeUI.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+#include "stdafx.h"
+
+namespace basecross {
+	class HPGaugeUI :public GameObject {
+	public:
+		HPGaugeUI(const shared_ptr<Stage>& stage);
+
+		void OnCreate()override;
+		void OnUpdate()override;
+	};
+}
