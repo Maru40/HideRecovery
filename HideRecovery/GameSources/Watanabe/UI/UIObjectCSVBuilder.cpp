a58f58e1bd41f8fb4b2aaf4a13a635452755b198
Takumi Watanabe <zeal20300166@fsgcl.jp>
2022-08-24 10:38:15 +0900

    UI用のオブジェクトビルダーを作成した

---
+++ HideRecovery/GameSources/Watanabe/UI/UIObjectCSVBuilder.cpp
@@ -0,0 +1,38 @@
+﻿#include "stdafx.h"
+#include "UIObjectCSVBuilder.h"
+
+namespace basecross {
+	UIObjectCSVBuilder::UIObjectCSVBuilder() {}
+	UIObjectCSVBuilder::~UIObjectCSVBuilder() {}
+
+	shared_ptr<UIObjectBase> UIObjectCSVBuilder::CreateFromCSV(const wstring& name, const shared_ptr<Stage>& stage, const wstring& line) {
+		auto it = m_creatorMap.find(name);
+		if (it == m_creatorMap.end()) {
+			return nullptr;
+		}
+		else {
+			auto ptr = (*it).second->Create(stage, line);
+			return ptr;
+		}
+	}
+
+	void UIObjectCSVBuilder::Build(const shared_ptr<Stage>& stage, const wstring& fileName) {
+		try {
+			//CSVファイル
+			CsvFile GameStageCsv(fileName);
+			GameStageCsv.ReadCsv();
+			//CSVの全体の配列
+			//CSVからすべての行を抜き出す
+			auto& LineVec = GameStageCsv.GetCsvVec();
+			for (auto& v : LineVec) {
+				//トークン（カラム）の配列
+				vector<wstring> Tokens;
+				Util::WStrToTokenVector(Tokens, v, L',');
+				CreateFromCSV(Tokens[0], stage, v);
+			}
+		}
+		catch (...) {
+			throw;
+		}
+	}
+}
---
+++ HideRecovery/GameSources/Watanabe/UI/UIObjectCSVBuilder.h
@@ -0,0 +1,69 @@
+﻿#pragma once
+#include "stdafx.h"
+#include "UIObjectBase.h"
+
+namespace basecross {
+	class UIObjectCreatorBaseCSV {
+	protected:
+		explicit UIObjectCreatorBaseCSV() {}
+		virtual ~UIObjectCreatorBaseCSV() {}
+	public:
+		/// <summary>
+		/// UIオブジェクトを作成する（純粋仮想関数）
+		/// </summary>
+		/// <param name="stage">ステージ</param>
+		/// <param name="line">1行分のCSV文字列</param>
+		/// <returns>UIオブジェクトのポインタ</returns>
+		virtual shared_ptr<UIObjectBase> Create(const shared_ptr<Stage>& stage, const wstring& line) = 0;
+	};
+
+	template<typename T>
+	class UIObjectCreatorCSV : public UIObjectCreatorBaseCSV {
+	public:
+		explicit UIObjectCreatorCSV() {}
+		virtual ~UIObjectCreatorCSV() {}
+
+		/// <summary>
+		/// UIオブジェクトを作成する（仮想関数）
+		/// </summary>
+		/// <param name="StagePtr">ステージ</param>
+		/// <param name="Line">1行分のCSV文字列</param>
+		/// <returns>UIオブジェクトのポインタ</returns>
+		shared_ptr<UIObjectBase> Create(const shared_ptr<Stage>& stage, const wstring& line)override {
+			return stage->AddGameObject<T>(line);
+		}
+	};
+
+	class UIObjectCSVBuilder {
+		map<wstring, shared_ptr<UIObjectCreatorBaseCSV>> m_creatorMap;
+	public:
+		UIObjectCSVBuilder();
+		~UIObjectCSVBuilder();
+
+		/// <summary>
+		/// クリエーターを登録する
+		/// </summary>
+		/// <typeparam name="T">登録する型（ゲームオブジェクトの派生）</typeparam>
+		/// <param name="name">型の識別名</param>
+		template<typename T>
+		void Register(const wstring& name) {
+			m_creatorMap[name] = shared_ptr<UIObjectCreatorCSV<T>>(new UIObjectCreatorCSV<T>());
+		}
+
+		/// <summary>
+		/// 型の識別名を使ってUIオブジェクトを構築する
+		/// </summary>
+		/// <param name="name">型の識別名</param>
+		/// <param name="stage">所属するステージ</param>
+		/// <param name="line">1行分のCSV文字列</param>
+		/// <returns>作成したUIオブジェクト</returns>
+		shared_ptr<UIObjectBase> CreateFromCSV(const wstring& name, const shared_ptr<Stage>& stage, const wstring& line);
+
+		/// <summary>
+		/// CSVからUIオブジェクトを構築する
+		/// </summary>
+		/// <param name="stage">所属するステージ</param>
+		/// <param name="fileName">CSVファイル名</param>
+		void Build(const shared_ptr<Stage>& stage, const wstring& fileName);
+	};
+}
