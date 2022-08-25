9ce09f72700b0d6bc18fe545a171f7043cf77ae8
Takumi Watanabe <zeal20300166@fsgcl.jp>
2022-08-25 13:56:18 +0900

    他プレイヤーのHPゲージクラスを作成した

---
+++ HideRecovery/GameSources/Watanabe/BoardPoly/HPGaugeBoardPoly.cpp
@@ -0,0 +1,13 @@
+﻿#include "stdafx.h"
+#include "HPGaugeBoardPoly.h"
+
+namespace basecross {
+	HPGaugeBoardPoly::HPGaugeBoardPoly(const shared_ptr<Stage>& stage)
+		:StageObjectBase(stage, L"HPGaugeBoardPoly")
+	{}
+
+	void HPGaugeBoardPoly::OnCreate() {
+	}
+	void HPGaugeBoardPoly::OnUpdate() {
+	}
+}
