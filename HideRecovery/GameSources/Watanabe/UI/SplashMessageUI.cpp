a8d0d8b52a2f48246e2ba0524661a00a17658fa8
Takumi Watanabe <zeal20300166@fsgcl.jp>
2022-08-25 11:16:24 +0900

    スプラッシュメッセージクラスを作成した

---
+++ HideRecovery/GameSources/Watanabe/UI/SplashMessageUI.cpp
@@ -0,0 +1,23 @@
+﻿#include "stdafx.h"
+#include "SplashMessageUI.h"
+
+namespace basecross {
+	SplashMessageUI::SplashMessageUI(const shared_ptr<Stage>& stage)
+		:UIObjectBase(stage, L"SplashMessageUI")
+	{}
+
+	SplashMessageUI::SplashMessageUI(const shared_ptr<Stage>& stage, const wstring& line)
+		: UIObjectBase(stage, L"SplashMessageUI")
+	{}
+
+	void SplashMessageUI::OnCreate() {
+	}
+
+	void SplashMessageUI::SetMessage(const wstring& key) {
+	}
+
+	void SplashMessageUI::Start() {
+	}
+	void SplashMessageUI::Reset() {
+	}
+}
---
+++ HideRecovery/GameSources/Watanabe/UI/SplashMessageUI.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+#include "stdafx.h"
+#include "UIObjectBase.h"
+#include "../Interface/I_Performable.h"
+
+namespace basecross {
+	class SplashMessageUI :public UIObjectBase, public I_Performable {
+	public:
+		SplashMessageUI(const shared_ptr<Stage>& stage);
+		SplashMessageUI(const shared_ptr<Stage>& stage, const wstring& line);
+
+		void OnCreate()override;
+
+		/// <summary>
+		/// メッセージをセット
+		/// </summary>
+		/// <param name="key">キー</param>
+		void SetMessage(const wstring& key);
+
+		void Start()override;
+		void Reset()override;
+	};
+}
