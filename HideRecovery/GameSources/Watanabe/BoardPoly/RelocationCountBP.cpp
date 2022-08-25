e9a3d40ab74a70be2cc89211009f890c687ead96
Takumi Watanabe <zeal20300166@fsgcl.jp>
2022-08-25 16:04:50 +0900

    再配置までのカウントダウンクラスを作成した

---
+++ HideRecovery/GameSources/Watanabe/BoardPoly/RelocationCountBP.cpp
@@ -0,0 +1,13 @@
+﻿#include "stdafx.h"
+#include "RelocationCountBP.h"
+
+namespace basecross {
+	RelocationCountBP::RelocationCountBP(const shared_ptr<Stage>& stage)
+		:StageObjectBase(stage, L"RelocationCountBP")
+	{}
+
+	void RelocationCountBP::OnCreate() {
+	}
+	void RelocationCountBP::OnUpdate() {
+	}
+}
---
+++ HideRecovery/GameSources/Watanabe/BoardPoly/RelocationCountBP.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+#include "stdafx.h"
+#include "StageObjectBase.h"
+
+namespace basecross {
+	class RelocationCountBP :public StageObjectBase {
+	public:
+		RelocationCountBP(const shared_ptr<Stage>& stage);
+
+		void OnCreate()override;
+		void OnUpdate()override;
+	};
+}
