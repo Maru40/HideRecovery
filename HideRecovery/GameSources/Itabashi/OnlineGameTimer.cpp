8fb4533384dc19b5b3d8a1302f3a1ee02208b067
Itabashi Oota <itaoo0430@gmail.com>
2022-08-26 09:37:41 +0900

    オンライン上でのタイマー同期用コンポーネント

---
+++ HideRecovery/GameSources/Itabashi/OnlineGameTimer.cpp
@@ -0,0 +1,108 @@
+﻿#include "stdafx.h"
+#include "OnlineGameTimer.h"
+
+namespace basecross
+{
+	OnlineGameTimer::OnlineGameTimer(const std::shared_ptr<GameObject>& owner) :
+		OnlineComponent(owner),
+		m_startCheckSpan(1.0f)
+	{
+
+	}
+
+	bool OnlineGameTimer::IsStartable() const
+	{
+		return m_isGameStartable;
+	}
+
+	void OnlineGameTimer::StartCheck()
+	{
+		if (!Online::OnlineManager::GetLocalPlayer().getIsMasterClient())
+		{
+			return;
+		}
+
+		m_timeCount += App::GetApp()->GetElapsedTime();
+
+		if (m_timeCount < m_startCheckSpan)
+		{
+			return;
+		}
+
+		m_timeCount -= m_startCheckSpan;
+
+		if (m_startOkPlayerNumbers.size() >= Online::OnlineManager::GetCurrentlyJoinedRoom().getPlayerCount() - 1)
+		{
+			m_isGameStartable = true;
+
+			for (auto& m_gameStartCountFuncs : m_gameStartCountFuncs)
+			{
+				m_gameStartCountFuncs();
+			}
+
+			Online::OnlineManager::RaiseEvent(false, nullptr, 0, GAMETIMER_START_EVENT_CODE);
+
+			return;
+		}
+
+		Online::OnlineManager::RaiseEvent(false, nullptr, 0, GAMETIMER_START_CHECK_EVENT_CODE);
+	}
+
+	void OnlineGameTimer::OnCreate()
+	{
+	}
+
+	void OnlineGameTimer::OnUpdate()
+	{
+		if (m_isGameStartable)
+		{
+			return;
+		}
+
+		StartCheck();
+
+	}
+
+	void OnlineGameTimer::OnCustomEventAction(int playerNumber, std::uint8_t eventCode, const std::uint8_t* bytes)
+	{
+		if (eventCode == GAMETIMER_START_CHECK_EVENT_CODE)
+		{
+			if (IsStartable())
+			{
+				return;
+			}
+
+			auto raiseOption = ExitGames::LoadBalancing::RaiseEventOptions();
+			raiseOption.setReceiverGroup(ExitGames::Lite::ReceiverGroup::MASTER_CLIENT);
+			Online::OnlineManager::RaiseEvent(false, nullptr, 0, GAMETIMER_START_OK_EVENT_CODE, raiseOption);
+			return;
+		}
+
+		if (eventCode == GAMETIMER_START_OK_EVENT_CODE)
+		{
+			if (!Online::OnlineManager::GetLocalPlayer().getIsMasterClient())
+			{
+				return;
+			}
+
+			m_startOkPlayerNumbers.insert(playerNumber);
+		}
+
+		if (eventCode == GAMETIMER_START_EVENT_CODE)
+		{
+			m_isGameStartable = true;
+
+			for (auto& gameStartCountFunc : m_gameStartCountFuncs)
+			{
+				gameStartCountFunc();
+			}
+
+			return;
+		}
+	}
+
+	void OnlineGameTimer::OnLeaveRoomEventAction(int playerNumber, bool isInactive)
+	{
+		m_startOkPlayerNumbers.erase(playerNumber);
+	}
+}
---
+++ HideRecovery/GameSources/Itabashi/OnlineGameTimer.h
@@ -0,0 +1,46 @@
+﻿#pragma once
+#include "stdafx.h"
+#include "OnlineManager.h"
+#include <unordered_set>
+#include <functional>
+
+namespace basecross
+{
+	class OnlineGameTimer : public Online::OnlineComponent
+	{
+	public:
+		static constexpr std::uint8_t GAMETIMER_START_CHECK_EVENT_CODE = 200;
+		static constexpr std::uint8_t GAMETIMER_START_OK_EVENT_CODE = 201;
+		static constexpr std::uint8_t GAMETIMER_START_EVENT_CODE = 202;
+
+	private:
+
+		bool m_isStartable = false;
+		bool m_isGameStartable = false;
+
+		float m_timeCount = 0.0f;
+
+		float m_startCheckSpan;
+
+		std::unordered_set<int> m_startOkPlayerNumbers;
+
+		std::vector<std::function<void()>> m_gameStartCountFuncs;
+
+		bool IsStartable() const;
+
+		void StartCheck();
+
+	public:
+		OnlineGameTimer(const std::shared_ptr<GameObject>& owner);
+
+		void OnCreate() override;
+
+		void OnUpdate() override;
+
+		void OnCustomEventAction(int playerNumber, std::uint8_t eventCode, const std::uint8_t* bytes) override;
+
+		void OnLeaveRoomEventAction(int playerNumber, bool isInactive) override;
+
+		void AddGameStartCountFuncs(const std::function<void()>& gameStartCountFunc) { m_gameStartCountFuncs.push_back(gameStartCountFunc); }
+	};
+}
