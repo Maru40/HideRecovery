9cff4371c780ba0ed39d620641c509d90e27efec
Maru40 <umeda.4072@gmail.com>
2022-08-26 11:40:58 +0900

    RackObjectでCollisionSizeの変更

--- HideRecovery/GameSources/Watanabe/StageObject/RackObject.cpp
+++ HideRecovery/GameSources/Watanabe/StageObject/RackObject.cpp
@@ -23,7 +23,7 @@ namespace basecross {
 	void RackObject::OnCreate() {
 		auto col = AddComponent<CollisionObb>();
 		col->SetFixed(true);
-		const float colSize = 1.1f;
+		const float colSize = 1.2f;
 		col->SetMakedSize(colSize);
 
 		auto drawComp = AddComponent<PNTBoneModelDraw>();
@@ -33,6 +33,9 @@ namespace basecross {
 
 		// 隠せるオブジェクトにする
 		AddComponent<HidePlace>(HidePlace::Parametor(Vec3(0.0f, 0.15f, 0.0f)));
+
+		//constexpr float fScale = 0.75f;
+		//transform->SetScale(Vec3(fScale));
 
 		auto shadow = AddComponent<Shadowmap>();
 		//shadow->SetMeshResource(L"Box_Model");
