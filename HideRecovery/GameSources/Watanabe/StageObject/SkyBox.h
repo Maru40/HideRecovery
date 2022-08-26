acb777dedfb40660ab11c45b7944a1eb7b817677
Takumi Watanabe <zeal20300166@fsgcl.jp>
2022-08-26 15:19:53 +0900

    仮のスカイボックスを実装した

---
+++ HideRecovery/GameSources/Watanabe/StageObject/SkyBox.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+#include "stdafx.h"
+#include "StageObjectBase.h"
+
+namespace basecross {
+	class SkyBox :public StageObjectBase {
+	public:
+		SkyBox(const shared_ptr<Stage>& stage, const Vec3& scale);
+
+		void OnCreate()override;
+	};
+}
