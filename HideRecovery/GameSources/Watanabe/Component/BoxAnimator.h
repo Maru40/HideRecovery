a04183602e1334320e51c27ec0f7d2f3811fc2df
Takumi Watanabe <zeal20300166@fsgcl.jp>
2022-08-26 11:41:06 +0900

    Box用のアニメーターを作成した

---
+++ HideRecovery/GameSources/Watanabe/Component/BoxAnimator.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "stdafx.h"
+#include "Animator.h"
+#include "BoxAnimationState.h"
+
+namespace basecross {
+	class BoxAnimator :public Animator {
+	public:
+		BoxAnimator(const shared_ptr<GameObject>& owner);
+
+		void OnCreate() override;
+
+		/// <summary>
+		/// アニメーションの変更
+		/// </summary>
+		/// <param name="state">変更するアニメーションのステート</param>
+		void ChangeBoxAnimation(BoxAnimationState::State state);
+	};
+}
--- HideRecovery/Tool/CustomBuildStep.bat
+++ HideRecovery/Tool/CustomBuildStep.bat
@@ -1,3 +1,4 @@
 @echo off
 call MotionCSV2CEnum.exe ..\media\Models\Player\PlayerAnimation.csv ..\GameSources\Watanabe\Component
 call MotionCSV2CEnum.exe ..\media\Models\Ball\BallAnimation.csv ..\GameSources\Watanabe\Component
+call MotionCSV2CEnum.exe ..\media\Models\Box\BoxAnimation.csv ..\GameSources\Watanabe\Component
