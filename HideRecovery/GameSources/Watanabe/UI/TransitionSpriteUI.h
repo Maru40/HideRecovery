1cfacfabe8a74275d5d03817584ebbfafa76ccc4
Takumi Watanabe <zeal20300166@fsgcl.jp>
2022-08-26 13:25:51 +0900

    画面遷移用のスプライトクラスを作成した

---
+++ HideRecovery/GameSources/Watanabe/UI/TransitionSpriteUI.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+#include "stdafx.h"
+#include "UIObjectBase.h"
+#include "../Component/FadeComponent.h"
+
+namespace basecross {
+	class TransitionSpriteUI :public UIObjectBase {
+	public:
+		TransitionSpriteUI(const shared_ptr<Stage>& stage);
+
+		void OnCreate()override;
+		void OnUpdate()override {}
+
+		void FadeIn() {
+			// 画面遷移なので逆になる
+			// 画面はFadeInに対して、このオブジェクトはFadeOutする
+			GetFade()->FadeOut();
+		}
+		void FadeOut() {
+			// 上に同じく逆
+			GetFade()->FadeIn();
+		}
+		shared_ptr<FadeComponent> GetFade() {
+			return GetComponent<FadeComponent>();
+		}
+	};
+}
