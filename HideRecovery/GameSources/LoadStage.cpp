2166ff638732296620ec66a694739757a6b922e9
Takumi Watanabe <zeal20300166@fsgcl.jp>
2022-08-27 11:12:16 +0900

    キル数などのラベル用スプライトを作成

--- HideRecovery/GameSources/LoadStage.cpp
+++ HideRecovery/GameSources/LoadStage.cpp
@@ -38,6 +38,7 @@ namespace basecross
 		ResourceLoadData(L"HPGauge_TX",L"HPGauge_TX.png"),
 		ResourceLoadData(L"TitleLogo_TX",L"TitleLogo_TX.png"),
 		ResourceLoadData(L"WinOrLose_TX",L"WinOrLose_TX.png"),
+		ResourceLoadData(L"ScoreLabel_TX",L"ScoreLabel_TX.png"),
 
 		ResourceLoadData(L"FiledMap_TX", L"FiledMap_TX.png"),
 
