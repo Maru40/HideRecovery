8bc8f2661f631e9e79e2d39b7529b95687edc21c
Itabashi Oota <itaoo0430@gmail.com>
2022-08-27 16:54:43 +0900

    メインステージからリザルトに移動するコンポーネントを作成

---
+++ HideRecovery/GameSources/Itabashi/MainStageTransitioner.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+#include "stdafx.h"
+#include "OnlineManager.h"
+
+namespace basecross
+{
+namespace Online
+{
+	class MainStageTransitioner : public OnlineComponent
+	{
+	public:
+
+	private:
+
+		float m_transitionDelayTime;
+		float m_elapsedTime = 0.0f;
+
+	public:
+		MainStageTransitioner(const std::shared_ptr<GameObject>& owner);
+
+		void OnUpdate() override;
+
+		void SetTransitionDelayTime(float transitionDelayTime) { m_transitionDelayTime = transitionDelayTime; }
+		float GetTransitionDelayTime() const { return m_transitionDelayTime; }
+	};
+}
+}
