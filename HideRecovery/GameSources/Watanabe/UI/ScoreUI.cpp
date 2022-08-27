ab21ae6310f00250c9f0186a9284200b30758a75
Takumi Watanabe <zeal20300166@fsgcl.jp>
2022-08-27 11:22:56 +0900

    スコア表示UIを作成した

---
+++ HideRecovery/GameSources/Watanabe/UI/ScoreUI.cpp
@@ -0,0 +1,15 @@
+﻿#include "stdafx.h"
+#include "ScoreUI.h"
+
+namespace basecross {
+	ScoreUI::ScoreUI(const shared_ptr<Stage>& stage, int playerNumber)
+		:UIObjectBase(stage, L"ScoreUI")
+	{}
+
+	ScoreUI::ScoreUI(const shared_ptr<Stage>& stage, const wstring& line)
+		: UIObjectBase(stage, L"ScoreUI")
+	{}
+
+	void ScoreUI::OnCreate() {
+	}
+}
---
+++ HideRecovery/GameSources/Watanabe/UI/ScoreUI.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+#include "stdafx.h"
+#include "UIObjectBase.h"
+
+namespace basecross {
+	class ScoreUI :public UIObjectBase {
+	public:
+		ScoreUI(const shared_ptr<Stage>& stage, int playerNumber);
+		ScoreUI(const shared_ptr<Stage>& stage, const wstring& line);
+
+		void OnCreate()override;
+	};
+}
