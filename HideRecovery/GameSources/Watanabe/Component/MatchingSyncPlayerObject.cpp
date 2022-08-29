d438954d1f56f33db79adc2ac69e0a27de935960
Takumi Watanabe <zeal20300166@fsgcl.jp>
2022-08-29 10:57:12 +0900

    マッチングの状況と画面のプレイヤーを同期するコンポーネントを作成した

---
+++ HideRecovery/GameSources/Watanabe/Component/MatchingSyncPlayerObject.cpp
@@ -0,0 +1,16 @@
+﻿#include "stdafx.h"
+#include "MatchingSyncPlayerObject.h"
+
+namespace basecross {
+	MatchingSyncPlayerObject::MatchingSyncPlayerObject(const shared_ptr<GameObject>& owner)
+		:Online::OnlineComponent(owner)
+	{}
+
+	void MatchingSyncPlayerObject::OnCreate() {
+	}
+
+	void MatchingSyncPlayerObject::OnCreateRoom() {
+	}
+	void MatchingSyncPlayerObject::OnJoinRoom() {
+	}
+}
---
+++ HideRecovery/GameSources/Watanabe/Component/MatchingSyncPlayerObject.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+#include "stdafx.h"
+#include "Itabashi/OnlineManager.h"
+
+namespace basecross {
+	class MatchingSyncPlayerObject :public Online::OnlineComponent {
+	public:
+		MatchingSyncPlayerObject(const shared_ptr<GameObject>& owner);
+
+		void OnCreate()override;
+
+		// 部屋を作成
+		void OnCreateRoom()override;
+		// 部屋に参加
+		void OnJoinRoom()override;
+	};
+}
