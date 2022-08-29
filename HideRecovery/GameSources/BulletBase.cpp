dd7b4532b75379c7a6f1840ce893bab7e86516dc
Maru40 <umeda.4072@gmail.com>
2022-08-29 16:40:42 +0900

    BulletSpeedを変更

--- HideRecovery/GameSources/BulletBase.cpp
+++ HideRecovery/GameSources/BulletBase.cpp
@@ -17,8 +17,8 @@ namespace basecross {
 
 	BulletBase_Parametor::BulletBase_Parametor() :
 		moveDirect(Vec3::Forward()),
-		moveSpeed(50.0f),
-		maxRange(30.0f),
+		moveSpeed(120.0f),
+		maxRange(50.0f),
 		damageData(1, nullptr)
 	{}
 
