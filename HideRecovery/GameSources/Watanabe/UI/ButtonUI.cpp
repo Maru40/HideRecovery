ace27335f4f250d80fa95e468964a502f5f3faaa
Takumi Watanabe <zeal20300166@fsgcl.jp>
2022-08-30 10:48:44 +0900

    ボタンUIクラスを作成した

---
+++ HideRecovery/GameSources/Watanabe/UI/ButtonUI.cpp
@@ -0,0 +1,17 @@
+﻿#include "stdafx.h"
+#include "ButtonUI.h"
+
+namespace basecross {
+	ButtonUI::ButtonUI(const shared_ptr<Stage>& stage)
+		:UIObjectBase(stage, L"ButtonUI")
+	{}
+	ButtonUI::ButtonUI(const shared_ptr<Stage>& stage, const wstring& line)
+		: UIObjectBase(stage, L"ButtonUI")
+	{}
+
+	void ButtonUI::OnCreate() {
+	}
+
+	void ButtonUI::SetActive(bool flg) {
+	}
+}
---
+++ HideRecovery/GameSources/Watanabe/UI/ButtonUI.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+#include "stdafx.h"
+#include "UIObjectBase.h"
+
+namespace basecross {
+	class ButtonUI :public UIObjectBase {
+	public:
+		ButtonUI(const shared_ptr<Stage>& stage);
+		ButtonUI(const shared_ptr<Stage>& stage, const wstring& line);
+
+		void OnCreate()override;
+
+		void SetActive(bool flg);
+		void ChangeButton() {}
+	};
+}
