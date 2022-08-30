a9fe84df3b948f532d3da22dc8d4cfa0f931722a
Maru40 <umeda.4072@gmail.com>
2022-08-31 06:49:14 +0900

    ChargeBulletでCSVを読み込めるようにした。

--- HideRecovery/GameSources/ChargeBullet.h
+++ HideRecovery/GameSources/ChargeBullet.h
@@ -28,6 +28,7 @@ namespace basecross {
 	public:
 		ChargeBullet(const std::shared_ptr<GameObject>& objPtr);
 
+		void OnCreate() override;
 		void OnUpdate() override;
 
 		void Shot(const std::shared_ptr<GameObject>& owner, const Vec3& direct) override;
