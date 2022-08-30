fc4a97fe60d251377391136c5ab07dd79cda3439
Maru40 <umeda.4072@gmail.com>
2022-08-30 11:27:45 +0900

    UILayerの実装

---
+++ HideRecovery/GameSources/Maruyama/UI/UILayer.h
@@ -0,0 +1,19 @@
+
+/*!
+@file UI.h
+@brief UI‚È‚Ç
+’S“–FŠÛR—TŠì
+*/
+
+#pragma once
+#include "stdafx.h"
+
+namespace basecross {
+	namespace UI
+	{
+		namespace Layer {
+			constexpr int MAP = 2;
+			constexpr int MAP_CURSOR = 3;
+		}
+	}
+}
