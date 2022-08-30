a92a1c6668d294ba982382762d28e4385e9e29de
Takumi Watanabe <zeal20300166@fsgcl.jp>
2022-08-30 11:37:22 +0900

    ゴールを示す板ポリオブジェクトクラスを作成

---
+++ HideRecovery/GameSources/Watanabe/BoardPoly/GoalBP.cpp
@@ -0,0 +1,14 @@
+﻿#include "stdafx.h"
+#include "GoalBP.h"
+#include "BillBoard.h"
+
+namespace basecross {
+	GoalBP::GoalBP(const shared_ptr<Stage>& stage)
+		:StageObjectBase(stage, L"GoalBP")
+	{}
+
+	void GoalBP::OnCreate() {
+	}
+	void GoalBP::OnUpdate() {
+	}
+}
---
+++ HideRecovery/GameSources/Watanabe/BoardPoly/GoalBP.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+#include "stdafx.h"
+#include "StageObjectBase.h"
+
+namespace basecross {
+	class GoalBP :public StageObjectBase {
+	public:
+		GoalBP(const shared_ptr<Stage>& stage);
+
+		void OnCreate()override;
+		void OnUpdate()override;
+	};
+}
