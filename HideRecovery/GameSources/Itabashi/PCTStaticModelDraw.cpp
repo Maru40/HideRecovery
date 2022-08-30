a0eb8f0912f1286993bd412d4565f92e3f5f7af7
Itabashi Oota <itaoo0430@gmail.com>
2022-08-30 16:45:27 +0900

    スカイボックス用のStaticModelのPCT用を作った

---
+++ HideRecovery/GameSources/Itabashi/PCTStaticModelDraw.cpp
@@ -0,0 +1,48 @@
+﻿#include "stdafx.h"
+#include "PCTStaticModelDraw.h"
+
+namespace basecross
+{
+	IMPLEMENT_DX11_PIXEL_SHADER(PS_PCTStaticModel, App::GetApp()->GetShadersPath() + L"PSPCTStaticModel.cso")
+
+	PCTStaticModelDraw::PCTStaticModelDraw(const shared_ptr<GameObject>& GameObjectPtr) :
+		SmBaseDraw(GameObjectPtr)
+	{}
+
+	PCTStaticModelDraw::~PCTStaticModelDraw() {}
+
+	void PCTStaticModelDraw::OnCreate() {
+		//ライティングのみだと極端になるので調整
+		//SetEmissive(bsm::Col4(0.5f, 0.5f, 0.5f, 0.0f));
+		//SetDiffuse(bsm::Col4(0.6f, 0.6f, 0.6f, 1.0f));
+	}
+
+	void PCTStaticModelDraw::OnDraw() {
+		if (GetGameObject()->GetAlphaActive()) {
+			if (!(GetBlendState() == BlendState::AlphaBlend || GetBlendState() == BlendState::Additive)) {
+				SetBlendState(BlendState::AlphaBlend);
+			}
+			SetRasterizerState(RasterizerState::DoubleDraw);
+		}
+		//メッシュリソースの取得
+		auto PtrMeshResource = GetMeshResource();
+		if (PtrMeshResource) {
+			DrawModel<VSPNTStatic, PS_PCTStaticModel>(PtrMeshResource->GetMashData());
+		}
+		//マルチメッシュリソースの取得
+		auto PtrMultiMeshResource = GetMultiMeshResource();
+		if (PtrMultiMeshResource) {
+			size_t count = PtrMultiMeshResource->GetMeshVecCount();
+			auto& vec = PtrMultiMeshResource->GetMeshVec();
+			for (size_t i = 0; i < count; i++) {
+				if (GetMultiMeshIsDraw(i)) {
+					DrawModel<VSPNTStatic, PSPCTStatic>(vec[i]);
+				}
+			}
+		}
+		//後始末
+		auto Dev = App::GetApp()->GetDeviceResources();
+		Dev->InitializeStates();
+
+	}
+}
