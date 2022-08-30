15b2e36f21616db59aa2cfd603cf7a6db94518a1
Takumi Watanabe <zeal20300166@fsgcl.jp>
2022-08-30 14:57:11 +0900

    UIを拡大縮小させるコンポーネントを作成

---
+++ HideRecovery/GameSources/Watanabe/Component/Zooming.cpp
@@ -0,0 +1,14 @@
+﻿#include "stdafx.h"
+#include "Zooming.h"
+
+namespace basecross {
+	Zooming::Zooming(const shared_ptr<GameObject>& owner)
+		:Component(owner)
+	{}
+
+	void Zooming::OnCreate() {
+	}
+
+	void Zooming::OnUpdate() {
+	}
+}
---
+++ HideRecovery/GameSources/Watanabe/Component/Zooming.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+#include "stdafx.h"
+
+namespace basecross {
+	class Zooming :public Component {
+	public:
+		Zooming(const shared_ptr<GameObject>& owner);
+
+		void OnCreate()override;
+		void OnUpdate()override;
+		void OnDraw()override {}
+	};
+}
