a0efeca69af00c95306dc29b2e8d710b92392215
Takumi Watanabe <zeal20300166@fsgcl.jp>
2022-08-30 17:07:33 +0900

    スコアUIのデータ取得バグを修正

--- HideRecovery/GameSources/Watanabe/UI/ScoreUI.h
+++ HideRecovery/GameSources/Watanabe/UI/ScoreUI.h
@@ -5,7 +5,10 @@
 
 namespace basecross {
 	class ScoreUI :public UIObjectBase {
+		// 表記としてのプレイヤー番号
 		int m_playerNumber;
+		// 処理内のプレイヤー番号
+		int m_gamePlayerNumber;
 		team::TeamType m_teamType;
 	public:
 		ScoreUI(const shared_ptr<Stage>& stage, int playerNumber, team::TeamType teamType);
