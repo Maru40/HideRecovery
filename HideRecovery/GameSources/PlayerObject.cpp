db3ef5a76f7af0f3cf92587625fc2013d5483b3a
Maru40 <umeda.4072@gmail.com>
2022-08-31 15:00:32 +0900

    PlayerObjectでAccessHidePlaceをでタッチ

--- HideRecovery/GameSources/PlayerObject.cpp
+++ HideRecovery/GameSources/PlayerObject.cpp
@@ -112,7 +112,7 @@ namespace basecross {
 		//AddComponent<FieldMap>();
 		//AddComponent<Teleport>();
 		AddComponent<EyeSearchRange>();
-		AddComponent<AccessHidePlace>();
+		//AddComponent<AccessHidePlace>();
 
 		AddComponent<Teleport>();	//テレポートの生成
 
