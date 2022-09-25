19ca1a8fd3980e9aeaaa718ed1230adf7002b6fd
Maru40 <umeda.4072@gmail.com>
2022-09-26 02:29:47 +0900

    テスト用のビヘイビアを設定

---
+++ SCRAMBLE BALL/GameSources/Maruyama/Enemy/Behavior/Tester/Tester_Behavior.cpp
@@ -0,0 +1,87 @@
+/*!
+@file Tester_Behavior.cpp
+@brief Tester_Behavior‚È‚ÇŽÀ‘Ì
+*/
+
+#include "stdafx.h"
+#include "Project.h"
+
+#include "Tester_Behavior.h"
+
+#include "Patch/PlayerInputer.h"
+#include "Maruyama/Enemy/Behavior/BehaviorTree.h"
+
+#include "Maruyama/Utility/Timer/GameTimer.h"
+
+namespace basecross {
+	namespace maru {
+
+		 namespace Behavior {
+
+			TesterTree::TesterTree(const std::shared_ptr<GameObject>& objPtr) :
+				Component( objPtr), 
+				m_behaviorTree(new BehaviorTree<TesterTree_Type>())
+			{}
+
+			void TesterTree::OnCreate() {
+				CreateNode();
+				CreateEdge();
+			}
+
+			void TesterTree::OnUpdate() {
+				m_behaviorTree->OnUpdate();
+			}
+
+			void TesterTree::CreateNode() {
+				m_behaviorTree->AddSelecter(BehaviorType::First, std::make_shared<Selecter::TesterFirst>());
+
+				m_behaviorTree->AddSelecter(BehaviorType::Selecter2, std::make_shared<Selecter::TesterSelecterSecond>());
+
+				m_behaviorTree->AddTask(BehaviorType::Task1, std::make_shared<Task::TesterTaskFirst>());
+
+				m_behaviorTree->AddTask(BehaviorType::Task2, std::make_shared<Task::TesterTaskSecond>());
+
+				m_behaviorTree->AddTask(BehaviorType::Task3, std::make_shared<Task::TesterTaskThird>());
+			}
+
+			void TesterTree::CreateEdge() {
+				m_behaviorTree->AddEdge(BehaviorType::First, BehaviorType::Selecter2, std::make_shared<PriorityControllerBase>(0.0f));
+				m_behaviorTree->AddEdge(BehaviorType::First, BehaviorType::Task1, std::make_shared<PriorityControllerBase>(1.0f));
+
+				m_behaviorTree->AddEdge(BehaviorType::Selecter2, BehaviorType::Task2, std::make_shared<PriorityControllerBase>(0.0f));
+				m_behaviorTree->AddEdge(BehaviorType::Selecter2, BehaviorType::Task3, std::make_shared<PriorityControllerBase>(1.0f));
+			}
+
+
+			namespace Task {
+				TesterTaskFirst::TesterTaskFirst() : m_timer(new GameTimer(0.0f)) {}
+				void TesterTaskFirst::OnStart() { m_timer->ResetTimer(3.0f); };
+				bool TesterTaskFirst::OnUpdate() { 
+					m_timer->UpdateTimer();
+					return m_timer->IsTimeUp();
+				};
+				void TesterTaskFirst::OnExit() {};
+
+
+				TesterTaskSecond::TesterTaskSecond() : m_timer(new GameTimer(0.0f)) {}
+				void TesterTaskSecond::OnStart() { m_timer->ResetTimer(3.0f); };
+				bool TesterTaskSecond::OnUpdate() { 
+					m_timer->UpdateTimer();
+					return m_timer->IsTimeUp();
+				};
+				void TesterTaskSecond::OnExit() {};
+
+
+				TesterTaskThird::TesterTaskThird() : m_timer(new GameTimer(0.0f)) {}
+				void TesterTaskThird::OnStart() { m_timer->ResetTimer(3.0f); };
+				bool TesterTaskThird::OnUpdate() {
+					m_timer->UpdateTimer();
+					return m_timer->IsTimeUp();
+				};
+				void TesterTaskThird::OnExit() {};
+			}
+			   
+		}
+
+	}
+}
---
+++ SCRAMBLE BALL/GameSources/Maruyama/Enemy/Behavior/Tester/Tester_Behavior.h
@@ -0,0 +1,118 @@
+/*!
+@file Tester_Behavior.h
+@brief Tester_BehaviorÇ»Ç«
+íSìñé“ÅFä€éR óTäÏ
+*/
+
+#pragma once
+
+#include "stdafx.h"
+#include "Project.h"
+
+#include "../Interface/I_Node.h" 
+#include "../Interface/I_Selecter.h"
+#include "../Interface/I_Task.h"
+
+namespace basecross {
+
+	class GameTimer;
+
+	namespace maru {
+
+		namespace Behavior {
+
+			template<class EnumType>
+			class BehaviorTree;
+
+			enum class TesterTree_Type {
+				First,
+				Selecter2,
+				Task1,
+				Task2,
+				Task3
+			};
+
+			class TesterTree : public Component 
+			{
+			public:
+				using BehaviorType = TesterTree_Type;
+
+			private:
+				std::unique_ptr<BehaviorTree<TesterTree_Type>> m_behaviorTree;
+
+			public:
+				TesterTree(const std::shared_ptr<GameObject>& objPtr);
+
+				void OnCreate() override;
+				void OnUpdate() override;
+
+			private:
+
+				void CreateNode();
+				void CreateEdge();
+			};
+
+
+			namespace Selecter {
+
+				class TesterFirst : public SelecterBase
+				{
+
+				public:
+
+
+				};
+
+
+				class TesterSelecterSecond : public SelecterBase
+				{
+
+				public:
+
+
+				};
+
+			}
+
+			namespace Task 
+			{
+				class TesterTaskFirst : public Behavior::I_Task
+				{
+					std::unique_ptr<GameTimer> m_timer;
+
+				public:
+					TesterTaskFirst();
+
+					void OnStart() override ;
+					bool OnUpdate() override ;
+					void OnExit() override ;
+				};
+
+				class TesterTaskSecond : public I_Task 
+				{
+					std::unique_ptr<GameTimer> m_timer;
+
+				public:
+					TesterTaskSecond();
+
+					void OnStart() override;
+					bool OnUpdate() override;
+					void OnExit() override;
+				};
+
+				class TesterTaskThird : public I_Task
+				{
+					std::unique_ptr<GameTimer> m_timer;
+
+				public:
+					TesterTaskThird();
+
+					void OnStart() override;
+					bool OnUpdate() override;
+					void OnExit() override;
+				};
+			}
+
+		}
+	}
+}
