32617986930d524b8c77fab9006030926fcd7696
Maru40 <umeda.4072@gmail.com>
2022-09-30 03:33:12 +0900

    StateMachineの仮実装

---
+++ SCRAMBLE BALL/GameSources/Maruyama/Utility/StateMachine/StateMachine.cpp
@@ -0,0 +1,22 @@
+
+/*!
+@file StateMachine.cpp
+@brief StateMachineのクラス実体
+担当：丸山裕喜
+*/
+
+#include "stdafx.h"
+#include "Project.h"
+
+#include "StateMachine.h"
+
+#include "Maruyama/Enemy/Astar/GraphBase.h"
+
+namespace basecross {
+
+	namespace maru {
+
+		
+
+	}
+}
---
+++ SCRAMBLE BALL/GameSources/Maruyama/Utility/StateMachine/StateMachine.h
@@ -0,0 +1,28 @@
+/*!
+@file StateMachine.h
+@brief StateMachine
+’S“–ÒFŠÛR —TŠì
+*/
+
+#pragma once
+
+#include "stdafx.h"
+#include "Project.h"
+
+namespace basecross {
+
+	template<class node_type, class EnumType, class TransitionStructMember>
+	class GraphBase;
+
+	namespace maru {
+
+		class NodeBase;
+		class EdgeBase;
+
+		class StateMachine
+		{
+			//std::shared_ptr<NodeBase, > m_graph;
+		};
+
+	}
+}
