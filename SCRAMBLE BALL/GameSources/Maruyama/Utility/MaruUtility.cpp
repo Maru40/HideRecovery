c320f0aca7d126a718c252b4064d8b09319dbe06
Maru40 <umeda.4072@gmail.com>
2022-10-12 15:24:07 +0900

    Utilityの参照渡しをconst参照に変更

--- SCRAMBLE BALL/GameSources/Maruyama/Utility/MaruUtility.cpp
+++ SCRAMBLE BALL/GameSources/Maruyama/Utility/MaruUtility.cpp
@@ -380,13 +380,13 @@ namespace basecross {
 
 		Vec3 Utility::ConvertWorldToScreen(const shared_ptr<ViewBase>& view, const Vec3& position)
 		{
-			const auto camera = view->GetTargetCamera();
-			const auto viewport = view->GetTargetViewport();
+			const auto& camera = view->GetTargetCamera();
+			const auto& viewport = view->GetTargetViewport();
 			float w = viewport.Width * 0.5f;  // 画面の幅の半分
 			float h = viewport.Height * 0.5f; // 画面の高さの半分
 
-			auto mView = camera->GetViewMatrix(); // ビュー行列
-			auto mProj = camera->GetProjMatrix(); // プロジェクション(射影)行列
+			const auto& mView = camera->GetViewMatrix(); // ビュー行列
+			const auto& mProj = camera->GetProjMatrix(); // プロジェクション(射影)行列
 			Mat4x4 mScreen( // スクリーン行列（BaseCross用）
 				{ w, 0, 0, 0 },
 				{ 0, h, 0, 0 },
