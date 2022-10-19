db611e6a2eabae5294745b6812c77b817df0d16c
Takumi Watanabe <zeal20300166@fsgcl.jp>
2022-10-19 09:28:24 +0900

    テストステージを実装

--- SCRAMBLE BALL/GameSources/Watanabe/TestStage/TestShaderStage.cpp
+++ SCRAMBLE BALL/GameSources/Watanabe/TestStage/TestShaderStage.cpp
@@ -6,5 +6,30 @@
 #include "stdafx.h"
 #include "TestShaderStage.h"
 
+#include "../DebugClass/Debug.h"
+
 namespace basecross {
+	void TestShaderStage::CreateViewLight() {
+		const Vec3 eye(5.0f, 5.0f, -5.0f);
+		const Vec3 at(0.0f);
+		auto PtrView = CreateView<SingleView>();
+		//ビューのカメラの設定
+		auto PtrCamera = ObjectFactory::Create<Camera>();
+		PtrView->SetCamera(PtrCamera);
+		PtrCamera->SetEye(eye);
+		PtrCamera->SetAt(at);
+		//マルチライトの作成
+		auto PtrMultiLight = CreateLight<MultiLight>();
+		//デフォルトのライティングを指定
+		PtrMultiLight->SetDefaultLighting();
+	}
+
+	void TestShaderStage::OnCreate() {
+		CreateViewLight();
+		AddGameObject<Debug>();
+		Debug::GetInstance()->Log(L"TestShaderStage");
+	}
+
+	void TestShaderStage::OnUpdate() {
+	}
 }
--- SCRAMBLE BALL/GameSources/Watanabe/TestStage/TestShaderStage.h
+++ SCRAMBLE BALL/GameSources/Watanabe/TestStage/TestShaderStage.h
@@ -7,4 +7,12 @@
 #include "stdafx.h"
 
 namespace basecross {
+	class TestShaderStage :public Stage {
+		void CreateViewLight();
+	public:
+		TestShaderStage() :Stage() {}
+
+		void OnCreate() override;
+		void OnUpdate() override;
+	};
 }
