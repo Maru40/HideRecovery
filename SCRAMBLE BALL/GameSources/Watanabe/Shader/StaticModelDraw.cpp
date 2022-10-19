5e4d134d82112ab435d1752bf3375bd4626b2540
Takumi Watanabe <zeal20300166@fsgcl.jp>
2022-10-19 14:54:48 +0900

    静的モデルの描画コンポーネントクラスを作成

---
+++ SCRAMBLE BALL/GameSources/Watanabe/Shader/StaticModelDraw.cpp
@@ -0,0 +1,10 @@
+﻿/*!
+@file   StaticModelDraw.cpp
+@brief  静的モデルの描画コンポーネント実体
+*/
+
+#include "stdafx.h"
+#include "StaticModelDraw.h"
+
+namespace basecross {
+}
---
+++ SCRAMBLE BALL/GameSources/Watanabe/Shader/StaticModelDraw.h
@@ -0,0 +1,12 @@
+﻿/*!
+@file   StaticModelDraw.h
+@brief  静的モデルの描画コンポーネント
+*/
+
+#pragma once
+#include "stdafx.h"
+
+namespace basecross {
+	class StaticModelDraw {
+	};
+}
