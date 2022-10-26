7192b68035175901a695df47fc310d84529c3a26
Maru40 <umeda.4072@gmail.com>
2022-10-26 10:31:37 +0900

    AccessHidePlaceのDrawUI更新をできるようにした。

--- SCRAMBLE BALL/GameSources/Maruyama/Player/Component/AccessHidePlace.cpp
+++ SCRAMBLE BALL/GameSources/Maruyama/Player/Component/AccessHidePlace.cpp
@@ -17,7 +17,8 @@ namespace basecross {
 
 	AccessHidePlace::AccessHidePlace(const std::shared_ptr<GameObject>& objPtr) :
 		Component(objPtr),
-		m_range(2.0f)
+		m_range(2.0f),
+		m_isUpdateDrawBoxUI(true)
 	{}
 
 	void AccessHidePlace::OnLateStart() {
@@ -25,24 +26,32 @@ namespace basecross {
 	}
 
 	void AccessHidePlace::OnUpdate() {
+		if (IsUpdateDrawBoxUI()) {
+			UpdateDrawBoxUI();
+		}
+	}
+
+	void AccessHidePlace::UpdateDrawBoxUI() {
 		for (auto& weakPlace : m_allHidePlaces) {
 			auto place = weakPlace.lock();
 			if (!place) {
 				continue;
 			}
 
+			//開いてあるなら、UIを非表示にする。
 			if (place->IsOpen()) {
 				place->SetDrawUI(false);
 				continue;
 			}
 
+			//近くに来たらUIを表示する。
 			auto toVec = maru::Utility::CalcuToTargetVec(GetGameObject(), place->GetGameObject());
 			auto selfPosition = transform->GetPosition();
 			auto placePosition = place->GetGameObject()->GetComponent<Transform>()->GetPosition();
 			float range = GetRange();
 			float length = toVec.length();
 			bool isDraw = toVec.length() < GetRange();
-			
+
 			if (isDraw != place->IsDrawUI()) {
 				place->SetDrawUI(isDraw);
 			}
@@ -56,6 +65,7 @@ namespace basecross {
 				continue;
 			}
 
+			//近いオブジェクトを開く
 			auto toVec = maru::Utility::CalcuToTargetVec(GetGameObject(), place->GetGameObject());
 			if (toVec.length() < GetRange()) {
 				place->Open();
