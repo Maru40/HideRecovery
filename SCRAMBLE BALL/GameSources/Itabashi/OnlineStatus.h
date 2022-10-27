7a775a8d47e0719427a8497ea1afeb8aff91fe14
Itabashi Oota <itaoo0430@gmail.com>
2022-10-27 17:14:53 +0900

    オンラインオブジェクト同期用コンポーネントを作成

---
+++ SCRAMBLE BALL/GameSources/Itabashi/OnlineStatus.h
@@ -0,0 +1,40 @@
+﻿#pragma once
+#include <unordered_map>
+#include "Utility/UniqueIdCreater.h"
+
+namespace basecross
+{
+namespace Online
+{
+	class OnlineStatus : public Component
+	{
+	public:
+
+		static constexpr std::uint32_t INVALID_INSTANCE_ID = 0;
+
+	private:
+		static UniqueIdCreater<std::uint32_t> m_instanceIdCreater;
+		static std::unordered_map<std::uint32_t, std::weak_ptr<OnlineStatus>> m_onlineStatusMap;
+		std::uint32_t m_instanceId = INVALID_INSTANCE_ID;
+
+	public:
+
+		OnlineStatus(const std::shared_ptr<GameObject>& owner);
+
+		void SetCreateInstanceId();
+
+		bool ChangeInstanceId(std::uint32_t instanceId);
+		std::uint32_t GetInstanceId() const { return m_instanceId; }
+
+		void OnUpdate() override {}
+
+		void OnDestroy() override;
+
+		void OnDraw() override {}
+
+		static std::shared_ptr<OnlineStatus> FindOnlineStatus(std::uint32_t instanceId);
+
+		static std::shared_ptr<GameObject> FindOnlineGameObject(std::uint32_t instanceId);
+	};
+}
+}
