2546f1179446c202222814d2971c4b7eec1ea427
Maru40 <umeda.4072@gmail.com>
2022-10-27 14:53:06 +0900

    EnemyAIDirectorでTeamTypeごとに分けるように変更

--- SCRAMBLE BALL/GameSources/Maruyama/Enemy/AIDirector/EnemyAIDirector.cpp
+++ SCRAMBLE BALL/GameSources/Maruyama/Enemy/AIDirector/EnemyAIDirector.cpp
@@ -16,6 +16,8 @@
 
 #include "Maruyama/Utility/Utility.h"
 
+#include "Maruyama/Interface/I_TeamMember.h"
+
 namespace basecross {
 
 	std::weak_ptr<Enemy::AIDirector> maru::SingletonComponent<Enemy::AIDirector>::sm_instance;
@@ -29,8 +31,13 @@ namespace basecross {
 		void AIDirector::OnLateStart() {
 			SettingStartAllEnemys();
 
-			//仮で全員一つのファクションにアサイン
-			auto faction = CreateFaction(m_enemys);
+			//チームタイプごとに分ける。
+			auto enemysMap = DivideTeamType(m_enemys);
+
+			//チームごとにファクションを生成する。
+			for (auto& pair : enemysMap) {
+				auto faction = CreateFaction(pair.second);
+			}
 		}
 
 		void AIDirector::OnUpdate() {
@@ -56,6 +63,21 @@ namespace basecross {
 		bool AIDirector::RemoveFaction(const std::shared_ptr<FactionCoordinator>& removeCoordinator) {
 			removeCoordinator->OnExit();
 			return maru::Utility::RemoveVec(m_factionCoordinators, removeCoordinator);
+		}
+
+		std::unordered_map<team::TeamType, std::vector<weak_ptr<EnemyBase>>> AIDirector::DivideTeamType(const std::vector<std::weak_ptr<EnemyBase>>& members) {
+			std::unordered_map<team::TeamType, std::vector<std::weak_ptr<EnemyBase>>> enemysMap;
+
+			for (auto& weakEnemy : m_enemys) {
+				auto enemy = weakEnemy.lock();
+				auto teamMember = enemy->GetGameObject()->GetComponent<I_TeamMember>(false);
+				if (teamMember) {
+					auto teamType = teamMember->GetTeam();
+					enemysMap[teamType].push_back(enemy);
+				}
+			}
+
+			return enemysMap;
 		}
 
 		//--------------------------------------------------------------------------------------
