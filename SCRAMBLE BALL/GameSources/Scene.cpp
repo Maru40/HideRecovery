e4de9adfa0fc9c1b3347a699664f01fedd6e64d1
Takumi Watanabe <zeal20300166@fsgcl.jp>
2022-10-31 10:06:32 +0900

    デバッグ用に変更した処理をもとに戻した

--- SCRAMBLE BALL/GameSources/Scene.cpp
+++ SCRAMBLE BALL/GameSources/Scene.cpp
@@ -68,7 +68,7 @@ namespace basecross {
 			//ResetActiveStage<GameStage>();
 		}
 		else if (event->m_MsgStr == L"ToLoadStage") {
-			ResetActiveStage<LoadStage>(L"ToWatanabeStage");
+			ResetActiveStage<LoadStage>(L"ToTitleStage");
 		}
 		else if (event->m_MsgStr == L"ToMainStage") {
 			ResetActiveStage<MainStage>();
