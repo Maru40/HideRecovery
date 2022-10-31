107488dde3c39e09e989f15d181602e4a1d3be53
Takumi Watanabe <zeal20300166@fsgcl.jp>
2022-10-31 10:37:14 +0900

    エフェクトテスト用のステージを作成

---
+++ SCRAMBLE BALL/GameSources/Watanabe/TestStage/TestEffectStage.cpp
@@ -0,0 +1,31 @@
+﻿#include "stdafx.h"
+#include "TestEffectStage.h"
+#include "../DebugClass/Debug.h"
+#include "../Effekseer/EfkEffect.h"
+
+namespace basecross {
+	void TestEffectStage::CreateViewLight() {
+		const Vec3 eye(5.0f, 5.0f, -5.0f);
+		const Vec3 at(0.0f);
+		auto PtrView = CreateView<SingleView>();
+		//ビューのカメラの設定
+		auto PtrCamera = ObjectFactory::Create<Camera>();
+		PtrView->SetCamera(PtrCamera);
+		//PtrCamera->SetNear(5);
+		//PtrCamera->SetFar(10);
+		//マルチライトの作成
+		auto PtrMultiLight = CreateLight<MultiLight>();
+		//デフォルトのライティングを指定
+		PtrMultiLight->SetDefaultLighting();
+	}
+
+	void TestEffectStage::OnCreate() {
+		CreateViewLight();
+		AddGameObject<Debug>();
+		Debug::GetInstance()->Log(L"TestEffectStage");
+		AddGameObject<EfkInterface>();
+	}
+
+	void TestEffectStage::OnUpdate() {
+	}
+}
---
+++ SCRAMBLE BALL/GameSources/Watanabe/TestStage/TestEffectStage.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+#include "stdafx.h"
+
+namespace basecross {
+	class TestEffectStage :public Stage {
+		void CreateViewLight();
+	public:
+		TestEffectStage() :Stage() {}
+
+		void OnCreate()override;
+		void OnUpdate()override;
+	};
+}
