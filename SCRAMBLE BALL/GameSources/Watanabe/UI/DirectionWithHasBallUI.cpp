cba155d6c14c25d26d3b078fd8e9b0795aeb5548
Takumi Watanabe <zeal20300166@fsgcl.jp>
2022-10-31 14:04:01 +0900

    仮の矢印を画面に表示させた

--- SCRAMBLE BALL/GameSources/Watanabe/UI/DirectionWithHasBallUI.cpp
+++ SCRAMBLE BALL/GameSources/Watanabe/UI/DirectionWithHasBallUI.cpp
@@ -1,12 +1,28 @@
 ﻿#include "stdafx.h"
 #include "DirectionWithHasBallUI.h"
+#include "../Manager/SpriteDataManager.h"
+#include "../Utility/Utility.h"
+
+#include "../DebugClass/Debug.h"
 
 namespace basecross {
 	DirectionWithHasBallUI::DirectionWithHasBallUI(const shared_ptr<Stage>& stage)
 		:UIObjectBase(stage, L"DirectionWithHasBallUI")
 	{}
 
+	shared_ptr<SimpleSprite> DirectionWithHasBallUI::CreateSprite(const wstring& spriteKey) {
+		auto sprite = GetStage()->AddGameObject<SimpleSprite>(
+			SimpleSprite::Type::SpriteData, spriteKey
+			);
+		return sprite;
+	}
+
 	void DirectionWithHasBallUI::OnCreate() {
+		m_cursor = CreateSprite(L"Triangle");
+		auto rectTrans = m_cursor->GetRectTransform();
+		rectTrans->SetPosition(Vec2(400, 0));
+		//rectTrans->SetScale(0.3f, 0.5f);
+		m_cursor->SetParent(GetThis<DirectionWithHasBallUI>());
 	}
 	void DirectionWithHasBallUI::OnUpdate() {
 	}
--- SCRAMBLE BALL/GameSources/Watanabe/UI/DirectionWithHasBallUI.h
+++ SCRAMBLE BALL/GameSources/Watanabe/UI/DirectionWithHasBallUI.h
@@ -1,9 +1,13 @@
 ﻿#pragma once
 #include "stdafx.h"
 #include "UIObjectBase.h"
+#include "SimpleSprite.h"
 
 namespace basecross {
 	class DirectionWithHasBallUI :public UIObjectBase {
+		shared_ptr<SimpleSprite> m_cursor;
+
+		shared_ptr<SimpleSprite> CreateSprite(const wstring& spriteKey);
 	public:
 		DirectionWithHasBallUI(const shared_ptr<Stage>& stage);
 
