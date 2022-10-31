5ea408ac320dc4231564ff8c8974dfaf3a2a0829
Takumi Watanabe <zeal20300166@fsgcl.jp>
2022-10-31 13:44:02 +0900

    ボールを持つプレイヤーを指すカーソルクラスを作成

---
+++ SCRAMBLE BALL/GameSources/Watanabe/UI/DirectionWithHasBallUI.cpp
@@ -0,0 +1,13 @@
+﻿#include "stdafx.h"
+#include "DirectionWithHasBallUI.h"
+
+namespace basecross {
+	DirectionWithHasBallUI::DirectionWithHasBallUI(const shared_ptr<Stage>& stage)
+		:UIObjectBase(stage, L"DirectionWithHasBallUI")
+	{}
+
+	void DirectionWithHasBallUI::OnCreate() {
+	}
+	void DirectionWithHasBallUI::OnUpdate() {
+	}
+}
---
+++ SCRAMBLE BALL/GameSources/Watanabe/UI/DirectionWithHasBallUI.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+#include "stdafx.h"
+#include "UIObjectBase.h"
+
+namespace basecross {
+	class DirectionWithHasBallUI :public UIObjectBase {
+	public:
+		DirectionWithHasBallUI(const shared_ptr<Stage>& stage);
+
+		void OnCreate()override;
+		void OnUpdate()override;
+	};
+}
