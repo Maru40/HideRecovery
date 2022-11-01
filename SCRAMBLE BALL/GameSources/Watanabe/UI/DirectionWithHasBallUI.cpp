3d2189861c8a74e95380e68a74dad925771ae715
Takumi Watanabe <zeal20300166@fsgcl.jp>
2022-11-01 14:19:11 +0900

    画面外の場合のみ表示するようにした

--- SCRAMBLE BALL/GameSources/Watanabe/UI/DirectionWithHasBallUI.cpp
+++ SCRAMBLE BALL/GameSources/Watanabe/UI/DirectionWithHasBallUI.cpp
@@ -19,27 +19,59 @@ namespace basecross {
 
 	void DirectionWithHasBallUI::OnCreate() {
 		m_camera = GetStage()->GetView()->GetTargetCamera();
-
 		m_cursor = CreateSprite(L"Triangle");
+
 		auto rectTrans = m_cursor->GetRectTransform();
 		rectTrans->SetPosition(Vec2(0, 400));
 		rectTrans->SetScale(0.3f, -0.3f);
+
 		m_cursor->SetParent(GetThis<DirectionWithHasBallUI>());
+		m_rectTrans = GetRectTransform();
 	}
+
 	void DirectionWithHasBallUI::OnUpdate() {
+		if (!m_targetObject) {
+			//return;
+		}
+
+		auto view = GetStage()->GetView();
+		// Viewportを0基準からー～＋範囲に変換
+		const Viewport& viewport = view->GetTargetViewport();
+		float halfWidth = viewport.Width / 2.0f;
+		float halfHeight = viewport.Height / 2.0f;
+		Rect2D<float> screenRect(
+			-halfWidth, -halfHeight,
+			halfWidth, halfHeight
+		);
+
+		// ターゲットが画面内か
+		bool isTargetInScreen = Utility::IsPresentInScreen(Vec3(0, 0, 0), view, screenRect);
+		SetDrawActive(!isTargetInScreen);
+		// ターゲットが画面内
+		if (isTargetInScreen) {
+			return;
+		}
+
+		// カメラ方向を計算
 		Vec3 cameraForward = m_camera->GetAt() - m_camera->GetEye();
 		cameraForward.normalize();
-		//
-		auto vec = Utility::Vector3ProjectOnPlane(Vec3(0, 0, 0) - m_camera->GetAt(), cameraForward);
-		vec.normalize();
-		auto cameraUp = m_camera->GetUp().GetNormalized();
-		//auto cameraUp = Vec3(1, 0, 0);
-		// カメラの上方向（スクリーンの上方向）と
-		// スクリーン上のベクトル（vec）の角度を求める
-		auto angle = Utility::GetTwoVectorAngle360(cameraUp, vec, cameraForward);
-		Debug::GetInstance()->Log(360 - angle);
-		Debug::GetInstance()->Log(angle);
+		// カメラ方向を法線とした平面にターゲット方向へのベクトルを投影する
+		Vec3 targetDirectionOnPlane = Utility::Vector3ProjectOnPlane(
+			Vec3(0, 0, 0) - m_camera->GetAt(), cameraForward);
+		targetDirectionOnPlane.normalize();
+		Vec3 cameraUp = m_camera->GetUp().GetNormalized();
 
-		GetRectTransform()->SetRotation(XMConvertToRadians(angle));
+		// カメラの上方向（スクリーンの上方向）を基準とした
+		// targetDirectionOnPlaneのなす角度を求める
+		float angle = Utility::GetTwoVectorAngle360(cameraUp, targetDirectionOnPlane, cameraForward);
+		m_rectTrans->SetRotation(XMConvertToRadians(angle));
+	}
+
+	void DirectionWithHasBallUI::SetTarget(const shared_ptr<Transform>& target) {
+		m_targetObject = target;
+	}
+
+	void DirectionWithHasBallUI::ClearTarget() {
+		SetTarget(nullptr);
 	}
 }
--- SCRAMBLE BALL/GameSources/Watanabe/UI/DirectionWithHasBallUI.h
+++ SCRAMBLE BALL/GameSources/Watanabe/UI/DirectionWithHasBallUI.h
@@ -7,11 +7,25 @@ namespace basecross {
 	class DirectionWithHasBallUI :public UIObjectBase {
 		shared_ptr<SimpleSprite> m_cursor;
 		shared_ptr<Camera> m_camera;
+		shared_ptr<RectTransform> m_rectTrans;
+		shared_ptr<Transform> m_targetObject;
+
 		shared_ptr<SimpleSprite> CreateSprite(const wstring& spriteKey);
 	public:
 		DirectionWithHasBallUI(const shared_ptr<Stage>& stage);
 
 		void OnCreate()override;
 		void OnUpdate()override;
+
+		/// <summary>
+		/// ターゲットをセット
+		/// </summary>
+		/// <param name="target">ターゲットのトランスフォーム</param>
+		void SetTarget(const shared_ptr<Transform>& target);
+
+		/// <summary>
+		/// ターゲット情報をクリア
+		/// </summary>
+		void ClearTarget();
 	};
 }
