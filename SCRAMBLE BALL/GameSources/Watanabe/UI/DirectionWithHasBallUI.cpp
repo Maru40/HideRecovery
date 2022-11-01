346824fbea0587564d132ff1bc489eb5d8c8d71e
Takumi Watanabe <zeal20300166@fsgcl.jp>
2022-11-01 13:31:21 +0900

    画面外にある特定の位置の方向を示すUIを実装

--- SCRAMBLE BALL/GameSources/Watanabe/UI/DirectionWithHasBallUI.cpp
+++ SCRAMBLE BALL/GameSources/Watanabe/UI/DirectionWithHasBallUI.cpp
@@ -18,12 +18,28 @@ namespace basecross {
 	}
 
 	void DirectionWithHasBallUI::OnCreate() {
+		m_camera = GetStage()->GetView()->GetTargetCamera();
+
 		m_cursor = CreateSprite(L"Triangle");
 		auto rectTrans = m_cursor->GetRectTransform();
-		rectTrans->SetPosition(Vec2(400, 0));
-		//rectTrans->SetScale(0.3f, 0.5f);
+		rectTrans->SetPosition(Vec2(0, 400));
+		rectTrans->SetScale(0.3f, -0.3f);
 		m_cursor->SetParent(GetThis<DirectionWithHasBallUI>());
 	}
 	void DirectionWithHasBallUI::OnUpdate() {
+		Vec3 cameraForward = m_camera->GetAt() - m_camera->GetEye();
+		cameraForward.normalize();
+		//
+		auto vec = Utility::Vector3ProjectOnPlane(Vec3(0, 0, 0) - m_camera->GetAt(), cameraForward);
+		vec.normalize();
+		auto cameraUp = m_camera->GetUp().GetNormalized();
+		//auto cameraUp = Vec3(1, 0, 0);
+		// カメラの上方向（スクリーンの上方向）と
+		// スクリーン上のベクトル（vec）の角度を求める
+		auto angle = Utility::GetTwoVectorAngle360(cameraUp, vec, cameraForward);
+		Debug::GetInstance()->Log(360 - angle);
+		Debug::GetInstance()->Log(angle);
+
+		GetRectTransform()->SetRotation(XMConvertToRadians(angle));
 	}
 }
--- SCRAMBLE BALL/GameSources/Watanabe/UI/DirectionWithHasBallUI.h
+++ SCRAMBLE BALL/GameSources/Watanabe/UI/DirectionWithHasBallUI.h
@@ -6,7 +6,7 @@
 namespace basecross {
 	class DirectionWithHasBallUI :public UIObjectBase {
 		shared_ptr<SimpleSprite> m_cursor;
-
+		shared_ptr<Camera> m_camera;
 		shared_ptr<SimpleSprite> CreateSprite(const wstring& spriteKey);
 	public:
 		DirectionWithHasBallUI(const shared_ptr<Stage>& stage);
