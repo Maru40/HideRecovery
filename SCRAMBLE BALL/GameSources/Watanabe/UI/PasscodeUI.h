53d105944bb8cc2bb34c259b8b40940b8ce66859
Takumi Watanabe <zeal20300166@fsgcl.jp>
2022-11-01 15:31:38 +0900

    パスコード表示用のUIを作成

---
+++ SCRAMBLE BALL/GameSources/Watanabe/UI/PasscodeUI.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+#include "stdafx.h"
+#include "UIObjectBase.h"
+#include "SimpleSprite.h"
+#include "Numbers.h"
+
+namespace basecross {
+	class PasscodeUI :public UIObjectBase {
+		shared_ptr<SimpleSprite> m_label;
+		shared_ptr<Numbers> m_passCodeNumbers;
+	public:
+		PasscodeUI(const shared_ptr<Stage>& stage);
+
+		void OnCreate()override;
+		void UpdateCode(int code);
+	};
+}
