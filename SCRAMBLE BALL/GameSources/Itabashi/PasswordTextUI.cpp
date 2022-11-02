0c28f6f5b54ea446918fb78ec5647f45e27b2940
Itabashi Oota <itaoo0430@gmail.com>
2022-11-02 09:44:28 +0900

    パスワードUI用コンポーネントを作成

---
+++ SCRAMBLE BALL/GameSources/Itabashi/PasswordTextUI.cpp
@@ -0,0 +1,86 @@
+﻿#include "PasswordTextUI.h"
+
+namespace basecross
+{
+namespace UI
+{
+	bool PasswordTextUI::TryPush(wchar_t wc)
+	{
+		// バックスペースが押されたら
+		if (wc == VK_BACK)
+		{
+			if (!m_passwordText.empty())
+			{
+				m_passwordText.pop_back();
+			}
+
+			return false;
+		}
+
+		// パスワード桁数に達していたら
+		if (IsComplete())
+		{
+			return false;
+		}
+
+
+		// Enterは無視する
+		if (wc == VK_RETURN)
+		{
+			return false;
+		}
+
+
+
+		if (L'0' <= wc && wc <= L'9')
+		{
+			m_passwordText.push_back(wc);
+			return true;
+		}
+
+		return false;
+	}
+
+	PasswordTextUI::PasswordTextUI(const std::shared_ptr<GameObject>& owner) :
+		Component(owner)
+	{
+		itbs::Input::InputTextManager::AddInputText(this);
+		m_passwordText.reserve(DEFAULT_PASSWORD_DIST_NUM);
+	}
+
+	PasswordTextUI::~PasswordTextUI()
+	{
+		itbs::Input::InputTextManager::RemoveInputText(this);
+	}
+
+	void PasswordTextUI::OnCreate()
+	{
+
+	}
+
+	void PasswordTextUI::OnUpdate()
+	{
+
+	}
+
+	bool PasswordTextUI::Push(wchar_t wc)
+	{
+		if (!TryPush(wc))
+		{
+			return false;
+		}
+
+		return true;
+	}
+
+	void PasswordTextUI::SetPasswordDisitNum(int passwordDisitNum)
+	{
+		m_passwordDisitNum = passwordDisitNum;
+
+		if (m_passwordDisitNum > m_passwordText.capacity())
+		{
+			m_passwordText.reserve(passwordDisitNum);
+		}
+	}
+}
+}
---
+++ SCRAMBLE BALL/GameSources/Itabashi/PasswordTextUI.h
@@ -0,0 +1,40 @@
+﻿#pragma once
+#include "Utility/InputTextManager.h"
+
+namespace basecross
+{
+namespace UI
+{
+	class PasswordTextUI : public Component, public itbs::Input::I_InputText
+	{
+		static constexpr int DEFAULT_PASSWORD_DIST_NUM = 5;
+
+		std::wstring m_passwordText;
+		int m_passwordDisitNum = DEFAULT_PASSWORD_DIST_NUM;
+
+		bool TryPush(wchar_t wc);
+
+	public:
+		PasswordTextUI(const std::shared_ptr<GameObject>& owner);
+
+		~PasswordTextUI() noexcept;
+
+		void OnCreate() override;
+
+		void OnUpdate() override;
+
+		void OnDraw() override {}
+
+		bool Push(wchar_t wc) override;
+
+		const wchar_t* GetText() const override { return m_passwordText.c_str(); }
+
+		void Clear() override { m_passwordText.clear(); }
+
+		void SetPasswordDisitNum(int passwordDisitNum);
+		int GetPasswordDisitNum() const { return m_passwordDisitNum; }
+
+		bool IsComplete() const { return m_passwordText.size() >= m_passwordDisitNum; }
+	};
+}
+}
