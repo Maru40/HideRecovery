66cb50a64a6efb4fd8e5ab7a61f1b6ef644e5da0
Itabashi Oota <itaoo0430@gmail.com>
2022-11-02 09:43:57 +0900

    文字入力管理クラスを作成し使用する

---
+++ SCRAMBLE BALL/GameSources/Itabashi/Utility/InputTextManager.cpp
@@ -0,0 +1,17 @@
+﻿#include "InputTextManager.h"
+
+namespace itbs
+{
+namespace Input
+{
+	std::unordered_set<I_InputText*> InputTextManager::m_inputTexts;
+
+	void InputTextManager::Push(wchar_t wc)
+	{
+		for (auto& inputText : m_inputTexts)
+		{
+			inputText->Push(wc);
+		}
+	}
+}
+}
---
+++ SCRAMBLE BALL/GameSources/Itabashi/Utility/InputTextManager.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+#include <unordered_set>
+
+namespace itbs
+{
+namespace Input
+{
+	class I_InputText
+	{
+	public:
+		virtual bool Push(wchar_t wc) = 0;
+
+		virtual const wchar_t* GetText() const = 0;
+
+		virtual void Clear() = 0;
+	};
+
+	class InputTextManager
+	{
+		static std::unordered_set<I_InputText*> m_inputTexts;
+
+	public:
+
+		InputTextManager() = delete;
+
+		static void Push(wchar_t wc);
+
+		static void AddInputText(I_InputText* inputText) { m_inputTexts.insert(inputText); }
+		static void RemoveInputText(I_InputText* inputText) { m_inputTexts.erase(inputText); }
+	};
+}
+}
