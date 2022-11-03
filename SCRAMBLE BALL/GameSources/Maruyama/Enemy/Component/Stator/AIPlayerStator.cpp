b9d4adb9b2f528ffcc30632ceed87517739cce48
Maru40 <umeda.4072@gmail.com>
2022-11-03 22:13:55 +0900

    AIPlayerStatorの途中登録

--- SCRAMBLE BALL/GameSources/Maruyama/Enemy/Component/Stator/AIPlayerStator.cpp
+++ SCRAMBLE BALL/GameSources/Maruyama/Enemy/Component/Stator/AIPlayerStator.cpp
@@ -16,8 +16,22 @@
 #include "Maruyama/Enemy/StateMachine/EnemyMainStateMachine.h"
 
 #include "Maruyama/Enemy/Component/Stator/StateNode/StateNode_HidePlacePatrol.h"
+#include "Maruyama/Enemy/Component/Stator/StateNode/StateNode_Buttle.h"
+
+#include "Maruyama/Enemy/Component/EyeSearchRange.h"
+#include "Maruyama/Utility/ObserveIsInEyeTarget.h"
 
 #include "Maruyama/Utility/SingletonComponent/GameManager.h"
+
+#include "Maruyama/Interface/I_FactionMember.h"
+#include "Maruyama/Interface/I_TeamMember.h"
+#include "Maruyama/Enemy/AIDirector/EnemyAIDirector.h"
+#include "Maruyama/Enemy/AIDirector/FactionCoordinator.h"
+#include "Maruyama/Enemy/AIDirector/TupleSpace.h"
+
+#include "Maruyama/Utility/SingletonComponent/ShareClassesManager.h"
+
+#include "Maruyama/Utility/Component/TargetManager.h"
 
 namespace basecross {
 	namespace Enemy {
@@ -31,13 +45,46 @@ namespace basecross {
 			return GameManager::GetInstance()->IsCurrentState(GameManager::State::Game);
 		}
 
+		bool AIPlayerStator::IsFindButtleTarget(const TransitionMember& member) {
+			auto targets = m_observeButtleTarget->SearchIsInEyeTargets();
+
+			if (targets.empty()) {
+				return false;
+			}
+
+			//ターゲットを見つけたことをAIDirectorに伝える。
+			for (auto& target : targets) {
+				auto factionMember = m_factionMember.lock();
+				auto tupleSpace = factionMember->GetAssignedFaction()->GetTupleSpace();
+				tupleSpace->Write<Tuple::FindTarget>(GetGameObject(), target);	//ターゲットを通知
+			}
+
+			//仮遷移実装
+			//bool isTransition = targets.size();
+			//if (isTransition) {
+			//	GetGameObject()->GetComponent<TargetManager>()->SetTarget(targets[0]);
+			//}
+
+			return false;
+		}
+
 		//--------------------------------------------------------------------------------------
 		///	AIPlayerStator本体
 		//--------------------------------------------------------------------------------------
 
 		AIPlayerStator::AIPlayerStator(const std::shared_ptr<GameObject>& objPtr):
-			StatorBase(objPtr)
-		{}
+			StatorBase(objPtr),
+			m_eye(objPtr->GetComponent<EyeSearchRange>()),
+			m_observeButtleTarget(new ObserveIsInEyeTarget(m_eye.lock()))
+		{
+			m_teamMember = objPtr->GetComponent<I_TeamMember>(false);
+			m_factionMember = objPtr->GetComponent<I_FactionMember>(false);
+		}
+
+		void AIPlayerStator::OnLateStart() {
+			//バトル用の監視対象をセッティング
+			SettingObserveButtleTargets();
+		}
 
 		void AIPlayerStator::CreateNode() {
 			auto enemy = GetGameObject()->GetComponent<EnemyBase>();
@@ -48,8 +95,8 @@ namespace basecross {
 			//隠し場所を探す状態
 			m_stateMachine->AddNode(StateType::HidePlacePatrol, std::make_shared<StateNode::HidePlacePatrol>(enemy));
 
-			//攻撃
-
+			//バトル
+			m_stateMachine->AddNode(StateType::Buttle, std::make_shared<StateNode::Buttle>(enemy));
 		}
 
 		void AIPlayerStator::CreateEdge() {
@@ -57,10 +104,32 @@ namespace basecross {
 
 			//None
 			m_stateMachine->AddEdge(StateType::None, StateType::HidePlacePatrol, &IsGameState);
+			//m_stateMachine->AddEdge(StateType::None, StateType::Buttle, &IsGameState);
 
 			//隠し場所を探す
+			m_stateMachine->AddEdge(
+				StateType::HidePlacePatrol, 
+				StateType::Buttle, 
+				[&](const TransitionMember& member) { return IsFindButtleTarget(member); }
+			);
 
-			//攻撃
+			//バトル
+		}
+
+		void AIPlayerStator::SettingObserveButtleTargets() {
+			auto selfTeamMember = GetGameObject()->GetComponent<I_TeamMember>(false);
+			if (!selfTeamMember) {
+				return;
+			}
+
+			//別チームをバトル監視対象にする。
+			auto teamMembers = ShareClassesManager::GetInstance()->GetCastShareClasses<I_TeamMember>();
+			for (auto& member : teamMembers) {
+				//別チームなら、監視対象に追加
+				if (member.lock()->GetTeam() != selfTeamMember->GetTeam()) {
+					m_observeButtleTarget->AddObserveTarget(member.lock()->GetOwnerObject());
+				}
+			}
 		}
 
 	}
--- SCRAMBLE BALL/GameSources/Maruyama/Enemy/Component/Stator/AIPlayerStator.h
+++ SCRAMBLE BALL/GameSources/Maruyama/Enemy/Component/Stator/AIPlayerStator.h
@@ -16,11 +16,14 @@ namespace basecross {
 	//--------------------------------------------------------------------------------------
 	///	前方宣言
 	//--------------------------------------------------------------------------------------
-	
+	class EyeSearchRange;
+	class ObserveIsInEyeTarget;
+	class I_TeamMember;
 
 	namespace Enemy {
 
 		class EnemyBase;
+		class I_FactionMember;
 
 		//--------------------------------------------------------------------------------------
 		///	AIPlayerStatorのステートタイプ
@@ -28,13 +31,15 @@ namespace basecross {
 		enum class AIPlayerStator_StateType {
 			None,
 			HidePlacePatrol,	//隠し場所探す。
-			Attack,				//攻撃
+			Buttle,				//バトル
 		};
 
 		//--------------------------------------------------------------------------------------
 		///	AIPlayerStatorの遷移条件メンバー
 		//--------------------------------------------------------------------------------------
 		struct AIPlayerStator_TransitionMember {
+			float hidePatrolEeyRange = 5.0f;	//隠し場所を探しているときの視界範囲
+
 			AIPlayerStator_TransitionMember() = default;
 		};
 
@@ -48,15 +53,39 @@ namespace basecross {
 			using TransitionMember = AIPlayerStator_TransitionMember;
 
 		private:
+			std::weak_ptr<EyeSearchRange> m_eye;			//視界管理
+			std::weak_ptr<I_TeamMember> m_teamMember;		//チームメンバーインターフェース
+			std::weak_ptr<I_FactionMember> m_factionMember;	//ファクションメンバーインターフェース
 
+			std::unique_ptr<ObserveIsInEyeTarget> m_observeButtleTarget;	//バトル用に監視したい対象
 
 		public:
 			AIPlayerStator(const std::shared_ptr<GameObject>& objPtr);
 
 			virtual ~AIPlayerStator() = default;
 
+			void OnLateStart() override;
+
 			void CreateNode() override;
 			void CreateEdge() override;
+
+		private:
+
+			/// <summary>
+			/// バトル対象として監視するターゲットをセッティング
+			/// </summary>
+			void SettingObserveButtleTargets();
+
+			//--------------------------------------------------------------------------------------
+			///	遷移条件系
+			//--------------------------------------------------------------------------------------
+
+			/// <summary>
+			/// バトルターゲット発見
+			/// </summary>
+			/// <param name="member">遷移条件メンバー</param>
+			/// <returns>バトルターゲット</returns>
+			bool IsFindButtleTarget(const TransitionMember& member);
 
 		};
 
