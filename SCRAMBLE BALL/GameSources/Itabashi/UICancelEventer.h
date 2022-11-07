cc0b67441a95058c604c56f5d5c03afab00d3252
Itabashi Oota <itaoo0430@gmail.com>
2022-11-07 15:03:17 +0900

    キャンセルイベント用コンポーネントを作成

---
+++ SCRAMBLE BALL/GameSources/Itabashi/UICancelEventer.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+
+namespace basecross
+{
+	class UICancelEventer : public Component, public I_Cancelable
+	{
+		std::vector<std::function<void()>> m_cancelEvents;
+	public:
+		UICancelEventer(const std::shared_ptr<GameObject>& owner);
+
+		void OnCancel() override;
+
+		void AddCancelEvent(const std::function<void()>& cancelEvent) { m_cancelEvents.push_back(cancelEvent); }
+	};
+}
