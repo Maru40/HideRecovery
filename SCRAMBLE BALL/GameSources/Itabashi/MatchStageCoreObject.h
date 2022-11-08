7ae0f299ed15881abc4b5c603edc11517e4b7849
Itabashi Oota <itaoo0430@gmail.com>
2022-11-08 15:42:36 +0900

    MatchStageの重要なコンポーネントを持ったオブジェクトを作成

---
+++ SCRAMBLE BALL/GameSources/Itabashi/MatchStageCoreObject.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+namespace basecross
+{
+namespace StageObject
+{
+	/// <summary>
+	/// MatchStageの重要なコンポーネントを持ったオブジェクト
+	/// </summary>
+	class MatchStageCoreObject : public GameObject
+	{
+	public:
+		MatchStageCoreObject(const std::shared_ptr<Stage>& stage);
+
+		void OnCreate() override;
+	};
+}
+}
