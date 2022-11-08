075ef331d35ee80e19235cbd6b276ff6b07095c8
Itabashi Oota <itaoo0430@gmail.com>
2022-11-08 16:00:04 +0900

    PasswordTextUIを表示に対応

--- SCRAMBLE BALL/GameSources/Itabashi/PasswordTextUI.cpp
+++ SCRAMBLE BALL/GameSources/Itabashi/PasswordTextUI.cpp
@@ -1,4 +1,5 @@
 ﻿#include "PasswordTextUI.h"
+#include "Watanabe/UI/Numbers.h"
 
 namespace basecross
 {
@@ -6,15 +7,22 @@ namespace UI
 {
 	bool PasswordTextUI::TryPush(wchar_t wc)
 	{
+		if (!IsSelected())
+		{
+			return false;
+		}
+		
 		// バックスペースが押されたら
 		if (wc == VK_BACK)
 		{
 			if (!m_passwordText.empty())
 			{
 				m_passwordText.pop_back();
+				
+				m_passwordNumber /= 10;
 			}
 
-			return false;
+			return true;
 		}
 
 		// パスワード桁数に達していたら
@@ -35,6 +43,10 @@ namespace UI
 		if (L'0' <= wc && wc <= L'9')
 		{
 			m_passwordText.push_back(wc);
+
+			m_passwordNumber *= 10;
+			m_passwordNumber += wc - L'0';
+
 			return true;
 		}
 
@@ -42,10 +54,10 @@ namespace UI
 	}
 
 	PasswordTextUI::PasswordTextUI(const std::shared_ptr<GameObject>& owner) :
-		Component(owner)
+		SelectableComponent(owner)
 	{
 		itbs::Input::InputTextManager::AddInputText(this);
-		m_passwordText.reserve(DEFAULT_PASSWORD_DIST_NUM);
+		m_passwordText.reserve(m_passwordDisitNum);
 	}
 
 	PasswordTextUI::~PasswordTextUI()
@@ -65,21 +77,50 @@ namespace UI
 
 	bool PasswordTextUI::Push(wchar_t wc)
 	{
+		// 入力に変更がなければ
 		if (!TryPush(wc))
 		{
 			return false;
 		}
 
+		auto numbers = m_numbers.lock();
+
+		if (!numbers)
+		{
+			return true;
+		}
+
+		int outputNumber = m_passwordNumber * static_cast<int>(std::pow(10, m_passwordDisitNum - m_passwordText.size()));
+
+		numbers->SetNumber(outputNumber);
+
 		return true;
 	}
 
-	void PasswordTextUI::SetPasswordDisitNum(int passwordDisitNum)
+	void PasswordTextUI::Clear()
 	{
-		m_passwordDisitNum = passwordDisitNum;
+		m_passwordText.clear();
+		m_passwordNumber = 0;
 
-		if (m_passwordDisitNum > m_passwordText.capacity())
+		auto numbers = m_numbers.lock();
+
+		if (numbers)
 		{
-			m_passwordText.reserve(passwordDisitNum);
+			numbers->SetNumber(0);
+		}
+	}
+
+	void PasswordTextUI::OnSubmit()
+	{
+		if (!IsComplete())
+		{
+			return;
+		}
+
+		for (const auto& submitEvent : m_submitEvents)
+		{
+			submitEvent(m_passwordText);
+			SimpleSoundManager::OnePlaySE(m_decisionSEKey);
 		}
 	}
 }
