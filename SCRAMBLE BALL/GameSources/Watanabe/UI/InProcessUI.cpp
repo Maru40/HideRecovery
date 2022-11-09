534df585348837c154f3ab043c1766ee73229c90
Takumi Watanabe <zeal20300166@fsgcl.jp>
2022-11-09 14:24:38 +0900

    処理中を示すUIオブジェクトを作成

---
+++ SCRAMBLE BALL/GameSources/Watanabe/UI/InProcessUI.cpp
@@ -0,0 +1,23 @@
+﻿/*!
+@file   InProcessUI.cpp
+@brief  処理中を示すUIの実体
+*/
+
+#include "stdafx.h"
+#include "InProcessUI.h"
+
+namespace basecross {
+	InProcessUI::InProcessUI(const shared_ptr<Stage>& stage)
+		:UIObjectBase(stage, L"InProcessUI")
+	{}
+
+	InProcessUI::InProcessUI(const shared_ptr<Stage>& stage, const wstring& line)
+		: UIObjectBase(stage, L"InProcessUI")
+	{
+	}
+
+	void InProcessUI::OnCreate() {
+	}
+	void InProcessUI::OnUpdate() {
+	}
+}
---
+++ SCRAMBLE BALL/GameSources/Watanabe/UI/InProcessUI.h
@@ -0,0 +1,21 @@
+﻿/*!
+@file   InProcessUI.h
+@brief  処理中を示すUI
+*/
+
+#pragma once
+#include "stdafx.h"
+#include "UIObjectBase.h"
+
+namespace basecross {
+	class InProcessUI :public UIObjectBase {
+		// 処理中か
+		bool m_isProcessing;
+	public:
+		InProcessUI(const shared_ptr<Stage>& stage);
+		InProcessUI(const shared_ptr<Stage>& stage, const wstring& line);
+
+		void OnCreate()override;
+		void OnUpdate()override;
+	};
+}
