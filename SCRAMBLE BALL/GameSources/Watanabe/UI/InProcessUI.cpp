9b4056dc67dc607770d3b109dc8520cace104a70
Takumi Watanabe <zeal20300166@fsgcl.jp>
2022-11-09 14:49:43 +0900

    ラベルを表示できるようにした

--- SCRAMBLE BALL/GameSources/Watanabe/UI/InProcessUI.cpp
+++ SCRAMBLE BALL/GameSources/Watanabe/UI/InProcessUI.cpp
@@ -5,8 +5,15 @@
 
 #include "stdafx.h"
 #include "InProcessUI.h"
+#include "../Utility/DataExtracter.h"
 
 namespace basecross {
+	map<InProcessUI::LabelType, Message::MessageData> InProcessUI::sm_Type2DataMap =
+	{
+		{LabelType::Matching,Message::MessageData(Type::SpriteData,L"Matching")},
+		{LabelType::Busy    ,Message::MessageData(Type::SpriteData,L"Busy")}
+	};
+
 	InProcessUI::InProcessUI(const shared_ptr<Stage>& stage)
 		:UIObjectBase(stage, L"InProcessUI")
 	{}
@@ -14,10 +21,25 @@ namespace basecross {
 	InProcessUI::InProcessUI(const shared_ptr<Stage>& stage, const wstring& line)
 		: UIObjectBase(stage, L"InProcessUI")
 	{
+		vector<wstring> tokens = DataExtracter::DelimitData(line);
+		size_t nextIndex = DataExtracter::RectTransformDataExtraction(tokens, m_rectTransformData);
 	}
 
 	void InProcessUI::OnCreate() {
+		m_label = GetStage()->AddGameObject<SimpleSprite>();
+		m_label->SetParent(GetThis<InProcessUI>());
+		//m_label->SetActive(false);
 	}
 	void InProcessUI::OnUpdate() {
+		// 描画していないときは何もしない
+		// ※SetActiveで表示非表示を設定しているため
+		// 　実際はなくても動作する（多分）
+		if (!GetDrawActive())
+			return;
+	}
+
+	void InProcessUI::SetLabel(LabelType messageType) {
+		auto data = sm_Type2DataMap[messageType];
+		m_label->ChangeSprite(data.Type, data.Key);
 	}
 }
--- SCRAMBLE BALL/GameSources/Watanabe/UI/InProcessUI.h
+++ SCRAMBLE BALL/GameSources/Watanabe/UI/InProcessUI.h
@@ -6,16 +6,37 @@
 #pragma once
 #include "stdafx.h"
 #include "UIObjectBase.h"
+#include "SplashMessageUI.h"
 
 namespace basecross {
 	class InProcessUI :public UIObjectBase {
-		// 処理中か
-		bool m_isProcessing;
+		using Type = SimpleSprite::Type;
+	public:
+		/// <summary>
+		/// 表示するラベルタイプ
+		/// </summary>
+		enum class LabelType {
+			Matching,	// マッチング
+			Busy,		// 通信中
+		};
+	private:
+		// ラベルに対応するデータマップ
+		static map<LabelType, Message::MessageData> sm_Type2DataMap;
+		// ラベルオブジェクト
+		shared_ptr<SimpleSprite> m_label;
+		// デフォルトの色
+		Col4 m_defaultColor;
 	public:
 		InProcessUI(const shared_ptr<Stage>& stage);
 		InProcessUI(const shared_ptr<Stage>& stage, const wstring& line);
 
 		void OnCreate()override;
 		void OnUpdate()override;
+
+		/// <summary>
+		/// 表示するラベルを設定
+		/// </summary>
+		/// <param name="messageType">ラベルタイプ</param>
+		void SetLabel(LabelType messageType);
 	};
 }
