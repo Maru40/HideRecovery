346d36457c7a047c3804f07b1d69ce52312f1422
Maru40 <umeda.4072@gmail.com>
2022-11-09 11:06:46 +0900

    StateNodeのDyningStateを実装

---
+++ SCRAMBLE BALL/GameSources/Maruyama/Enemy/Component/Stator/StateNode/StateNode_Dead.cpp
@@ -0,0 +1,38 @@
+/*!
+@file StateNode_Dead.cpp
+@brief StateNode_Dead‚È‚ÇŽÀ‘Ì
+*/
+
+#include "stdafx.h"
+#include "Project.h"
+
+#include "StateNode_Dead.h"
+
+namespace basecross {
+	namespace Enemy {
+
+		namespace StateNode {
+
+			Dead::Dead(const std::shared_ptr<EnemyBase>& owner) :
+				EnemyStateNodeBase(owner)
+			{
+
+			}
+
+			void Dead::OnStart() {
+				StartChangeComponents();
+
+
+			}
+
+			bool Dead::OnUpdate() {
+				return false;
+			}
+
+			void Dead::OnExit() {
+				ExitChangeComponents();
+			}
+
+		}
+	}
+}
