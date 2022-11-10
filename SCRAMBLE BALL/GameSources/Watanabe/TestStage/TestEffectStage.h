e7d329701ff40ccc4779278e3cd4ffbb8766bc5a
Takumi Watanabe <zeal20300166@fsgcl.jp>
2022-11-10 11:03:27 +0900

    テストステージを更新

--- SCRAMBLE BALL/GameSources/Watanabe/TestStage/TestEffectStage.h
+++ SCRAMBLE BALL/GameSources/Watanabe/TestStage/TestEffectStage.h
@@ -2,11 +2,13 @@
 #include "stdafx.h"
 #include "../Utility/GameController.h"
 #include "../Effekseer/EfkEffect.h"
+#include "../UI/UIObjectBase.h"
 
 namespace basecross {
 	class TestEffectStage :public Stage {
 		GameController m_controller;
 		shared_ptr<EfkComponent> m_efkComp;
+		shared_ptr<UIObjectBase> m_ui;
 		void CreateViewLight();
 	public:
 		TestEffectStage() :Stage(), m_controller(0) {}
