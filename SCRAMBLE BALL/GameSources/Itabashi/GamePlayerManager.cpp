2eaffb7090e0e918d9e8cfe6532e4ff30451cd33
Itabashi Oota <itaoo0430@gmail.com>
2022-11-11 09:35:28 +0900

    オンラインプレイヤー番号をAIに対応させた

--- SCRAMBLE BALL/GameSources/Itabashi/GamePlayerManager.cpp
+++ SCRAMBLE BALL/GameSources/Itabashi/GamePlayerManager.cpp
@@ -3,6 +3,7 @@
 #include "OnlineMatching.h"
 #include "Itabashi/OtherPlayerObject.h"
 #include "Itabashi/SelfPlayerObject.h"
+#include "Maruyama/Enemy/Object/AIPlayerObject.h"
 
 namespace basecross
 {
@@ -12,30 +13,38 @@ namespace basecross
 		m_managedControllers.resize(Online::OnlineMatching::MAX_PLAYER_NUM);
 	}
 
-	std::shared_ptr<PlayerObject> GamePlayerManager::CreatePlayerObject(int gameNumber, int playerNumber)
+	std::shared_ptr<PlayerObject> GamePlayerManager::CreatePlayerObject(int playerNumber)
 	{
-		std::shared_ptr<PlayerObject> playerObject;
+		// プレイヤー番号が無効なら
+		if (playerNumber == Online::OnlineManager::INVALID_ONLINE_PLAYER_NUMBER)
+		{
+			return nullptr;
+		}
+
+		const auto& localPlayer = Online::OnlineManager::GetLocalPlayer();
 
 		// 自分自身なら
-		if (playerNumber == Online::OnlineManager::GetLocalPlayer().getNumber())
+		if (playerNumber == localPlayer.getNumber())
 		{
-			playerObject = GetStage()->AddGameObject<StageObject::SelfPlayerObject>();
-		}
-		else
-		{
-			playerObject = GetStage()->AddGameObject<StageObject::OtherPlayerObject>();
+			return GetStage()->AddGameObject<StageObject::SelfPlayerObject>();
 		}
 
-		playerObject->OnlineSetting(gameNumber, playerNumber);
+		// AIプレイヤーであり、ホストであるなら
+		if (playerNumber < 0 && localPlayer.getIsMasterClient())
+		{
+			return GetStage()->AddGameObject<Enemy::AIPlayerObject>();
+		}
 
-		return playerObject;
+		return GetStage()->AddGameObject<StageObject::OtherPlayerObject>();
 	}
 
 	void GamePlayerManager::OnLateStart()
 	{
 		for (int i = 0; i < Online::OnlineMatching::MAX_PLAYER_NUM; ++i)
 		{
-			auto playerObject = CreatePlayerObject(i, Online::OnlineMatching::GetPlayerNumberToGameNumber(i));
+			int playerNumber = Online::OnlineMatching::GetPlayerNumberToGameNumber(i);
+			auto playerObject = CreatePlayerObject(playerNumber);
+			playerObject->OnlineSetting(i, playerNumber);
 		}
 	}
 }
