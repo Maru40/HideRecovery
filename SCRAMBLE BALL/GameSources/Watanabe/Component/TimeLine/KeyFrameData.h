2e8bf54a66dcc36959a5fe3eded5f0f2e16fb998
Takumi Watanabe <zeal20300166@fsgcl.jp>
2022-11-11 15:47:14 +0900

    キーフレーム用のデータを作成

---
+++ SCRAMBLE BALL/GameSources/Watanabe/Component/TimeLine/KeyFrameData.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+#include "stdafx.h"
+
+namespace basecross {
+	template<class T>
+	struct KeyFrameData {
+		float Time;			// キーフレーム位置
+		Lerp::rate Rate;	// このフレームからの保管方法
+
+		/// <summary>
+		/// 補間する
+		/// </summary>
+		virtual void Interpolation(const shared_ptr<T>& keyFrame) = 0;
+
+		bool operator<(const KeyFrameData& right) const {
+			return Time < right.Time;
+		}
+	};
+
+	struct CameraKeyFrameData :public KeyFrameData<CameraKeyFrameData> {
+		Vec3 Eye;
+		Vec3 At;
+
+		void Interpolation(const shared_ptr<CameraKeyFrameData>& keyFrame)override {
+		}
+	};
+}
