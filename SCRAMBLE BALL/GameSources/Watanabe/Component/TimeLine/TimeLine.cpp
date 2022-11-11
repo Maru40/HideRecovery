c03624183b8f0e55bc7ad8b77c4d524726fb1eb9
Takumi Watanabe <zeal20300166@fsgcl.jp>
2022-11-11 15:40:34 +0900

    すべてのオブジェクトのアニメーションを管理するTimeLineクラスを作成

---
+++ SCRAMBLE BALL/GameSources/Watanabe/Component/TimeLine/TimeLine.cpp
@@ -0,0 +1,13 @@
+﻿#include "stdafx.h"
+#include "TimeLine.h"
+
+namespace basecross {
+	TimeLine::TimeLine(const shared_ptr<GameObject>& owner)
+		:Component(owner)
+	{}
+
+	void TimeLine::OnCreate() {
+	}
+	void TimeLine::OnUpdate() {
+	}
+}
---
+++ SCRAMBLE BALL/GameSources/Watanabe/Component/TimeLine/TimeLine.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+#include "stdafx.h"
+
+namespace basecross {
+	class TimeLine :public Component {
+	public:
+		TimeLine(const shared_ptr<GameObject>& owner);
+
+		void OnCreate()override;
+		void OnUpdate()override;
+		void OnDraw()override {}
+	};
+}
