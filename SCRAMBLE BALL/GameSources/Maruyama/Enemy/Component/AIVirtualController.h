e8a6a1eafc8744d8f54479c03586d6137703f1b7
Maru40 <umeda.4072@gmail.com>
2022-11-11 15:31:34 +0900

    AIVirtualControllerの実装

---
+++ SCRAMBLE BALL/GameSources/Maruyama/Enemy/Component/AIVirtualController.h
@@ -0,0 +1,28 @@
+
+/*!
+@file AIVirtualController.h
+@brief AIVirtualController‚È‚Ç
+’S“–FŠÛR—TŠì
+*/
+
+#pragma once
+#include "stdafx.h"
+
+namespace basecross {
+
+	class AIVirtualController : public Component
+	{
+		Vec2 m_inputDirection;			//“ü—Í•ûŒü
+		Vec2 m_targetInputDirection;	//–Ú•W‚Ì“ü—Í•ûŒü
+
+	public:
+		AIVirtualController(const std::shared_ptr<GameObject>& objPtr);
+
+		void OnUpdate() override;
+
+		void SetInputDirection(const Vec2& direction);
+
+		Vec2 GetInputDirection() const noexcept { return m_inputDirection; }
+	};
+
+}
