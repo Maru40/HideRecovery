31d5bce364a13d8a5095666db45e79c2de6d518a
Maru40 <umeda.4072@gmail.com>
2022-11-12 04:53:53 +0900

    AIVirtualControllerのいらない処理削除

--- SCRAMBLE BALL/GameSources/Maruyama/Enemy/Component/AIVirtualController.h
+++ SCRAMBLE BALL/GameSources/Maruyama/Enemy/Component/AIVirtualController.h
@@ -13,12 +13,9 @@ namespace basecross {
 	class AIVirtualController : public Component
 	{
 		Vec2 m_inputDirection;			//“ü—Í•ûŒü
-		Vec2 m_targetInputDirection;	//–Ú•W‚Ì“ü—Í•ûŒü
 
 	public:
 		AIVirtualController(const std::shared_ptr<GameObject>& objPtr);
-
-		void OnUpdate() override;
 
 		void SetInputDirection(const Vec2& direction);
 
