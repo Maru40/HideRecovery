38530149105a1df7f780047f8f988cbf5e80fe8b
Itabashi Oota <itaoo0430@gmail.com>
2022-11-12 14:38:22 +0900

    PasscodeUIに選択矢印を追加

--- SCRAMBLE BALL/GameSources/Watanabe/UI/PasscodeUI.cpp
+++ SCRAMBLE BALL/GameSources/Watanabe/UI/PasscodeUI.cpp
@@ -10,22 +10,39 @@ namespace basecross {
 
 	void PasscodeUI::OnCreate()
 	{
-		auto label = GetStage()->AddGameObject<SimpleSprite>(
-			SimpleSprite::Type::SpriteData, L"EnterPasscode"
-			);
+		auto ptrThis = GetThis<GameObject>();
+		std::shared_ptr<RectTransform> rectTransform;
+
+		auto label = GetStage()->AddGameObject<SimpleSprite>(SimpleSprite::Type::SpriteData, L"EnterPasscode");
+		label->SetParent(ptrThis);
+		rectTransform = label->GetRectTransform();
+		rectTransform->SetPosition(0, 100);
 
 		auto passcodeNumbers = GetStage()->AddGameObject<Numbers>(4);
-
-		label->GetRectTransform()->SetPosition(0, 100);
+		passcodeNumbers->SetParent(GetThis<PasscodeUI>());
 		passcodeNumbers->GetRectTransform()->SetPosition(0, -100);
 
-		label->SetParent(GetThis<PasscodeUI>());
-		passcodeNumbers->SetParent(GetThis<PasscodeUI>());
+		Vec2 triangleScale = Vec2(0.35f, 0.35f);
+
+		auto upTriangle = GetStage()->AddGameObject<SimpleSprite>(SimpleSprite::Type::SpriteData, L"Triangle");
+		upTriangle->SetParent(passcodeNumbers);
+		rectTransform = upTriangle->GetRectTransform();
+		rectTransform->SetPosition(0, 120);
+		rectTransform->SetRotation(XM_PI);
+		rectTransform->SetScale(triangleScale);
+
+		auto downTriangle = GetStage()->AddGameObject<SimpleSprite>(SimpleSprite::Type::SpriteData, L"Triangle");
+		downTriangle->SetParent(passcodeNumbers);
+		rectTransform = downTriangle->GetRectTransform();
+		rectTransform->SetPosition(0, -120);
+		rectTransform->SetScale(triangleScale);
 
 		auto passwordTextUI = AddComponent<UI::PasswordTextUI>();
 
 		passwordTextUI->SetNumbers(passcodeNumbers);
 		passwordTextUI->SetDecisionSEKey(L"DecisionSE");
+		passwordTextUI->SetUpTriangle(upTriangle);
+		passwordTextUI->SetDownTriangle(downTriangle);
 
 		AddComponent<UICancelEventer>();
 	}
