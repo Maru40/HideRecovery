ecf30bc5fe5572557b740e557aefa47213abf52f
Maru40 <umeda.4072@gmail.com>
2022-11-14 04:32:09 +0900

    RelifMemberの実装

--- SCRAMBLE BALL/GameSources/Maruyama/Enemy/Behavior/Task/RelifMember.cpp
+++ SCRAMBLE BALL/GameSources/Maruyama/Enemy/Behavior/Task/RelifMember.cpp
@@ -21,6 +21,7 @@
 #include "Maruyama/TaskList/CommonTasks/TargetSeek.h"
 #include "Maruyama/TaskList/CommonTasks/OpenBox.h"
 #include "Maruyama/TaskList/CommonTasks/Task_Wait.h"
+#include "Maruyama/TaskList/CommonTasks/Task_AroundEyeCheck.h"
 
 #include "Maruyama/Utility/SingletonComponent/SingletonComponent.h"
 #include "Maruyama/Enemy/ImpactMap/FieldImpactMap.h"
@@ -47,21 +48,175 @@ namespace basecross {
 
 			namespace Task {
 
-				RelifMember::RelifMember(const std::shared_ptr<Enemy::EnemyBase>& owner):
-					TaskBase(owner)
+				//--------------------------------------------------------------------------------------
+				///	見方の周りの警戒タスクパラメータ
+				//--------------------------------------------------------------------------------------
+
+				RelifMember_Parametor::RelifMember_Parametor() :
+					moveAstarParam(new basecross::Task::MoveAstar::Parametor()),
+					targetSeekParam(new TaskListNode::TargetSeek::Parametor()),
+					aroundEyeCheckParam(new basecross::Task::AroundEyeCheck_Parametor()),
+					waitParam(new basecross::Task::Wait::Parametor(0.5f))
 				{}
 
-				void RelifMember::OnStart() {
+				RelifMember_Parametor::~RelifMember_Parametor() {
+					delete(moveAstarParam);
+					delete(targetSeekParam);
+				}
 
+				//--------------------------------------------------------------------------------------
+				///	見方の周りの警戒タスク本体
+				//--------------------------------------------------------------------------------------
+
+				RelifMember::RelifMember(const std::shared_ptr<Enemy::EnemyBase>& owner):
+					TaskBase(owner),
+					m_param(Parametor()),
+					m_taskList(new TaskList<TaskEnum>())
+				{
+					InitializeParametor();
+					DefineTask();
+
+					auto object = GetOwner()->GetGameObject();
+					m_transform = object->GetComponent<Transform>();
+					m_eyeRange = object->GetComponent<EyeSearchRange>();
+					m_targetManager = object->GetComponent<TargetManager>();
+					m_velocityManager = object->GetComponent<VelocityManager>();
+					m_rotationController = object->GetComponent<RotationController>();
+					m_factionMember = object->GetComponent<Enemy::I_FactionMember>();
+				}
+
+				void RelifMember::OnStart() {
+					CalculateTarget();	//ターゲットの計算
+
+					SelectTask();		//タスクの選択
 				}
 
 				bool RelifMember::OnUpdate() {
-					return true;
+					m_taskList->UpdateTask();
+					Rotation();
+					CheckForceNextMoveArriveTask();
+
+					return IsEnd();
 				}
 
 				void RelifMember::OnExit() {
-
+					m_taskList->ForceStop();
 				}
+
+				std::shared_ptr<GameObject> RelifMember::CalculateTarget() {
+
+					return nullptr;
+				}
+
+				void RelifMember::CheckForceNextMoveArriveTask() {
+					if (m_taskList->IsEnd() || !m_targetManager.lock()->HasTarget()) {
+						return;
+					}
+
+					//Astar移動でなかったら処理をしない
+					if (m_taskList->GetCurrentTaskType() != TaskEnum::MoveAstar) {
+						return;
+					}
+
+					//ターゲットが障害物無の状態なら
+					auto startPosition = m_transform.lock()->GetPosition();
+					auto targetPosition = m_targetManager.lock()->GetTargetPosition();
+					auto toTargetPosition = targetPosition - startPosition;
+
+					//ターゲットの距離が視界距離より大きかったら、処理をしない。
+					if (m_eyeRange.lock()->GetEyeLength() < toTargetPosition.length()) {
+						return;
+					}
+
+					//Ray判定を取る。
+					auto objects = GetOwner()->GetGameObject()->GetStage()->GetGameObjectVec();
+					if (!maru::UtilityObstacle::IsRayObstacle(startPosition, targetPosition, objects)) {
+						m_taskList->ForceNextTask();
+					}
+				}
+
+				void RelifMember::DefineTask() {
+					auto ownerObject = GetOwner()->GetGameObject();
+
+					//Astar移動
+					m_taskList->DefineTask(
+						TaskEnum::MoveAstar,
+						std::make_shared<basecross::Task::MoveAstar>(GetOwner(), m_param.moveAstarParam)
+					);
+
+					//目的地の近くについたら時の到着移動
+					m_taskList->DefineTask(
+						TaskEnum::MoveArrive,
+						std::make_shared<TaskListNode::TargetSeek>(ownerObject, m_param.targetSeekParam)
+					);
+
+					//ターゲットの周りを警戒する。
+					m_taskList->DefineTask(
+						TaskEnum::AroundPtrol,
+						std::make_shared<basecross::Task::AroundEyeCheck>(ownerObject, m_param.aroundEyeCheckParam)
+					);
+
+					//待機行動
+					DefineWaitTask();
+				}
+
+				void RelifMember::DefineWaitTask() {
+					//開始イベント
+					m_param.waitParam->start = [&]() {
+
+					};
+
+					//終了イベント
+					m_param.waitParam->exit = [&]() {
+
+					};
+
+					//タスクの定義
+					m_taskList->DefineTask(
+						TaskEnum::Wait,
+						std::make_shared<basecross::Task::Wait>(m_param.waitParam)
+					);
+				}
+
+				void RelifMember::SelectTask() {
+					TaskEnum tasks[] = {
+						TaskEnum::MoveAstar,
+						TaskEnum::MoveArrive,
+						TaskEnum::AroundPtrol,
+						TaskEnum::Wait
+					};
+
+					for (const auto& task : tasks) {
+						m_taskList->AddTask(task);
+					}
+				}
+
+				void RelifMember::InitializeParametor() {
+					constexpr float MoveSpeed = 8.5f;
+					constexpr float NearTargetRange = 3.5f;
+
+					//Astarで目標の近くまで移動するパラメータ
+					m_param.moveAstarParam->movePositionsParam->moveParamPtr->speed = MoveSpeed;
+					m_param.moveAstarParam->movePositionsParam->moveParamPtr->targetNearRange = 2.0f;
+					m_param.moveAstarParam->movePositionsParam->moveParamPtr->moveType = basecross::Task::ToTargetMove_MoveType::SeekVelocity;
+
+					//目的地の近くまで移動した場合の設定
+					m_param.targetSeekParam->toTargetMoveParam->speed = MoveSpeed;
+					m_param.targetSeekParam->toTargetMoveParam->moveType = basecross::Task::ToTargetMove::MoveType::ArriveVelocity;
+					m_param.targetSeekParam->toTargetMoveParam->targetNearRange = NearTargetRange;
+				}
+
+				void RelifMember::Rotation() {
+					auto rotationController = m_rotationController.lock();
+					auto velocityManager = m_velocityManager.lock();
+					if (!velocityManager || !rotationController) {
+						return;
+					}
+
+					rotationController->SetDirection(velocityManager->GetVelocity());
+				}
+
+				bool RelifMember::IsEnd() const { return m_taskList->IsEnd(); }
 
 			}
 		}
--- SCRAMBLE BALL/GameSources/Maruyama/Enemy/Behavior/Task/RelifMember.h
+++ SCRAMBLE BALL/GameSources/Maruyama/Enemy/Behavior/Task/RelifMember.h
@@ -33,6 +33,7 @@ namespace basecross {
 		struct MoveAstar_Parametor;
 		struct ToTargetMove_Parametor;
 		struct Wait_Parametor;
+		struct AroundEyeCheck_Parametor;
 	}
 
 	namespace TaskListNode {
@@ -45,16 +46,98 @@ namespace basecross {
 
 			namespace Task {
 
+				//--------------------------------------------------------------------------------------
+				///	見方の周りの警戒タスクパラメータ
+				//--------------------------------------------------------------------------------------
+				struct RelifMember_Parametor {
+					basecross::Task::MoveAstar_Parametor* moveAstarParam;
+					TaskListNode::TargetSeek_Parametor* targetSeekParam;
+					std::shared_ptr<basecross::Task::AroundEyeCheck_Parametor> aroundEyeCheckParam;	//周囲警戒パラメータ
+					std::shared_ptr<basecross::Task::Wait_Parametor> waitParam;
+
+					RelifMember_Parametor();
+
+					virtual ~RelifMember_Parametor();
+				};
+
+				//--------------------------------------------------------------------------------------
+				///	見方の周りの警戒タスク
+				//--------------------------------------------------------------------------------------
 				class RelifMember : public TaskBase<Enemy::EnemyBase>
 				{
 				public:
+					using Parametor = RelifMember_Parametor;
+
+					enum class TaskEnum {
+						MoveAstar,			//Astarを利用してターゲットの近くまで移動する。
+						MoveArrive,			//ターゲットが視界内なら到着行動
+						AroundPtrol,		//周りを警戒
+						Wait,
+					};
+
+				private:
+					Parametor m_param;
+
+					std::unique_ptr<TaskList<TaskEnum>> m_taskList;	//タスクリスト
+
+					std::weak_ptr<Transform> m_transform;					//トランスフォーム
+					std::weak_ptr<EyeSearchRange> m_eyeRange;				//視界管理
+					std::weak_ptr<TargetManager> m_targetManager;			//ターゲット管理
+					std::weak_ptr<VelocityManager> m_velocityManager;		//速度管理
+					std::weak_ptr<RotationController> m_rotationController;	//向き管理
+					std::weak_ptr<Enemy::I_FactionMember> m_factionMember;	//ファクションメンバー
+
+				public:
 					RelifMember(const std::shared_ptr<Enemy::EnemyBase>& owner);
+
+					virtual ~RelifMember() = default;
 
 					void OnStart() override;
 
 					bool OnUpdate() override;
 
 					void OnExit() override;
+
+				private:
+
+					/// <summary>
+					/// ターゲットの取得
+					/// </summary>
+					/// <returns>ターゲット</returns>
+					std::shared_ptr<GameObject> CalculateTarget();
+
+					/// <summary>
+					/// 次のMoveArriveタスクに変更するかを監視する。
+					/// </summary>
+					void CheckForceNextMoveArriveTask();
+
+					/// <summary>
+					/// タスクの定義
+					/// </summary>
+					void DefineTask();
+
+					/// <summary>
+					/// 待機中のタスクの定義
+					/// </summary>
+					void DefineWaitTask();
+
+					/// <summary>
+					/// タスクの選択
+					/// </summary>
+					void SelectTask();
+
+					/// <summary>
+					/// パラメータの初期化
+					/// </summary>
+					void InitializeParametor();
+
+					/// <summary>
+					/// 向き調整
+					/// </summary>
+					void Rotation();
+
+				public:
+					bool IsEnd() const;
 				};
 
 			}
