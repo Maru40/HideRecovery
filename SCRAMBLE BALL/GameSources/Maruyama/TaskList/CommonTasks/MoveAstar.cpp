b46e0c4f4b72d9bb3d614830eb334cdce2b88651
Maru40 <umeda.4072@gmail.com>
2022-11-14 10:33:30 +0900

    MoveAstarのバグを修正

--- SCRAMBLE BALL/GameSources/Maruyama/TaskList/CommonTasks/MoveAstar.cpp
+++ SCRAMBLE BALL/GameSources/Maruyama/TaskList/CommonTasks/MoveAstar.cpp
@@ -129,14 +129,14 @@ namespace basecross {
 			}
 
 			//減速処理開始
-			m_velocityManager.lock()->StartDeseleration();
+			//m_velocityManager.lock()->StartDeseleration();
 
-			CalculateMovePositions();	//新しいポジションに変更
+			auto positions = CalculateMovePositions();	//新しいポジションに変更
 
-			SelectTask();				//タスクの再始動
+			SelectTask();	//タスクの再始動
 
 			//減速処理終了
-			m_velocityManager.lock()->SetIsDeseleration(false);
+			//m_velocityManager.lock()->SetIsDeseleration(false);
 			SetIsSearchRoute(false);	//検索終了
 		}
 
@@ -152,17 +152,11 @@ namespace basecross {
 			//エリアのルートを取得
 			auto areaRouteIndices = fieldImpactMap->SearchAreaRouteIndices(startPosition, targetPosition);
 
-			//Debug::GetInstance()->Log(L"AreaRoute-------------------");
-
-			//Debug::GetInstance()->Log(fieldImpactMap->SearchNearAreaIndex(startPosition));
-
 			//Astar検索が最初の自分自身のノードを省くため、最初は現在所属しているエリアから検索する。
 			m_areaRoute.push(fieldImpactMap->SearchNearAreaIndex(startPosition));
 			for (const auto& areaRouteIndex : areaRouteIndices) {
 				m_areaRoute.push(areaRouteIndex);
-				//Debug::GetInstance()->Log(areaRouteIndex);
 			}
-			//Debug::GetInstance()->Log(L"AreaRoute-------------------");p
 
 			return m_areaRoute;
 		}
@@ -222,6 +216,11 @@ namespace basecross {
 		//--------------------------------------------------------------------------------------
 
 		bool MoveAstar::IsEnd() const {
+			auto targetManager = m_targetManager.lock();
+			if (!targetManager || !targetManager->HasTarget()) {
+				return true;
+			}
+
 			return m_areaRoute.empty() && m_taskList->IsEnd();
 		}
 
