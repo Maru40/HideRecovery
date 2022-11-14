1c15fadaa879beea013531e10d2847d7b15eaf93
Takumi Watanabe <zeal20300166@fsgcl.jp>
2022-11-14 12:56:53 +0900

    タイムラインで実行するイベントを管理するクラスを実装

---
+++ SCRAMBLE BALL/GameSources/Watanabe/TimeLine/TimeLineEventDataList.cpp
@@ -0,0 +1,53 @@
+﻿/*!
+@file   TimeLineEventDataList.cpp
+@brief  タイムライン上に設定されるイベント管理クラス実体
+*/
+
+#include "stdafx.h"
+#include "TimeLineEventDataList.h"
+#include "../DebugClass/Debug.h"
+
+namespace basecross {
+	namespace timeline {
+		void TimeLineEventDataList::Initialize() {
+			// 実行順にソート
+			m_eventList.sort(
+				[](shared_ptr<TimeLineEventData> left, shared_ptr<TimeLineEventData> right) {
+					return left->ExecutingTime < right->ExecutingTime;
+				}
+			);
+			m_eventQueue = AdvQueue<shared_ptr<TimeLineEventData>>(m_eventList);
+		}
+
+		void TimeLineEventDataList::Update(float delta) {
+			if (m_eventQueue.empty())
+				return;
+
+			auto currentEventData = m_eventQueue.front();
+			// 指定した時間が経過した
+			if (delta > currentEventData->ExecutingTime) {
+				// 関数が有効であれば実行
+				if (auto func = currentEventData->Function)
+					func();
+
+				// 先頭を削除
+				m_eventQueue.pop();
+				// 同じ実行時間のイベントがあればなくなるまでループ
+				while (delta > m_eventQueue.front()->ExecutingTime) {
+					if (auto func = m_eventQueue.front()->Function)
+						func();
+					m_eventQueue.pop();
+				}
+			}
+		}
+		void TimeLineEventDataList::AddEvent(float executingTime, const Func& func) {
+			m_eventList.push_back(
+				make_shared<TimeLineEventData>(executingTime, func)
+			);
+		}
+
+		void TimeLineEventDataList::ClearEvent() {
+			m_eventList.clear();
+		}
+	}
+}
