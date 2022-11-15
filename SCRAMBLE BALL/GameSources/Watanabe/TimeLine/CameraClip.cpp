a200cd30ef139deb3ace83b6b40ac6fdbff90e2c
Takumi Watanabe <zeal20300166@fsgcl.jp>
2022-11-15 09:28:05 +0900

    カメラ用のClipクラスを作成

---
+++ SCRAMBLE BALL/GameSources/Watanabe/TimeLine/CameraClip.cpp
@@ -0,0 +1,10 @@
+﻿#include "stdafx.h"
+#include "CameraClip.h"
+
+namespace basecross {
+	namespace timeline {
+		CameraClip::CameraClip(const shared_ptr<GameObject>& owner)
+			:ClipBase(owner)
+		{}
+	}
+}
---
+++ SCRAMBLE BALL/GameSources/Watanabe/TimeLine/CameraClip.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+#include "stdafx.h"
+#include "ClipBase.h"
+
+namespace basecross {
+	namespace timeline {
+		class CameraClip :public ClipBase {
+		public:
+			CameraClip(const shared_ptr<GameObject>& owner);
+		};
+	}
+}
