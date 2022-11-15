42497b26319e081a4422c05ff68782262a9cbb97
Takumi Watanabe <zeal20300166@fsgcl.jp>
2022-11-15 09:35:15 +0900

    カメラ用のキーフレームクラスを作成

--- SCRAMBLE BALL/GameSources/Watanabe/TimeLine/CameraClip.h
+++ SCRAMBLE BALL/GameSources/Watanabe/TimeLine/CameraClip.h
@@ -1,12 +1,24 @@
 ﻿#pragma once
 #include "stdafx.h"
 #include "ClipBase.h"
+#include "CameraKeyFrame.h"
 
 namespace basecross {
 	namespace timeline {
 		class CameraClip :public ClipBase {
 		public:
 			CameraClip(const shared_ptr<GameObject>& owner);
+
+			/// <summary>
+			/// 補間する
+			/// </summary>
+			void Interpolation(
+				const shared_ptr<CameraKeyFrame>& current,
+				const shared_ptr<CameraKeyFrame>& next,
+				float delta) {
+				auto Eye = Lerp::CalculateLerp(current->Eye, next->Eye, current->Time, next->Time, delta, current->Rate);
+				auto At = Lerp::CalculateLerp(current->At, next->At, current->Time, next->Time, delta, current->Rate);
+			}
 		};
 	}
 }
---
+++ SCRAMBLE BALL/GameSources/Watanabe/TimeLine/CameraKeyFrame.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+#include "stdafx.h"
+#include "../Utility/TransformData.h"
+
+namespace basecross {
+	namespace timeline {
+		struct CameraKeyFrame {
+			Vec3 Eye;
+			Vec3 At;
+			float Time;			// キーフレーム位置
+			Lerp::rate Rate;	// このフレームからの保管方法
+
+			CameraKeyFrame(const Vec3& eye, const Vec3& at, float time, const Lerp::rate& rate)
+				:Eye(eye), At(at), Time(time), Rate(rate)
+			{}
+
+			// ソート用
+			bool operator<(const CameraKeyFrame& right) const {
+				return Time < right.Time;
+			}
+		};
+	}
+}
