1cbf7dcc15e8580c6aff12f3e0a30f219c86dfca
Itabashi Oota <itaoo0430@gmail.com>
2022-11-15 17:15:13 +0900

    マッチング時に接続が切れた際に表示するUIオブジェクトを作成

---
+++ SCRAMBLE BALL/GameSources/Itabashi/MatchStageReconnectUIObject.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+namespace basecross
+{
+namespace StageObject
+{
+	class MatchStageReconnectUIObject : public UIObject
+	{
+		std::weak_ptr<GameObject> m_backTitleButtonObject;
+		std::weak_ptr<GameObject> m_toContinueButtonObject;
+
+		std::shared_ptr<GameObject> CreateUIButtonObject(const std::shared_ptr<Stage>& stage, const std::wstring& spriteKey, float x, float y);
+	public:
+		MatchStageReconnectUIObject(const std::shared_ptr<Stage>& stage);
+
+		void OnCreate() override;
+
+		std::shared_ptr<GameObject> GetBackTitleButtonObject() const { return m_backTitleButtonObject.lock(); }
+		std::shared_ptr<GameObject> GetToContinueButtonObject() const { return m_toContinueButtonObject.lock(); }
+	};
+}
+}
