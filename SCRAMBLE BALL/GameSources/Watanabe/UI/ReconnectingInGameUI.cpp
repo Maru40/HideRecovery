7215cdb62b11d459dd336d915e3d9d50081fa12a
Takumi Watanabe <zeal20300166@fsgcl.jp>
2022-11-17 10:05:24 +0900

    ゲーム中に接続が切れたとき用のUIクラスを作成

---
+++ SCRAMBLE BALL/GameSources/Watanabe/UI/ReconnectingInGameUI.cpp
@@ -0,0 +1,11 @@
+﻿#include "stdafx.h"
+#include "ReconnectingInGameUI.h"
+
+namespace basecross {
+	ReconnectingInGameUI::ReconnectingInGameUI(const shared_ptr<Stage>& stage)
+		:UIObjectBase(stage, L"ReconnectingInGameUI")
+	{}
+
+	void ReconnectingInGameUI::OnCreate() {
+	}
+}
---
+++ SCRAMBLE BALL/GameSources/Watanabe/UI/ReconnectingInGameUI.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+#include "stdafx.h"
+#include "UIObjectBase.h"
+
+namespace basecross {
+	class InProcessUI;
+	class SplashMessageUI;
+
+	class ReconnectingInGameUI :public UIObjectBase {
+		weak_ptr<InProcessUI> m_inProcessUI;
+		vector<weak_ptr<UIObjectBase>> m_toTitleUIs;
+		weak_ptr<SplashMessageUI> m_splashMessageUI;
+	public:
+		ReconnectingInGameUI(const shared_ptr<Stage>& stage);
+
+		void OnCreate()override;
+		void OnUpdate()override {}
+
+		void SetSplashMessageUI(const shared_ptr<SplashMessageUI>& ui) { m_splashMessageUI = ui; }
+	};
+}
