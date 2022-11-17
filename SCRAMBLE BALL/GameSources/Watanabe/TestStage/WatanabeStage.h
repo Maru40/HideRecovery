285a46f8ef6320cc78ceda3fce428b5c15c0d9da
Takumi Watanabe <zeal20300166@fsgcl.jp>
2022-11-17 09:42:21 +0900

    UIテスト用に不要なコードを削除

--- SCRAMBLE BALL/GameSources/Watanabe/TestStage/WatanabeStage.h
+++ SCRAMBLE BALL/GameSources/Watanabe/TestStage/WatanabeStage.h
@@ -1,19 +1,10 @@
 ﻿#pragma once
 #include "stdafx.h"
-#include "../Interface/I_Performable.h"
-#include "Watanabe/UI/GameStartUI.h"
-#include "Watanabe/UI/GameFinishUI.h"
 
 namespace basecross {
 	class WatanabeStage :public Stage {
 		//ビューの作成
 		void CreateViewLight();
-		shared_ptr<GameObject> m_obj;
-		shared_ptr<I_Performable> m_per;
-		float m_delta = 0;
-		std::shared_ptr<GameStartUI> m_gameStartUI;
-		std::shared_ptr<GameFinishUI> m_gameFinishUI;
-
 	public:
 		WatanabeStage() :Stage() {}
 
