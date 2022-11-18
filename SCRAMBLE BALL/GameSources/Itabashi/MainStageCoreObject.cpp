3646c14dbeaf83f48b3ea45aca49fae4dcb1254f
Itabashi Oota <itaoo0430@gmail.com>
2022-11-18 15:02:37 +0900

    MainStageの重要な部分をCoreObjectとしてまとめた

---
+++ SCRAMBLE BALL/GameSources/Itabashi/MainStageCoreObject.cpp
@@ -0,0 +1,25 @@
+﻿#include "MainStageCoreObject.h"
+#include "OnlineGameTimer.h"
+#include "OnlineGameItemManager.h"
+#include "GamePlayerManager.h"
+#include "MainStageTransitioner.h"
+
+namespace basecross
+{
+namespace StageObject
+{
+	MainStageCoreObject::MainStageCoreObject(const std::shared_ptr<Stage>& stage) :
+		GameObject(stage)
+	{
+
+	}
+
+	void MainStageCoreObject::OnCreate()
+	{
+		AddComponent<OnlineGameTimer>();
+		AddComponent<OnlineGameItemManager>();
+		AddComponent<GamePlayerManager>();
+		AddComponent<Online::MainStageTransitioner>();
+	}
+}
+}
