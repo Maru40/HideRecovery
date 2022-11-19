c973a770c3ba9e86e4885bd9c0e18cc4665ce5a7
Itabashi Oota <itaoo0430@gmail.com>
2022-11-19 14:49:11 +0900

    オンラインの生存確認用コンポーネントを作成

---
+++ SCRAMBLE BALL/GameSources/Itabashi/OnlineAliveChecker.cpp
@@ -0,0 +1,94 @@
+﻿#include "OnlineAliveChecker.h"
+
+namespace basecross
+{
+namespace Online
+{
+	const ExitGames::LoadBalancing::RaiseEventOptions OnlineAliveChecker::m_raiseEventOptions =
+		ExitGames::LoadBalancing::RaiseEventOptions(0, ExitGames::Lite::EventCache::DO_NOT_CACHE, NULL, 0, ExitGames::Lite::ReceiverGroup::MASTER_CLIENT);
+
+	OnlineAliveChecker::OnlineAliveChecker(const std::shared_ptr<GameObject>& owner) :
+		OnlineComponent(owner)
+	{
+
+	}
+
+	void OnlineAliveChecker::MasterProcess(float deltaTime)
+	{
+		m_aliveCheckCountTime += deltaTime;
+
+		if (m_aliveCheckCountTime >= m_aliveCheckTime)
+		{
+			m_aliveCheckCountTime -= m_aliveCheckTime;
+			OnlineManager::RaiseEvent(true, nullptr, 0, CONNECT_ALIVE_CHECK_EVENT_CODE);
+			m_isWaitingResponse = true;
+			m_aliveResponseCountTime = 0.0f;
+			return;
+		}
+
+		if (!m_isWaitingResponse)
+		{
+			return;
+		}
+
+		m_aliveResponseCountTime += deltaTime;
+		
+		if (m_aliveResponseCountTime < m_aliveResponseTime || !m_isAlive)
+		{
+			return;
+		}
+
+		m_isAlive = false;
+	}
+
+	void OnlineAliveChecker::ClientProcess(float deltaTime)
+	{
+		m_aliveResponseCountTime += deltaTime;
+
+		if (m_aliveResponseCountTime < m_aliveCheckTime + m_aliveResponseTime)
+		{
+			return;
+		}
+
+		m_isAlive = false;
+	}
+
+	void OnlineAliveChecker::OnCreate()
+	{
+		const auto& inRoomPlayers = OnlineManager::GetCurrentlyJoinedRoom().getPlayers();
+
+
+		OnlineManager::GetCurrentlyJoinedRoom().getPlayers();
+	}
+
+	void OnlineAliveChecker::OnUpdate()
+	{
+		auto deltaTime = App::GetApp()->GetElapsedTime();
+
+		if (OnlineManager::GetLocalPlayer().getIsMasterClient())
+		{
+			MasterProcess(deltaTime);
+		}
+		else
+		{
+			ClientProcess(deltaTime);
+		}		
+	}
+
+	void OnlineAliveChecker::OnCustomEventAction(int playerNumber, std::uint8_t eventCode, const std::uint8_t* bytes)
+	{
+		if (eventCode == CONNECT_ALIVE_CHECK_EVENT_CODE)
+		{
+			OnlineManager::RaiseEvent(true, nullptr, 0, CONNECT_ALIVE_RESPONSE_EVENT_CODE, m_raiseEventOptions);
+			m_isAlive = true;
+			m_aliveResponseCountTime = 0.0f;
+			return;
+		}
+		if (eventCode == CONNECT_ALIVE_RESPONSE_EVENT_CODE)
+		{
+			m_isWaitingResponse = false;
+			m_isAlive = true;
+		}
+	}
+}
+}
---
+++ SCRAMBLE BALL/GameSources/Itabashi/OnlineAliveChecker.h
@@ -0,0 +1,44 @@
+﻿#pragma once
+#include "OnlineManager.h"
+
+namespace basecross
+{
+namespace Online
+{
+	class OnlineAliveChecker : public OnlineComponent
+	{
+	public:
+		static constexpr std::uint8_t CONNECT_ALIVE_CHECK_EVENT_CODE = 171;
+		static constexpr std::uint8_t CONNECT_ALIVE_RESPONSE_EVENT_CODE = 172;
+
+	private:
+		float m_aliveCheckTime = 1.0f;
+		float m_aliveResponseTime = 0.5f;
+
+		float m_aliveCheckCountTime = 0.0f;
+		float m_aliveResponseCountTime = 0.0f;
+
+		bool m_isWaitingResponse = false;
+
+		bool m_isAlive = true;
+
+		static const ExitGames::LoadBalancing::RaiseEventOptions m_raiseEventOptions;
+
+		void MasterProcess(float deltaTime);
+		void ClientProcess(float deltaTime);
+
+	public:
+		OnlineAliveChecker(const std::shared_ptr<GameObject>& owner);
+
+		void OnCreate() override;
+
+		void OnUpdate() override;
+
+		void OnCustomEventAction(int playerNumber, std::uint8_t eventCode, const std::uint8_t* bytes) override;
+
+		bool IsAlive() const { return m_isAlive; }
+
+		bool PlayerAlive(int onlinePlayerNumber);
+	};
+}
+}
