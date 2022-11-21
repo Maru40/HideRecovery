2f7bc9476e255e4400e80517b27ff0bca560abcf
Takumi Watanabe <zeal20300166@fsgcl.jp>
2022-11-21 15:35:57 +0900

    ボールオブジェクト用のイベント実行クラスを作成

---
+++ SCRAMBLE BALL/GameSources/Watanabe/Component/BallObjectEventExecuter.cpp
@@ -0,0 +1,35 @@
+﻿#include "stdafx.h"
+#include "BallObjectEventExecuter.h"
+
+namespace basecross {
+	BallObjectEventExecuter::BallObjectEventExecuter(const shared_ptr<GameObject>& owner)
+		:Component(owner)
+	{}
+
+	void BallObjectEventExecuter::OnEnable() {
+		ExecuteEvent(m_enableEventList);
+	}
+
+	void BallObjectEventExecuter::OnDisable() {
+		ExecuteEvent(m_disableEventList);
+	}
+
+	void BallObjectEventExecuter::ExecuteEvent(vector<Func>& eventList) {
+		for (auto& func : eventList) {
+			if (func)
+				func();
+		}
+	}
+
+	void BallObjectEventExecuter::AddEvent(const Func& enableEvent, const Func& disableEvent) {
+		AddEnableEvent(enableEvent);
+		AddDisableEvent(disableEvent);
+	}
+
+	void BallObjectEventExecuter::AddEnableEvent(const Func& enableEvent) {
+		m_enableEventList.push_back(enableEvent);
+	}
+	void BallObjectEventExecuter::AddDisableEvent(const Func& disableEvent) {
+		m_disableEventList.push_back(disableEvent);
+	}
+}
---
+++ SCRAMBLE BALL/GameSources/Watanabe/Component/BallObjectEventExecuter.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+#include "stdafx.h"
+
+namespace basecross {
+	class BallObjectEventExecuter :public Component {
+		using Func = function<void()>;
+
+		vector<Func> m_disableEventList;
+		vector<Func> m_enableEventList;
+
+		void ExecuteEvent(vector<Func>& eventList);
+	public:
+		BallObjectEventExecuter(const shared_ptr<GameObject>& owner);
+
+		void OnEnable()override;
+		void OnDisable()override;
+
+		void AddEvent(const Func& enableEvent, const Func& disableEvent);
+
+		void AddEnableEvent(const Func& enableEvent);
+		void AddDisableEvent(const Func& disableEvent);
+	};
+}
