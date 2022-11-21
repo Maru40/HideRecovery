4c36e1b29cf1bdf9297134c1dcfa85ad8fa02283
Maru40 <umeda.4072@gmail.com>
2022-11-21 12:31:49 +0900

    AIVritualControllerで当たったときにボールを取得するように変更

--- SCRAMBLE BALL/GameSources/Maruyama/Enemy/Component/AIVirtualController.h
+++ SCRAMBLE BALL/GameSources/Maruyama/Enemy/Component/AIVirtualController.h
@@ -10,16 +10,26 @@
 
 namespace basecross {
 
+	class PlayerStatus;
+	class OnlinePlayerSynchronizer;
+
 	class AIVirtualController : public Component
 	{
 		Vec2 m_inputDirection;			//入力方向
 
+		std::weak_ptr<PlayerStatus> m_status;							//ステータス
+		std::weak_ptr<OnlinePlayerSynchronizer> m_onlineSynchronizer;	//オンライン
+
 	public:
 		AIVirtualController(const std::shared_ptr<GameObject>& objPtr);
+
+		void OnStart() override;
 
 		void SetInputDirection(const Vec2& direction);
 
 		Vec2 GetInputDirection() const noexcept { return m_inputDirection; }
+
+		void OnCollisionEnter(std::shared_ptr<GameObject>& other);
 	};
 
 }
