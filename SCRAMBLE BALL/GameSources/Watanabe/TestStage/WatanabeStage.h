a751364b1eb4dce515b134b9f3fcdbc9ece694d4
Takumi Watanabe <zeal20300166@fsgcl.jp>
2022-11-22 13:25:57 +0900

    テストステージで板ポリのディゾブルをテスト

--- SCRAMBLE BALL/GameSources/Watanabe/TestStage/WatanabeStage.h
+++ SCRAMBLE BALL/GameSources/Watanabe/TestStage/WatanabeStage.h
@@ -2,12 +2,16 @@
 #include "stdafx.h"
 
 namespace basecross {
-	class ReconnectingInGameUI;
+	class DissolveAnimator;
 
 	class WatanabeStage :public Stage {
-		shared_ptr<ReconnectingInGameUI> m_ui;
+		vector<weak_ptr<DissolveAnimator>> m_dis;
+		shared_ptr<MeshResource> m_meshData;
+
 		//ビューの作成
 		void CreateViewLight();
+
+		void CreateMapOutCollision(const Vec3& startPosition, const Vec3& forward, const float& width, const float& height = 15.0f, const float& depth = 1.0f);
 	public:
 		WatanabeStage() :Stage() {}
 
