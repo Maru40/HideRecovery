c64f2e7663291d6da8d99552911befbfd24f5c38
Takumi Watanabe <zeal20300166@fsgcl.jp>
2022-11-22 14:28:47 +0900

    バリア用の描画コンポーネントを作成

---
+++ SCRAMBLE BALL/GameSources/Watanabe/Shader/BarrierShader.cpp
@@ -0,0 +1,5 @@
+﻿#include "stdafx.h"
+#include "BarrierShader.h"
+
+namespace basecross {
+}
---
+++ SCRAMBLE BALL/GameSources/Watanabe/Shader/BarrierShader.h
@@ -0,0 +1,7 @@
+﻿#pragma once
+#include "stdafx.h"
+
+namespace basecross {
+	class BarrierShader {
+	};
+}
