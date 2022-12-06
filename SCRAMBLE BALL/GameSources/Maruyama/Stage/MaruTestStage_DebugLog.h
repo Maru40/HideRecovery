01a3d41b8db832e6af1d0648c8565817c570753f
Maru40 <umeda.4072@gmail.com>
2022-12-06 16:36:08 +0900

    DebugStageの追加

---
+++ SCRAMBLE BALL/GameSources/Maruyama/Stage/MaruTestStage_DebugLog.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "stdafx.h"
+#include "GameStageBase.h"
+
+namespace basecross
+{
+
+	class MaruTestStage_DebugLog :public GameStageBase {
+		void CreateViewLight();
+
+	public:
+		MaruTestStage_DebugLog() :GameStageBase() {}
+
+		void OnCreate() override;
+		void OnUpdate() override;
+	};
+
+}
