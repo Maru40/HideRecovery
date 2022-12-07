474d2e9618228232c41543868531c52b6182f27a
Maru40 <umeda.4072@gmail.com>
2022-12-07 17:47:44 +0900

    ImpactMapのRectDataを別のクラスに映した。

--- SCRAMBLE BALL/GameSources/Maruyama/Enemy/ImpactMap/ImpactMap.cpp
+++ SCRAMBLE BALL/GameSources/Maruyama/Enemy/ImpactMap/ImpactMap.cpp
@@ -32,44 +32,6 @@ namespace basecross {
 	namespace maru {
 
 		//--------------------------------------------------------------------------------------
-		///	四角形データ(生成する範囲に利用)
-		//--------------------------------------------------------------------------------------
-
-		Rect::Rect()
-			: Rect(Vec3(0.0f), 0.0f, 0.0f)
-		{}
-
-		Rect::Rect(const Vec3& centerPosition, const float& width, const float& depth)
-			: centerPosition(centerPosition), width(width), depth(depth)
-		{}
-
-		Vec3 Rect::CalculateStartPosition() const {
-			auto position = centerPosition;
-			auto scale = Vec3(width, 0.0f, depth);
-			auto halfScale = scale * 0.5f;
-			float x = position.x - halfScale.x;
-			float y = position.y;
-			float z = position.z - halfScale.z;
-			Vec3 startPosition(x, y, z);
-
-			return startPosition;
-		}
-
-		bool Rect::IsInRect(const Vec3& position) {
-			auto rectStartPosition = CalculateStartPosition();
-
-			if (position.x > rectStartPosition.x &&
-				position.x < rectStartPosition.x + this->width &&
-				position.z > rectStartPosition.z &&
-				position.z < rectStartPosition.z + this->depth
-			) {
-				return true;
-			}
-
-			return false;
-		}
-
-		//--------------------------------------------------------------------------------------
 		///	Factory_影響マップ
 		//--------------------------------------------------------------------------------------
 
