eb5e8773419dcf4dcb3a3c0b135024ccbbc3cd43
Maru40 <umeda.4072@gmail.com>
2022-12-07 17:48:16 +0900

    ImpactCellMapの生成

---
+++ SCRAMBLE BALL/GameSources/Maruyama/Enemy/ImpactMap/ImpactCellMap.cpp
@@ -0,0 +1,20 @@
+
+/*!
+@file ImpactCellMap.cpp
+@brief ImpactCellMapのクラス実体
+担当：丸山裕喜
+*/
+
+#include "stdafx.h"
+#include "Project.h"
+
+#include "ImpactCellMap.h"
+
+namespace basecross {
+
+	namespace maru {
+
+
+
+	}
+}
---
+++ SCRAMBLE BALL/GameSources/Maruyama/Enemy/ImpactMap/ImpactCellMap.h
@@ -0,0 +1,25 @@
+
+/*!
+@file ImpactCellMap.h
+@brief ImpactCellMap‚È‚Ç
+’S“–FŠÛR—TŠì
+*/
+
+#pragma once
+#include "stdafx.h"
+
+namespace basecross {
+
+	namespace maru {
+
+		class ImpactCellMap
+		{
+		public:
+			ImpactCellMap();
+
+			
+		};
+
+	}
+
+}
