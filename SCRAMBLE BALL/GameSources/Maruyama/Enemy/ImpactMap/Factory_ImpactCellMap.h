27836ce98c6ae791adcb5bbff0583e7b897ccd8e
Maru40 <umeda.4072@gmail.com>
2022-12-07 19:11:46 +0900

    Factory_ImpactCellMapの更新

--- SCRAMBLE BALL/GameSources/Maruyama/Enemy/ImpactMap/Factory_ImpactCellMap.h
+++ SCRAMBLE BALL/GameSources/Maruyama/Enemy/ImpactMap/Factory_ImpactCellMap.h
@@ -7,15 +7,40 @@
 #pragma once
 #include "stdafx.h"
 
+#include "Maruyama/Patch/CommonStructs/RectData.h"
+
 namespace basecross {
 
 	namespace maru {
 
+		//--------------------------------------------------------------------------------------
+		///	前方宣言
+		//--------------------------------------------------------------------------------------
+		class Cell;
+
+		//--------------------------------------------------------------------------------------
+		///	セルマップファクトリーのパラメータ
+		//--------------------------------------------------------------------------------------
 		struct Factory_ImpactCellMap_Parametor
 		{
+			Rect oneCellRect;		//一つのCellの生成情報。
+			int widthCount;			//横に展開するセルの数。
+			int depthCount;			//奥行きに展開するセルの数。
+			Vec3 centerPosition;	//マップの中心位置。
 
+			Factory_ImpactCellMap_Parametor();
+
+			Factory_ImpactCellMap_Parametor(
+				const Rect& oneCellRect,
+				const int widthCount,
+				const int depthCount,
+				const Vec3& centerPosition
+			);
 		};
 
+		//--------------------------------------------------------------------------------------
+		///	セルマップファクトリー
+		//--------------------------------------------------------------------------------------
 		class Factory_ImpactCellMap
 		{
 		public:
@@ -24,7 +49,7 @@ namespace basecross {
 		private:
 
 		public:
-			static void CreateMap(const Parametor& param);
+			static std::vector<std::shared_ptr<Cell>> CreateCells(const Parametor& param);
 		};
 
 	}
