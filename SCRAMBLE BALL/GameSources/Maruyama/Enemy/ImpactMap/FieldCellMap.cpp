d2e60255c8340ba9c5e06c4b6a605e350f2ad99a
Maru40 <umeda.4072@gmail.com>
2022-12-08 10:13:40 +0900

    FiledCellMapの生成

---
+++ SCRAMBLE BALL/GameSources/Maruyama/Enemy/ImpactMap/FieldCellMap.cpp
@@ -0,0 +1,30 @@
+
+/*!
+@file FieldCellMap.cpp
+@brief FieldCellMapのクラス実体
+担当：丸山裕喜
+*/
+
+#include "stdafx.h"
+#include "Project.h"
+
+#include "FieldCellMap.h"
+
+namespace basecross {
+
+	namespace maru {
+
+		FieldCellMap::FieldCellMap(const std::shared_ptr<GameObject>& objPtr) :
+			Component(objPtr)
+		{}
+
+		void FieldCellMap::OnCreate() {
+
+		}
+
+		void FieldCellMap::OnUpdate() {
+
+		}
+
+	}
+}
---
+++ SCRAMBLE BALL/GameSources/Maruyama/Enemy/ImpactMap/FieldCellMap.h
@@ -0,0 +1,35 @@
+
+/*!
+@file FieldCellMap.h
+@brief FieldCellMapなど
+担当：丸山裕喜
+*/
+
+#pragma once
+#include "stdafx.h"
+
+namespace basecross {
+
+	namespace maru {
+
+		//--------------------------------------------------------------------------------------
+		///	前方宣言
+		//--------------------------------------------------------------------------------------
+		class ImpactCellMap;
+
+		//--------------------------------------------------------------------------------------
+		///	フィールド用のセルマップ
+		//--------------------------------------------------------------------------------------
+		class FieldCellMap : public Component
+		{
+		public:
+			FieldCellMap(const std::shared_ptr<GameObject>& objPtr);
+
+			void OnCreate() override;
+			void OnUpdate() override;
+
+			
+		};
+
+	}
+}
