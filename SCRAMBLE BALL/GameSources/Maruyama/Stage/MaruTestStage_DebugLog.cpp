3b380c7f90e6484039850d42cba8091639df1219
Maru40 <umeda.4072@gmail.com>
2022-12-12 03:09:16 +0900

    TestStageの更新

--- SCRAMBLE BALL/GameSources/Maruyama/Stage/MaruTestStage_DebugLog.cpp
+++ SCRAMBLE BALL/GameSources/Maruyama/Stage/MaruTestStage_DebugLog.cpp
@@ -11,6 +11,8 @@
 #include "Maruyama/Enemy/ImpactMap/Cell.h"
 #include "Maruyama/Enemy/ImpactMap/FieldCellMap.h"
 
+#include "Maruyama/Enemy/ImpactMap/Factory_WayPointMap_FloodFill.h"
+
 namespace basecross {
 
 	void MaruTestStage_DebugLog::CreateViewLight() {
@@ -33,14 +35,15 @@ namespace basecross {
 		auto log = AddGameObject<Debug>();
 		Debug::GetInstance()->Log(L"MaruTestStage_DebugLog");
 
+		//スレッドプールのデバッグ
 		AddGameObject<Tester::TesterThreadObject>();
 
-		//Cellのデバッグ表示
-		//auto cellParam = maru::Cell::Parametor();
-		//auto cell = std::make_shared<maru::Cell>(cellParam);
-		//cell->OnDebugDraw(GetThis<Stage>());
+		//セルデバッグ表示
+		//AddGameObject<GameObject>()->AddComponent<maru::FieldCellMap>();
 
-		AddGameObject<GameObject>()->AddComponent<maru::FieldCellMap>();
+		//フラッドフィルアルゴリズムテスト
+		//auto floodFill = maru::Factory_WayPointMap_FloodFill(GetThis<Stage>());
+
 	}
 
 	void MaruTestStage_DebugLog::OnUpdate() {
