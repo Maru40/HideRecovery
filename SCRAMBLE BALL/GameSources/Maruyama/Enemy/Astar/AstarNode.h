fb10a06913e5c4d0087a9e22a7f74dec33573c8a
Maru40 <umeda.4072@gmail.com>
2022-12-14 00:24:58 +0900

    AstarNodeの生成

---
+++ SCRAMBLE BALL/GameSources/Maruyama/Enemy/Astar/AstarNode.h
@@ -0,0 +1,37 @@
+
+/*!
+@file AstarNode.h
+@brief AstarNode‚È‚Ç
+’S“–FŠÛR—TŠì
+*/
+
+#pragma once
+#include "stdafx.h"
+
+#include "Maruyama/StateMachine/NodeBase.h"
+
+namespace basecross {
+
+	namespace maru {
+
+		struct AstarNode_Parametor
+		{
+
+		};
+
+		class AstarNode : public NodeBase
+		{
+		public:
+
+
+		private:
+
+
+		public:
+			AstarNode();
+
+			AstarNode(const int index);
+		};
+
+	}
+}
