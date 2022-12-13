012c00e4a77fd6a5a5b2ae5d9b03963fdaf556d5
Maru40 <umeda.4072@gmail.com>
2022-12-14 01:33:33 +0900

    AstarNodeにParentの設定を追加

--- SCRAMBLE BALL/GameSources/Maruyama/Enemy/Astar/AstarNode.cpp
+++ SCRAMBLE BALL/GameSources/Maruyama/Enemy/Astar/AstarNode.cpp
@@ -19,11 +19,34 @@ namespace basecross {
 		{}
 
 		AstarNode::AstarNode(const int index) :
-			NodeBase(index)
+			AstarNode(index, Vec3(0.0f))
 		{}
 
-		
+		AstarNode::AstarNode(const int index, const Vec3& position) :
+			AstarNode(index, position, nullptr)
+		{}
 
+		AstarNode::AstarNode(const int index, const Vec3& position, const std::shared_ptr<AstarNode>& parent):
+			NodeBase(index), 
+			m_position(position), 
+			m_parent(parent)
+		{}
+
+		//--------------------------------------------------------------------------------------
+		///	アクセッサ
+		//--------------------------------------------------------------------------------------
+
+		void AstarNode::SetParent(const std::shared_ptr<AstarNode>& parent) noexcept {
+			m_parent = parent;
+		}
+
+		std::shared_ptr<AstarNode> AstarNode::GetParent() const noexcept {
+			return m_parent.lock();
+		}
+
+		bool AstarNode::HasParent() const noexcept {
+			return !m_parent.expired();
+		}
 	}
 
 }
