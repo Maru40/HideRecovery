b5c93fef55f42e83dd55c9fa2cd6fb0af03cc37a
Maru40 <umeda.4072@gmail.com>
2023-02-12 03:07:26 +0900

    AIDirectorにスレッドプールの実装

--- SCRAMBLE BALL/GameSources/Maruyama/Enemy/AIDirector/AIDirector_Ex.cpp
+++ SCRAMBLE BALL/GameSources/Maruyama/Enemy/AIDirector/AIDirector_Ex.cpp
@@ -13,6 +13,8 @@
 #include "TupleSpace.h"
 #include "AIRequester.h"
 
+#include "Maruyama/Thread/ThreadPool.h"
+
 namespace basecross {
 
 	std::weak_ptr<Enemy::AIDirector_Ex> maru::SingletonComponent<Enemy::AIDirector_Ex>::sm_instance;
@@ -20,10 +22,11 @@ namespace basecross {
 	namespace Enemy {
 
 		AIDirector_Ex::AIDirector_Ex(const std::shared_ptr<GameObject>& owner) :
-			SingletonComponent(owner)
+			SingletonComponent(owner),
+			m_threadPool(new ThreadPool())
 		{}
 
-				
+		
 
 	}
 
