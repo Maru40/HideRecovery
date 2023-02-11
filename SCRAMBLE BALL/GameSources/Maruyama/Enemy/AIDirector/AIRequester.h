90bf375f5d66f2674b703994ae727387ec9e2cd3
Maru40 <umeda.4072@gmail.com>
2023-02-12 02:36:10 +0900

    AIRequesterの生成

---
+++ SCRAMBLE BALL/GameSources/Maruyama/Enemy/AIDirector/AIRequester.h
@@ -0,0 +1,25 @@
+
+/*!
+@file I_AIRequster.h
+@brief I_AIRequsterなど
+担当：丸山裕喜
+*/
+
+#pragma once
+#include "stdafx.h"
+
+namespace basecross {
+
+	namespace Enemy {
+
+		//--------------------------------------------------------------------------------------
+		/// AIのリクエストする物
+		//--------------------------------------------------------------------------------------
+		class I_AIRequster
+		{
+		public:
+
+		};
+
+	}
+}
