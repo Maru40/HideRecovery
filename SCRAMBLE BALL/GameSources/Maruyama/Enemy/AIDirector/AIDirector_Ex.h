21c38d0d6fca888626c2bacf9554238400346cb6
Maru40 <umeda.4072@gmail.com>
2023-03-06 01:45:17 +0900

    AIDirectorにAstarThreadControllerの生成

--- SCRAMBLE BALL/GameSources/Maruyama/Enemy/AIDirector/AIDirector_Ex.h
+++ SCRAMBLE BALL/GameSources/Maruyama/Enemy/AIDirector/AIDirector_Ex.h
@@ -10,16 +10,13 @@
 
 #include "Maruyama/Utility/SingletonComponent/SingletonComponent.h"
 
-#include <future>
-#include <tuple>
-#include <utility>
-
 namespace basecross {
 
 	//--------------------------------------------------------------------------------------
 	/// 前方宣言
 	//--------------------------------------------------------------------------------------
 	class ThreadPool;
+	class AstarThreadController;
 
 	namespace Enemy {
 
@@ -35,11 +32,10 @@ namespace basecross {
 		//--------------------------------------------------------------------------------------
 		class AIDirector_Ex : public maru::SingletonComponent<AIDirector_Ex>
 		{
-		private:
-			std::unique_ptr<ThreadPool> m_threadPool;	//スレッドプール
+		public:
 
-			//std::unordered_map<I_AIRequester*, std::queue<AIRequestData*>> m_requestsMap;	//リクエストマップ
-			std::queue<AIRequestData*> m_requestDatas;	//リクエスト一覧
+		private:
+			std::unique_ptr<AstarThreadController> m_astarThread;	//AstarThreadController
 
 		public:
 			AIDirector_Ex(const std::shared_ptr<GameObject>& owner);
@@ -48,31 +44,13 @@ namespace basecross {
 
 		private:
 
-			//リクエスト処理
-			//C++14,17でそれぞれ別のテンプレートが推奨されているため、分岐
-			#if ((defined(_MSVC_LANG) && _MSVC_LANG >= 201703L) || __cplusplus >= 201703L)
-				/**
-				* @brief Submit a function with zero or more arguments and a return value into the task queue,
-				* and get a future for its eventual returned value.
-				*/
-				// C++17 から推奨されている, 関数の返り値の型R を取得する方法
-					template <class F, class... Args,
-						class R = std::invoke_result_t<std::decay_t<F>, std::decay_t<Args>...>>
-			#else
-				// C++14 で, 関数の返り値の型R を取得する方法
-					template <class F, class... Args,
-						class R = typename std::result_of<std::decay_t<F>(std::decay_t<Args>...)>::type>
-			#endif
-			_NODISCARD std::future<R> Request(F&& function, Args... args) {
-				return m_threadPool->Submit(function, args...);
-			}
-
 		public:
+
 			/// <summary>
-			/// タスクのリクエスト処理
+			/// Astarスレッドの取得
 			/// </summary>
-			/// <param name="requester">リクエスト者</param>
-			_NODISCARD void Request(const std::shared_ptr<I_AIRequester>& requester, I_AIRequestEvent* const eventPtr);
+			/// <returns></returns>
+			_NODISCARD const std::unique_ptr<AstarThreadController>& GetAstarThreadController() const;
 
 		};
 
